0ab2d215bc6573368e4447c43a53460f014074ba
Karel Minarik <karel.minarik@elasticsearch.org>
2013-05-03 15:25:36 +0200

    [CLIENT] Setup: Added Rake test tasks, Yard, Cane, SimpleCov and RequireProf integrations
    * Do not install "cane" and "simplecov" gems on Ruby < 1.9
    * Do not load Cane task on Ruby < 1.9
    * Require "rubygems" on Ruby < 1.9
    * Print require times when REQUIRE_PROF is set
    * Evaluate testing coverage when COVERAGE is set
    * Generate documentation with Yard

--- elasticsearch-client/elasticsearch-client.gemspec
+++ elasticsearch-client/elasticsearch-client.gemspec
@@ -29,6 +29,13 @@ Gem::Specification.new do |s|
   s.add_development_dependency "shoulda-context"
   s.add_development_dependency "mocha"
   s.add_development_dependency "turn"
+  s.add_development_dependency "yard"
+
+  if defined?(RUBY_VERSION) && RUBY_VERSION > '1.9'
+    s.add_development_dependency "simplecov"
+    s.add_development_dependency "cane"
+    s.add_development_dependency "require-prof"
+  end
 
   s.description = <<-DESC.gsub(/^    /, '')
     Ruby client for Elasticsearch.
--- elasticsearch-client/test/test_helper.rb
+++ elasticsearch-client/test/test_helper.rb
@@ -1,9 +1,18 @@
+RUBY_1_8 = defined?(RUBY_VERSION) && RUBY_VERSION < '1.9'
+
+require 'rubygems' if defined?(RUBY_VERSION) && RUBY_VERSION < '1.9'
+
+require 'simplecov' and SimpleCov.start { add_filter "/test/" } if ENV["COVERAGE"]
+
 require 'test/unit'
 require 'shoulda-context'
 require 'mocha/setup'
 require 'turn' unless ENV["TM_FILEPATH"] || ENV["NOTURN"]
 
+require 'require-prof' if ENV["REQUIRE_PROF"]
 require 'elasticsearch-client'
+
+RequireProf.print_timing_infos if ENV["REQUIRE_PROF"]
 
 class Test::Unit::TestCase
   def setup
