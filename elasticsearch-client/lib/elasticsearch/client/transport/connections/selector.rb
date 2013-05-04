1a9b3b65b4852b6a3db1e6f3dea2f20a0a377722
Karel Minarik <karel.minarik@elasticsearch.org>
2013-05-04 12:39:10 +0200

    [CLIENT] Added the <Connections::Selector> implementations (random, roundrobin)

--- elasticsearch-client/lib/elasticsearch/client/transport/connections/selector.rb
+++ elasticsearch-client/lib/elasticsearch/client/transport/connections/selector.rb
@@ -5,16 +5,34 @@ module Elasticsearch
         module Selector
 
           module Base
+            attr_reader :connections
+
             def initialize(arguments={})
+              @connections = arguments[:connections]
+            end
+
+            def select(options={})
+              raise NoMethodError, "Implement this method in the selector implementation."
             end
           end
 
           class Random
             include Base
+
+            def select(options={})
+              connections.to_a.send( defined?(RUBY_VERSION) && RUBY_VERSION > '1.9' ? :sample : :choice)
+            end
           end
 
           class RoundRobin
             include Base
+
+            def select(options={})
+              # On Ruby 1.9, Array#rotate could be used instead
+              @current = @current.nil? ? 0 : @current+1
+              @current = 0 if @current >= connections.size
+              connections[@current]
+            end
           end
 
         end
