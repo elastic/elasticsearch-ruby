1a1e3bd1f884daa2d61be760a232157480f28841
Karel Minarik <karel.minarik@elasticsearch.org>
2013-06-12 20:27:50 +0200

    [API] Moved the "api" and "namespace" source files under `elasticsearch/api`
    Reflect better the namespacing and layout of the library.

--- elasticsearch-api/lib/elasticsearch/api.rb
+++ elasticsearch-api/lib/elasticsearch/api.rb
@@ -1,11 +1,11 @@
 require "multi_json"
 
 require "elasticsearch/api/version"
-require "elasticsearch/namespace/common"
-require "elasticsearch/utils"
+require "elasticsearch/api/namespace/common"
+require "elasticsearch/api/utils"
 
-Dir[ File.expand_path('../api/**/*.rb', __FILE__) ].each       { |f| require f }
-Dir[ File.expand_path('../namespace/**/*.rb', __FILE__) ].each { |f| require f }
+Dir[ File.expand_path('../api/actions/**/*.rb', __FILE__) ].each   { |f| require f }
+Dir[ File.expand_path('../api/namespace/**/*.rb', __FILE__) ].each { |f| require f }
 
 module Elasticsearch
   module API
