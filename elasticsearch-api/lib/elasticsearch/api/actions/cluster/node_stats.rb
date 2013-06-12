35c2c9b5da27a5288f605200ad744ba7986c8fb3
Karel Minarik <karel.minarik@elasticsearch.org>
2013-06-12 22:16:42 +0200

    [API] Added the "Node Stats" API
    The API should be potentially split into multiple methods,
    to consolidate the volatile semantics in calls like:
    
    * "/_nodes/stats/?http,fs"
    * "/_nodes/stats/indices"
    * "/_nodes/stats/indices/docs"
    * "/_nodes/stats/indices/fielddata"
    * "/_nodes/stats/indices/fielddata/fieldA,fieldB"

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/cluster/node_stats.rb
@@ -0,0 +1,71 @@
+module Elasticsearch
+  module API
+    module Cluster
+      module Actions
+
+        # Returns statistical information about nodes in the cluster.
+        #
+        # @option arguments [List] :metric Limit the information returned for `indices` family to a specific metric
+        #                                  (options: docs, fielddata, filter_cache, flush, get, id_cache, indexing, merges,
+        #                                  refresh, search, store, warmer)
+        # @option arguments [List] :node_id A comma-separated list of node IDs or names to limit the returned information;
+        #                                   use `_local` to return information from the node you're connecting to, leave
+        #                                   empty to get information from all nodes
+        # @option arguments [Boolean] :all Return all available information
+        # @option arguments [Boolean] :clear Reset the default level of detail
+        # @option arguments [List] :fields A comma-separated list of fields for `fielddata` metric (supports wildcards)
+        # @option arguments [Boolean] :fs Return information about the filesystem
+        # @option arguments [Boolean] :http Return information about HTTP
+        # @option arguments [Boolean] :indices Return information about indices
+        # @option arguments [Boolean] :jvm Return information about the JVM
+        # @option arguments [Boolean] :network Return information about network
+        # @option arguments [Boolean] :os Return information about the operating system
+        # @option arguments [Boolean] :process Return information about the Elasticsearch process
+        # @option arguments [Boolean] :thread_pool Return information about the thread pool
+        # @option arguments [Boolean] :transport Return information about transport
+        #
+        # @see http://elasticsearch.org/guide/reference/api/admin-cluster-nodes-stats/
+        #
+        def node_stats(arguments={})
+          method = 'GET'
+
+          case
+            # Field data metric for the `indices` metric family
+            when arguments[:indices] && arguments[:metric] == 'fielddata'
+              path   = Utils.__pathify( '_nodes', Utils.__listify(arguments[:node_id]), 'stats/indices/fielddata' )
+              params = { :fields => Utils.__listify(arguments[:fields]) }
+
+            # `indices` metric family incl. a metric
+            when arguments[:indices] && arguments[:metric]
+              path   = Utils.__pathify( '_nodes', Utils.__listify(arguments[:node_id]), 'stats/indices', arguments[:metric] )
+              params = {}
+
+            else
+              path   = Utils.__pathify( '_nodes', Utils.__listify(arguments[:node_id]), 'stats' )
+
+              params = arguments.select do |k,v|
+                [ :all,
+                  :clear,
+                  :fields,
+                  :fs,
+                  :http,
+                  :indices,
+                  :jvm,
+                  :network,
+                  :os,
+                  :process,
+                  :thread_pool,
+                  :transport ].include?(k)
+              end
+              # Normalize Ruby 1.8 and Ruby 1.9 Hash#select behaviour
+              params = Hash[params] unless params.is_a?(Hash)
+          end
+
+          body = nil
+
+          perform_request(method, path, params, body).body
+        end
+      end
+    end
+  end
+end
---
+++ elasticsearch-api/test/unit/cluster/node_stats_test.rb
@@ -0,0 +1,65 @@
+require 'test_helper'
+
+module Elasticsearch
+  module Test
+    class ClusterNodeStatsTest < ::Test::Unit::TestCase
+
+      context "Cluster: Node stats" do
+        subject { FakeClient.new(nil) }
+
+        should "perform correct request" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal 'GET', method
+            assert_equal '_nodes/stats', url
+            assert_equal Hash.new, params
+            assert_nil   body
+            true
+          end.returns(FakeResponse.new)
+
+          subject.cluster.node_stats
+        end
+
+        should "send :node_id correctly" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal '_nodes/foo/stats', url
+            true
+          end.returns(FakeResponse.new)
+
+          subject.cluster.node_stats :node_id => 'foo'
+        end
+
+        should "get specific metric families" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal '_nodes/stats', url
+            assert_equal( {:http => true, :fs => true}, params )
+            true
+          end.returns(FakeResponse.new)
+
+          subject.cluster.node_stats :http => true, :fs => true
+        end
+
+        should "get specific metric for the indices family" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal '_nodes/stats/indices/filter_cache', url
+            true
+          end.returns(FakeResponse.new)
+
+          subject.cluster.node_stats :indices => true, :metric => 'filter_cache'
+        end
+
+        should "get fielddata statistics for the indices family" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal '_nodes/stats/indices/fielddata', url
+            assert_equal( {:fields => 'foo,bar'}, params )
+            true
+          end.returns(FakeResponse.new).twice
+
+          subject.cluster.node_stats :indices => true, :metric => 'fielddata', :fields => 'foo,bar'
+          subject.cluster.node_stats :indices => true, :metric => 'fielddata', :fields => ['foo','bar']
+        end
+
+      end
+
+    end
+  end
+end
