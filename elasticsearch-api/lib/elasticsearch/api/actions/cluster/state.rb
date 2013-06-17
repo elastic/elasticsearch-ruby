a0dcc9e7fab1ab999a38a7569995e9844361f81f
Karel Minarik <karel.minarik@elasticsearch.org>
2013-06-18 00:32:51 +0200

    [API] Added the "Cluster State" API

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/cluster/state.rb
@@ -0,0 +1,43 @@
+module Elasticsearch
+  module API
+    module Cluster
+      module Actions
+
+        # Get information about the cluster state (indices settings, allocations, etc)
+        #
+        # @option arguments [Boolean] :filter_blocks Do not return information about blocks
+        # @option arguments [Boolean] :filter_index_templates Do not return information about index templates
+        # @option arguments [List] :filter_indices Limit returned metadata information to specific indices
+        # @option arguments [Boolean] :filter_metadata Do not return information about indices metadata
+        # @option arguments [Boolean] :filter_nodes Do not return information about nodes
+        # @option arguments [Boolean] :filter_routing_table Do not return information about shard allocation
+        #                                                   (`routing_table` and `routing_nodes`)
+        # @option arguments [Boolean] :local Return local information, do not retrieve the state from master node
+        #                                    (default: false)
+        # @option arguments [Time] :master_timeout Specify timeout for connection to master
+        #
+        # @see http://elasticsearch.org/guide/reference/api/admin-cluster-state/
+        #
+        def state(arguments={})
+          method = 'GET'
+          path   = "_cluster/state"
+          params = arguments.select do |k,v|
+            [ :filter_blocks,
+              :filter_index_templates,
+              :filter_indices,
+              :filter_metadata,
+              :filter_nodes,
+              :filter_routing_table,
+              :local,
+              :master_timeout ].include?(k)
+          end
+          # Normalize Ruby 1.8 and Ruby 1.9 Hash#select behaviour
+          params = Hash[params] unless params.is_a?(Hash)
+          body = nil
+
+          perform_request(method, path, params, body).body
+        end
+      end
+    end
+  end
+end
---
+++ elasticsearch-api/test/unit/cluster/state_test.rb
@@ -0,0 +1,37 @@
+require 'test_helper'
+
+module Elasticsearch
+  module Test
+    class ClusterStateTest < ::Test::Unit::TestCase
+
+      context "Cluster: State" do
+        subject { FakeClient.new(nil) }
+
+        should "perform correct request" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal 'GET', method
+            assert_equal '_cluster/state', url
+            assert_equal Hash.new, params
+            assert_nil   body
+            true
+          end.returns(FakeResponse.new)
+
+          subject.cluster.state
+        end
+
+        should "send the API parameters" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal 'GET', method
+            assert_equal '_cluster/state', url
+            assert_equal({:filter_blocks => true}, params)
+            true
+          end.returns(FakeResponse.new)
+
+          subject.cluster.state :filter_blocks => true
+        end
+
+      end
+
+    end
+  end
+end
