bbae91b54aea48cbbc4a28966a45c45285447f9d
Karel Minarik <karel.minarik@elasticsearch.org>
2013-06-19 00:11:45 +0200

    [API] Added the "Clear Cache" API

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/indices/clear_cache.rb
@@ -0,0 +1,67 @@
+module Elasticsearch
+  module API
+    module Indices
+      module Actions
+
+        # Clear caches and other auxiliary data structures.
+        #
+        # Can be performed against a specific index, or against all indices.
+        #
+        # By default, all caches and data structures will be cleared.
+        # Pass a specific cache or structure name to clear just a single one.
+        #
+        # @example Clear all caches and data structures
+        #
+        #     client.indices.clear_cache
+        #
+        # @example Clear the field data structure only
+        #
+        #     client.indices.clear_cache field_data: true
+        #
+        # @example Clear only specific field in the field data structure
+        #
+        #     client.indices.clear_cache field_data: true, fields: 'created_at', filter_cache: false, id_cache: false
+        #
+        # @option arguments [List] :index A comma-separated list of index name to limit the operation
+        # @option arguments [Boolean] :field_data Clear field data
+        # @option arguments [Boolean] :fielddata Clear field data
+        # @option arguments [List] :fields A comma-separated list of fields to clear when using the
+        #                                  `field_data` parameter(default: all)
+        # @option arguments [Boolean] :filter Clear filter caches
+        # @option arguments [Boolean] :filter_cache Clear filter caches
+        # @option arguments [Boolean] :filter_keys A comma-separated list of keys to clear when using the
+        #                                          `filter_cache` parameter (default: all)
+        # @option arguments [Boolean] :id Clear ID caches for parent/child
+        # @option arguments [Boolean] :id_cache Clear ID caches for parent/child
+        # @option arguments [String] :ignore_indices When performed on multiple indices,
+        #                                            allows to ignore `missing` ones (options: none, missing)
+        # @option arguments [List] :index A comma-separated list of index name to limit the operation
+        # @option arguments [Boolean] :recycler Clear the recycler cache
+        #
+        # @see http://www.elasticsearch.org/guide/reference/api/admin-indices-clearcache/
+        #
+        def clear_cache(arguments={})
+          method = 'POST'
+          path   = Utils.__pathify( Utils.__listify(arguments[:index]), '_cache/clear' )
+          params = arguments.select do |k,v|
+            [ :field_data,
+              :fielddata,
+              :fields,
+              :filter,
+              :filter_cache,
+              :filter_keys,
+              :id,
+              :id_cache,
+              :ignore_indices,
+              :recycler ].include?(k)
+          end
+          # Normalize Ruby 1.8 and Ruby 1.9 Hash#select behaviour
+          params = Hash[params] unless params.is_a?(Hash)
+          body = nil
+
+          perform_request(method, path, params, body).body
+        end
+      end
+    end
+  end
+end
