c9242f506efa9b5ff0cba23a90784218daaeae68
Karel Minarik <karel.minarik@elasticsearch.org>
2013-06-19 00:43:50 +0200

    [API] Added the "Close Index" API

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/indices/close.rb
@@ -0,0 +1,29 @@
+module Elasticsearch
+  module API
+    module Indices
+      module Actions
+
+        # Close an index (keep the data on disk, but deny operations with the index)
+        #
+        # @option arguments [String] :index The name of the index (*Required*)
+        # @option arguments [Time] :timeout Explicit operation timeout
+        #
+        # @see http://www.elasticsearch.org/guide/reference/api/admin-indices-open-close/
+        #
+        def close(arguments={})
+          raise ArgumentError, "Required argument 'index' missing" unless arguments[:index]
+          method = 'POST'
+          path   = "#{arguments[:index]}/_close"
+          params = arguments.select do |k,v|
+            [ :timeout ].include?(k)
+          end
+          # Normalize Ruby 1.8 and Ruby 1.9 Hash#select behaviour
+          params = Hash[params] unless params.is_a?(Hash)
+          body   = nil
+
+          perform_request(method, path, params, body).body
+        end
+      end
+    end
+  end
+end
