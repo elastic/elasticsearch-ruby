66746f308ac24d35596226e1d366ceb5584f3c7e
Karel Minarik <karel.minarik@elasticsearch.org>
2013-06-18 22:24:37 +0200

    [API] Added the "Analyze" API

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/indices/analyze.rb
@@ -0,0 +1,53 @@
+module Elasticsearch
+  module API
+    module Indices
+      module Actions
+
+        # Return the result of the analysis process (tokens)
+        #
+        # Allows to "test-drive" the Elasticsearch analysis process by performing the analysis on the
+        # same text with different analyzers. An ad-hoc analysis chain can be built from specific
+        # _tokenizer_ and _filters_.
+        #
+        # @option arguments [String] :index The name of the index to scope the operation
+        # @option arguments [Hash] :body The text on which the analysis should be performed
+        # @option arguments [String] :analyzer The name of the analyzer to use
+        # @option arguments [String] :field Use the analyzer configured for this field
+        #                                   (instead of passing the analyzer name)
+        # @option arguments [List] :filters A comma-separated list of filters to use for the analysis
+        # @option arguments [String] :index The name of the index to scope the operation
+        # @option arguments [Boolean] :prefer_local With `true`, specify that a local shard should be used if available,
+        #                                           with `false`, use a random shard (default: true)
+        # @option arguments [String] :text The text on which the analysis should be performed
+        #                                  (when request body is not used)
+        # @option arguments [String] :tokenizer The name of the tokenizer to use for the analysis
+        # @option arguments [String] :format Format of the output (options: detailed, text)
+        #
+        # @see http://www.elasticsearch.org/guide/reference/api/admin-indices-analyze/
+        #
+        def analyze(arguments={})
+          method = 'GET'
+          path   = Utils.__pathify( arguments[:index], '_analyze' )
+          params = arguments.select do |k,v|
+            [ :analyzer,
+              :field,
+              :filters,
+              :index,
+              :prefer_local,
+              :text,
+              :tokenizer,
+              :format ].include?(k)
+          end
+          # Normalize Ruby 1.8 and Ruby 1.9 Hash#select behaviour
+          params = Hash[params] unless params.is_a?(Hash)
+
+          params[:filters] = Utils.__listify(params[:filters]) if params[:filters]
+
+          body   = arguments[:body]
+
+          perform_request(method, path, params, body).body
+        end
+      end
+    end
+  end
+end
