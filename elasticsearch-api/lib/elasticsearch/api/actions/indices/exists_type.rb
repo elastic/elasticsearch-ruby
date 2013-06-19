e5baae636d7cad88638005cc5d20c892e6790f42
Karel Minarik <karel.minarik@elasticsearch.org>
2013-06-19 17:09:40 +0200

    [API] Added the "Type Exists" API

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/indices/exists_type.rb
@@ -0,0 +1,39 @@
+module Elasticsearch
+  module API
+    module Indices
+      module Actions
+
+        # Return true if the specified type exists, false otherwise.
+        #
+        # @option arguments [List] :index A comma-separated list of index names; use `_all`
+        #                                 to check the types across all indices (*Required*)
+        # @option arguments [List] :type A comma-separated list of document types to check (*Required*)
+        # @option arguments [String] :ignore_indices When performed on multiple indices, allows to ignore `missing` ones
+        #                                            (options: none, missing)
+        #
+        # @see http://www.elasticsearch.org/guide/reference/api/admin-indices-types-exists/
+        #
+        def exists_type(arguments={})
+          raise ArgumentError, "Required argument 'index' missing" unless arguments[:index]
+          raise ArgumentError, "Required argument 'type' missing" unless arguments[:type]
+          method = 'HEAD'
+          path   = Utils.__pathify( Utils.__listify(arguments[:index]), arguments[:type] )
+          params = arguments.select do |k,v|
+            [ :ignore_indices ].include?(k)
+          end
+          # Normalize Ruby 1.8 and Ruby 1.9 Hash#select behaviour
+          params = Hash[params] unless params.is_a?(Hash)
+          body = nil
+
+          perform_request(method, path, params, body).status == 200 ? true : false
+        rescue Exception => e
+          if e.class.to_s =~ /NotFound/ || e.message =~ /Not\s*Found|404/i
+            false
+          else
+            raise e
+          end
+        end
+      end
+    end
+  end
+end
