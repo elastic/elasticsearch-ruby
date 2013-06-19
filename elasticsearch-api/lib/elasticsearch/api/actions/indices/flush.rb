8e1d78d6a2a6f97fba0c39df4230908fd98d3fd6
Karel Minarik <karel.minarik@elasticsearch.org>
2013-06-19 17:45:37 +0200

    [API] Added the "Flush" API

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/indices/flush.rb
@@ -0,0 +1,40 @@
+module Elasticsearch
+  module API
+    module Indices
+      module Actions
+
+        # "Flush" the index or indices.
+        #
+        # The "flush" operation clears the transaction log and memory and writes data to disk.
+        # It corresponds to a Lucene "commit" operation.
+        #
+        # @note The flush operation is handled automatically by Elasticsearch, you don't need to perform it manually.
+        #
+        # @option arguments [List] :index A comma-separated list of index names; use `_all` or empty string for all indices
+        # @option arguments [Boolean] :force TODO: ?
+        # @option arguments [Boolean] :full TODO: ?
+        # @option arguments [String] :ignore_indices When performed on multiple indices, allows to ignore `missing` ones
+        #                                            (options: none, missing)
+        # @option arguments [Boolean] :refresh Refresh the index after performing the operation
+        #
+        # @see http://www.elasticsearch.org/guide/reference/api/admin-indices-flush/
+        #
+        def flush(arguments={})
+          method = 'POST'
+          path   = Utils.__pathify( Utils.__listify(arguments[:index]), '_flush' )
+          params = arguments.select do |k,v|
+            [ :force,
+              :full,
+              :ignore_indices,
+              :refresh ].include?(k)
+          end
+          # Normalize Ruby 1.8 and Ruby 1.9 Hash#select behaviour
+          params = Hash[params] unless params.is_a?(Hash)
+          body = nil
+
+          perform_request(method, path, params, body).body
+        end
+      end
+    end
+  end
+end
---
+++ elasticsearch-api/test/unit/indices/flush_test.rb
@@ -0,0 +1,45 @@
+require 'test_helper'
+
+module Elasticsearch
+  module Test
+    class IndicesFlushTest < ::Test::Unit::TestCase
+
+      context "Indices: Flush" do
+        subject { FakeClient.new(nil) }
+
+        should "perform correct request" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal 'POST', method
+            assert_equal '_flush', url
+            assert_equal Hash.new, params
+            assert_nil   body
+            true
+          end.returns(FakeResponse.new)
+
+          subject.indices.flush
+        end
+
+        should "perform request against multiple indices" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal 'foo,bar/_flush', url
+            true
+          end.returns(FakeResponse.new)
+
+          subject.indices.flush :index => ['foo','bar']
+        end
+
+        should "pass the URL parameters" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal 'foo/_flush', url
+            assert_equal true, params[:refresh]
+            true
+          end.returns(FakeResponse.new)
+
+          subject.indices.flush :index => 'foo', :refresh => true
+        end
+
+      end
+
+    end
+  end
+end
