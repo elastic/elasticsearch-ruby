17a58ef779bc27cbb78ce6beed640e1857ddeea8
Karel Minarik <karel.minarik@elasticsearch.org>
2013-06-19 11:19:11 +0200

    [API] Added the "Create Index" API

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/indices/create.rb
@@ -0,0 +1,83 @@
+module Elasticsearch
+  module API
+    module Indices
+      module Actions
+
+        # Create an index.
+        #
+        # Pass the index `settings` and `mappings` in the `:body` attribute.
+        #
+        # @example Create an index with specific settings, custom analyzers and mappings
+        #
+        #     client.indices.create index: 'test',
+        #                           body: {
+        #                             settings: {
+        #                               index: {
+        #                                 number_of_shards: 1,
+        #                                 number_of_replicas: 0,
+        #                                 'routing.allocation.include.name' => 'node-1'
+        #                               },
+        #                               analysis: {
+        #                                 filter: {
+        #                                   ngram: {
+        #                                     type: 'nGram',
+        #                                     min_gram: 3,
+        #                                     max_gram: 25
+        #                                   }
+        #                                 },
+        #                                 analyzer: {
+        #                                   ngram: {
+        #                                     tokenizer: 'whitespace',
+        #                                     filter: ['lowercase', 'stop', 'ngram'],
+        #                                     type: 'custom'
+        #                                   },
+        #                                   ngram_search: {
+        #                                     tokenizer: 'whitespace',
+        #                                     filter: ['lowercase', 'stop'],
+        #                                     type: 'custom'
+        #                                   }
+        #                                 }
+        #                               }
+        #                             },
+        #                             mappings: {
+        #                               document: {
+        #                                 properties: {
+        #                                   title: {
+        #                                     type: 'multi_field',
+        #                                     fields: {
+        #                                         title:  { type: 'string', analyzer: 'snowball' },
+        #                                         exact:  { type: 'string', analyzer: 'keyword' },
+        #                                         ngram:  { type: 'string',
+        #                                                   index_analyzer: 'ngram',
+        #                                                   search_analyzer: 'ngram_search'
+        #                                         }
+        #                                     }
+        #                                   }
+        #                                 }
+        #                               }
+        #                             }
+        #                           }
+        #
+        # @option arguments [String] :index The name of the index (*Required*)
+        # @option arguments [Hash] :body Optional configuration for the index (`settings` and `mappings`)
+        # @option arguments [Time] :timeout Explicit operation timeout
+        #
+        # @see http://www.elasticsearch.org/guide/reference/api/admin-indices-create-index/
+        #
+        def create(arguments={})
+          raise ArgumentError, "Required argument 'index' missing" unless arguments[:index]
+          method = 'PUT'
+          path   = "#{arguments[:index]}"
+          params = arguments.select do |k,v|
+            [ :timeout ].include?(k)
+          end
+          # Normalize Ruby 1.8 and Ruby 1.9 Hash#select behaviour
+          params = Hash[params] unless params.is_a?(Hash)
+          body   = arguments[:body]
+
+          perform_request(method, path, params, body).body
+        end
+      end
+    end
+  end
+end
---
+++ elasticsearch-api/test/unit/indices/create_test.rb
@@ -0,0 +1,42 @@
+require 'test_helper'
+
+module Elasticsearch
+  module Test
+    class IndicesCreateTest < ::Test::Unit::TestCase
+
+      context "Indices: Create" do
+        subject { FakeClient.new(nil) }
+
+        should "require the :index argument" do
+          assert_raise ArgumentError do
+            subject.indices.create
+          end
+        end
+
+        should "perform correct request" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal 'PUT', method
+            assert_equal 'foo', url
+            assert_equal Hash.new, params
+            assert_nil   body
+            true
+          end.returns(FakeResponse.new)
+
+          subject.indices.create :index => 'foo'
+        end
+
+        should "pass the URL parameters" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal 'foo', url
+            assert_equal '1s', params[:timeout]
+            true
+          end.returns(FakeResponse.new)
+
+          subject.indices.create :index => 'foo', :timeout => '1s'
+        end
+
+      end
+
+    end
+  end
+end
