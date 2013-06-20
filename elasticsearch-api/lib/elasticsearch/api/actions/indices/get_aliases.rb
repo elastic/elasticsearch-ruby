8f797130b5e50cda2988ba63c992c53f9114d90b
Karel Minarik <karel.minarik@elasticsearch.org>
2013-06-20 10:14:59 +0200

    [API] Added the "Get Aliases" API

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/indices/get_aliases.rb
@@ -0,0 +1,32 @@
+module Elasticsearch
+  module API
+    module Indices
+      module Actions
+
+        # Get a list of all aliases, or aliases for a specific index.
+        #
+        # @example Get a list of all aliases
+        #
+        #     client.indices.get_aliases
+        #
+        # @option arguments [List] :index A comma-separated list of index names to filter aliases
+        # @option arguments [Time] :timeout Explicit timestamp for the document
+        #
+        # @see http://www.elasticsearch.org/guide/reference/api/admin-indices-aliases/
+        #
+        def get_aliases(arguments={})
+          method = 'GET'
+          path   = Utils.__pathify( Utils.__listify(arguments[:index]), '_aliases' )
+          params = arguments.select do |k,v|
+            [ :timeout ].include?(k)
+          end
+          # Normalize Ruby 1.8 and Ruby 1.9 Hash#select behaviour
+          params = Hash[params] unless params.is_a?(Hash)
+          body   = nil
+
+          perform_request(method, path, params, body).body
+        end
+      end
+    end
+  end
+end
---
+++ elasticsearch-api/test/unit/indices/get_aliases_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+module Elasticsearch
+  module Test
+    class IndicesGetAliasesTest < ::Test::Unit::TestCase
+
+      context "Indices: Get aliases" do
+        subject { FakeClient.new(nil) }
+
+        should "perform correct request" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal 'GET', method
+            assert_equal '_aliases', url
+            assert_equal Hash.new, params
+            assert_nil   body
+            true
+          end.returns(FakeResponse.new)
+
+          subject.indices.get_aliases
+        end
+
+        should "perform request against an index" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal 'foo/_aliases', url
+            true
+          end.returns(FakeResponse.new)
+
+          subject.indices.get_aliases :index => 'foo'
+        end
+
+      end
+
+    end
+  end
+end
