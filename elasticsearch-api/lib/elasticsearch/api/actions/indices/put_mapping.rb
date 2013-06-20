c4c6410fd2e7bc9a5fe7d620395d87f59d1a74c4
Karel Minarik <karel.minarik@elasticsearch.org>
2013-06-20 17:19:50 +0200

    [API] Added the "Put Mapping" API

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/indices/put_mapping.rb
@@ -0,0 +1,49 @@
+module Elasticsearch
+  module API
+    module Indices
+      module Actions
+
+        # Create or update mapping.
+        #
+        # Pass the mapping definition(s) in the `:body` argument.
+        #
+        # @example Create or update a mapping for a specific document type
+        #
+        #     client.indices.put_mapping index: 'myindex', type: 'mytype', body: {
+        #       mytype: {
+        #         properties: {
+        #           title: { type: 'string', analyzer: 'snowball' }
+        #         }
+        #       }
+        #     }
+        #
+        # @option arguments [Hash] :body The mapping definition (*Required*)
+        # @option arguments [List] :index A comma-separated list of index names; use `_all` to perform the operation
+        #                                 on all indices (*Required*)
+        # @option arguments [String] :type The name of the document type (*Required*)
+        # @option arguments [Boolean] :ignore_conflicts Specify whether to ignore conflicts while updating the mapping
+        #                                               (default: false)
+        # @option arguments [Time] :timeout Explicit operation timeout
+        #
+        # @see http://www.elasticsearch.org/guide/reference/api/admin-indices-put-mapping/
+        #
+        def put_mapping(arguments={})
+          raise ArgumentError, "Required argument 'index' missing" unless arguments[:index]
+          raise ArgumentError, "Required argument 'type' missing"  unless arguments[:type]
+          raise ArgumentError, "Required argument 'body' missing"  unless arguments[:body]
+          method = 'PUT'
+          path   = Utils.__pathify( Utils.__listify(arguments[:index]), arguments[:type], '_mapping' )
+          params = arguments.select do |k,v|
+            [ :ignore_conflicts,
+              :timeout ].include?(k)
+          end
+          # Normalize Ruby 1.8 and Ruby 1.9 Hash#select behaviour
+          params = Hash[params] unless params.is_a?(Hash)
+          body   = arguments[:body]
+
+          perform_request(method, path, params, body).body
+        end
+      end
+    end
+  end
+end
---
+++ elasticsearch-api/test/unit/indices/put_mapping_test.rb
@@ -0,0 +1,57 @@
+require 'test_helper'
+
+module Elasticsearch
+  module Test
+    class IndicesPutMappingTest < ::Test::Unit::TestCase
+
+      context "Indices: Put mapping" do
+        subject { FakeClient.new(nil) }
+
+        should "require the :index argument" do
+          assert_raise ArgumentError do
+            subject.indices.put_mapping :type => 'bar'
+          end
+        end
+
+        should "require the :type argument" do
+          assert_raise ArgumentError do
+            subject.indices.put_mapping :index => 'foo'
+          end
+        end
+
+        should "perform correct request" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal 'PUT', method
+            assert_equal 'foo/bar/_mapping', url
+            assert_equal Hash.new, params
+            assert_equal({ :foo => {} }, body)
+            true
+          end.returns(FakeResponse.new)
+
+          subject.indices.put_mapping :index => 'foo', :type => 'bar', :body => { :foo => {} }
+        end
+
+        should "perform request against multiple indices" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal 'foo,bar/bam/_mapping', url
+            true
+          end.returns(FakeResponse.new)
+
+          subject.indices.put_mapping :index => ['foo','bar'], :type => 'bam', :body => {}
+        end
+
+        should "pass the URL parameters" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal 'foo/bar/_mapping', url
+            assert_equal true, params[:ignore_conflicts]
+            true
+          end.returns(FakeResponse.new)
+
+          subject.indices.put_mapping :index => 'foo', :type => 'bar', :body => {}, :ignore_conflicts => true
+        end
+
+      end
+
+    end
+  end
+end
