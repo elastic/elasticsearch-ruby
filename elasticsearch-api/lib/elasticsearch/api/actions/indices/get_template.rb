ce6f310986fcc438fd31260e94dba6b8d1402c0b
Karel Minarik <karel.minarik@elasticsearch.org>
2013-06-20 10:55:13 +0200

    [API] Added the "Get Template" API

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/indices/get_template.rb
@@ -0,0 +1,26 @@
+module Elasticsearch
+  module API
+    module Indices
+      module Actions
+
+        # Get a single index template.
+        #
+        # @note Use the {Cluster::Actions#state} API to get a list of all templates.
+        #
+        # @option arguments [String] :name The name of the template (*Required*)
+        #
+        # @see http://www.elasticsearch.org/guide/reference/api/admin-indices-templates/
+        #
+        def get_template(arguments={})
+          raise ArgumentError, "Required argument 'name' missing" unless arguments[:name]
+          method = 'GET'
+          path   = "_template/#{arguments[:name]}"
+          params = {}
+          body = nil
+
+          perform_request(method, path, params, body).body
+        end
+      end
+    end
+  end
+end
---
+++ elasticsearch-api/test/unit/indices/get_template_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+module Elasticsearch
+  module Test
+    class IndicesGetTemplateTest < ::Test::Unit::TestCase
+
+      context "Indices: Get template" do
+        subject { FakeClient.new(nil) }
+
+        should "require the :name argument" do
+          assert_raise ArgumentError do
+            subject.indices.get_template
+          end
+        end
+
+        should "perform correct request" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal 'GET', method
+            assert_equal '_template/foo', url
+            assert_equal Hash.new, params
+            assert_nil   body
+            true
+          end.returns(FakeResponse.new)
+
+          subject.indices.get_template :name => 'foo'
+        end
+
+      end
+
+    end
+  end
+end
