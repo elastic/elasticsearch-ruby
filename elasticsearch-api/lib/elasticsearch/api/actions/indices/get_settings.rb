b32577dbf69977d6a635f6f9af86787b5b264d9b
Karel Minarik <karel.minarik@elasticsearch.org>
2013-06-20 10:44:56 +0200

    [API] Added the "Get Settings" API

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/indices/get_settings.rb
@@ -0,0 +1,24 @@
+module Elasticsearch
+  module API
+    module Indices
+      module Actions
+
+        # Return the settings for all indices, or a list of indices.
+        #
+        # @option arguments [List] :index A comma-separated list of index names; use `_all` or empty string
+        #                                 to perform the operation on all indices
+        #
+        # @see http://www.elasticsearch.org/guide/reference/api/admin-indices-get-settings/
+        #
+        def get_settings(arguments={})
+          method = 'GET'
+          path   = Utils.__pathify( Utils.__listify(arguments[:index]), Utils.__listify(arguments[:type]), '_settings' )
+          params = {}
+          body   = nil
+
+          perform_request(method, path, params, body).body
+        end
+      end
+    end
+  end
+end
---
+++ elasticsearch-api/test/unit/indices/get_settings_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+module Elasticsearch
+  module Test
+    class IndicesGetSettingsTest < ::Test::Unit::TestCase
+
+      context "Indices: Get settings" do
+        subject { FakeClient.new(nil) }
+
+        should "perform correct request" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal 'GET', method
+            assert_equal '_settings', url
+            assert_equal Hash.new, params
+            assert_nil   body
+            true
+          end.returns(FakeResponse.new)
+
+          subject.indices.get_settings
+        end
+
+        should "perform request against an index" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal 'foo/_settings', url
+            true
+          end.returns(FakeResponse.new)
+
+          subject.indices.get_settings :index => 'foo'
+        end
+
+      end
+
+    end
+  end
+end
