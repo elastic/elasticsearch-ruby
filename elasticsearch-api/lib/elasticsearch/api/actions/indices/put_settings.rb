c6976dda371d037e0c8e509eb72438edf12fedbb
Karel Minarik <karel.minarik@elasticsearch.org>
2013-06-21 10:44:36 +0200

    [API] Added the "Put Settings" API

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/indices/put_settings.rb
@@ -0,0 +1,45 @@
+module Elasticsearch
+  module API
+    module Indices
+      module Actions
+
+        # Update the settings for one or multiple indices.
+        #
+        # @example Change the number of replicas for all indices
+        #
+        #     client.indices.put_settings body: { index: { number_of_replicas: 0 } }
+        #
+        #
+        # @example Change the number of replicas for a specific index
+        #
+        #     client.indices.put_settings index: 'myindex', body: { index: { number_of_replicas: 0 } }
+        #
+        #
+        # @example Disable "flush" for all indices
+        #
+        #     client.indices.put_settings body: { 'index.translog.disable_flush' => true }
+        #
+        # @example Allocate specific index on specific nodes
+        #
+        #     client.indices.put_settings index: 'my-big-index',
+        #                                 body: { 'index.routing.allocation.require.tag' => 'bigbox' }
+        #
+        # @option arguments [Hash] :body The index settings to be updated (*Required*)
+        # @option arguments [List] :index A comma-separated list of index names; use `_all` or empty string
+        #                                to perform the operation on all indices
+        #
+        # @see http://www.elasticsearch.org/guide/reference/api/admin-indices-update-settings/
+        #
+        def put_settings(arguments={})
+          raise ArgumentError, "Required argument 'body' missing" unless arguments[:body]
+          method = 'PUT'
+          path   = Utils.__pathify( Utils.__listify(arguments[:index]), '_settings' )
+          params = {}
+          body   = arguments[:body]
+
+          perform_request(method, path, params, body).body
+        end
+      end
+    end
+  end
+end
---
+++ elasticsearch-api/test/unit/indices/put_settings_test.rb
@@ -0,0 +1,50 @@
+require 'test_helper'
+
+module Elasticsearch
+  module Test
+    class IndicesPutSettingsTest < ::Test::Unit::TestCase
+
+      context "Indices: Put settings" do
+        subject { FakeClient.new(nil) }
+
+        should "require the :body argument" do
+          assert_raise ArgumentError do
+            subject.indices.put_settings
+          end
+        end
+
+        should "perform correct request" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal 'PUT', method
+            assert_equal '_settings', url
+            assert_equal Hash.new, params
+            assert_equal Hash.new, body
+            true
+          end.returns(FakeResponse.new)
+
+          subject.indices.put_settings :body => {}
+        end
+
+        should "perform request against a specific indices" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal 'foo/_settings', url
+            true
+          end.returns(FakeResponse.new)
+
+          subject.indices.put_settings :index => 'foo', :body => {}
+        end
+
+        should "perform request against multiple indices" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal 'foo,bar/_settings', url
+            true
+          end.returns(FakeResponse.new)
+
+          subject.indices.put_settings :index => ['foo','bar'], :body => {}
+        end
+
+      end
+
+    end
+  end
+end
