5f9e407f214dbf78509e03bcf6067fd172f84e27
Karel Minarik <karel.minarik@elasticsearch.org>
2013-06-21 13:32:07 +0200

    [API] Added the "Snapshot Index" API

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/indices/snapshot_index.rb
@@ -0,0 +1,32 @@
+module Elasticsearch
+  module API
+    module Indices
+      module Actions
+
+        # When using the shared storage gateway, manually trigger the snapshot operation.
+        #
+        # @deprecated The shared gateway has been deprecated [https://github.com/elasticsearch/elasticsearch/issues/2458]
+        #
+        # @option arguments [List] :index A comma-separated list of index names; use `_all` or empty string
+        #                                to perform the operation on all indices
+        # @option arguments [String] :ignore_indices When performed on multiple indices, allows to ignore `missing` ones
+        #                                            (options: none, missing)
+        #
+        # @see http://www.elasticsearch.org/guide/reference/api/admin-indices-gateway-snapshot/
+        #
+        def snapshot_index(arguments={})
+          method = 'POST'
+          path   = Utils.__pathify( Utils.__listify(arguments[:index]), '_gateway/snapshot' )
+          params = arguments.select do |k,v|
+            [ :ignore_indices ].include?(k)
+          end
+          # Normalize Ruby 1.8 and Ruby 1.9 Hash#select behaviour
+          params = Hash[params] unless params.is_a?(Hash)
+          body   = nil
+
+          perform_request(method, path, params, body).body
+        end
+      end
+    end
+  end
+end
---
+++ elasticsearch-api/test/unit/indices/snapshot_index_test.rb
@@ -0,0 +1,55 @@
+require 'test_helper'
+
+module Elasticsearch
+  module Test
+    class IndicesSnapshotIndexTest < ::Test::Unit::TestCase
+
+      context "Indices: Snapshot index" do
+        subject { FakeClient.new(nil) }
+
+        should "perform correct request" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal 'POST', method
+            assert_equal '_gateway/snapshot', url
+            assert_equal Hash.new, params
+            assert_nil   body
+            true
+          end.returns(FakeResponse.new)
+
+          subject.indices.snapshot_index
+        end
+
+        should "perform request against an index" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal 'foo/_gateway/snapshot', url
+            true
+          end.returns(FakeResponse.new)
+
+          subject.indices.snapshot_index :index => 'foo'
+        end
+
+        should "perform request against multiple indices" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal 'foo,bar/_gateway/snapshot', url
+            true
+          end.returns(FakeResponse.new).twice
+
+          subject.indices.snapshot_index :index => ['foo','bar']
+          subject.indices.snapshot_index :index => 'foo,bar'
+        end
+
+        should "pass the URL parameters" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal 'foo,bar/_gateway/snapshot', url
+            assert_equal 'missing', params[:ignore_indices]
+            true
+          end.returns(FakeResponse.new)
+
+          subject.indices.snapshot_index :index => ['foo','bar'], :ignore_indices => 'missing'
+        end
+
+      end
+
+    end
+  end
+end
