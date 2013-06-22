50bcaee701b13565e0a8ac4d11a1832fd3303af6
Karel Minarik <karel.minarik@elasticsearch.org>
2013-06-22 23:09:29 +0200

    [API] Added the "Scroll" API

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/scroll.rb
@@ -0,0 +1,44 @@
+module Elasticsearch
+  module API
+    module Actions
+
+      # Efficiently iterate over a large result set.
+      #
+      # When using `from` and `size` to return a large result sets, performance drops as you "paginate" in the set,
+      # and you can't guarantee the consistency when the index is being updated at the same time.
+      #
+      # "Scrolling" the results is frequently used with the `scan` search type.
+      #
+      # @example Scroll results
+      #
+      #     result = client.search index: 'scrollindex',
+      #                            scroll: '5m',
+      #                            body: { query: { match: { title: 'test' } }, sort: '_id' }
+      #
+      #     client.scroll scroll: '5m', scroll_id: result['_scroll_id']
+      #
+      # @option arguments [String] :scroll_id The scroll ID
+      # @option arguments [Hash] :body The scroll ID if not passed by URL or query parameter.
+      # @option arguments [Duration] :scroll Specify how long a consistent view of the index
+      #                                      should be maintained for scrolled search
+      # @option arguments [String] :scroll_id The scroll ID for scrolled search
+      #
+      # @see http://www.elasticsearch.org/guide/reference/api/search/scroll/
+      # @see http://www.elasticsearch.org/guide/reference/api/search/search-type/
+      #
+      def scroll(arguments={})
+        method = 'GET'
+        path   = "_search/scroll"
+        params = arguments.select do |k,v|
+          [ :scroll,
+            :scroll_id ].include?(k)
+        end
+        # Normalize Ruby 1.8 and Ruby 1.9 Hash#select behaviour
+        params = Hash[params] unless params.is_a?(Hash)
+        body   = arguments[:body]
+
+        perform_request(method, path, params, body).body
+      end
+    end
+  end
+end
