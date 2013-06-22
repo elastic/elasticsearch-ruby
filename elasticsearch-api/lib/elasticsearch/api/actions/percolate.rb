ca342466ec5ca15cded8f40f7386fbca9c176049
Karel Minarik <karel.minarik@elasticsearch.org>
2013-06-22 12:06:31 +0200

    [API] Added the "Percolate" API

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/percolate.rb
@@ -0,0 +1,57 @@
+module Elasticsearch
+  module API
+    module Actions
+
+      # Return names of queries matching the passed document.
+      #
+      # Percolator allows you to register queries and then evaluate a document against them:
+      # the IDs of matching queries are returned in the response.
+      #
+      # @example Register queries named "alert-1" and "alert-2" for the "myindex" index
+      #
+      #     client.index index: '_percolator',
+      #                  type: 'myindex',
+      #                  id: 'alert-1',
+      #                  body: { query: { query_string: { query: 'foo' } } }
+      #
+      #     client.index index: '_percolator',
+      #                  type: 'myindex',
+      #                  id: 'alert-2',
+      #                  body: { query: { query_string: { query: 'bar' } } }
+      #
+      # @example Evaluate a document against the queries
+      #
+      #     client.percolate index: 'myindex', body: { doc: { title: "Foo" } }
+      #     # => {"ok":true,"matches":["alert-1"]}
+      #
+      #     client.percolate index: 'myindex', body: { doc: { title: "Foo Bar" } }
+      #     # => {"ok":true,"matches":["alert-1","alert-2"]}
+      #
+      # @option arguments [String] :index The name of the index with a registered percolator query (*Required*)
+      # @option arguments [String] :type The document type
+      # @option arguments [Hash] :body The document (`doc`) to percolate against registered queries;
+      #                                optionally also a `query` to limit the percolation to specific registered queries
+      # @option arguments [Boolean] :prefer_local With `true`, specify that a local shard should be used if available,
+      #                                           with `false`, use a random shard (default: true)
+      #
+      # @see http://elasticsearch.org/guide/reference/api/percolate/
+      #
+      def percolate(arguments={})
+        raise ArgumentError, "Required argument 'index' missing" unless arguments[:index]
+        raise ArgumentError, "Required argument 'body' missing"  unless arguments[:body]
+        arguments[:type] ||= 'document'
+
+        method = 'GET'
+        path   = "#{arguments[:index]}/#{arguments[:type]}/_percolate"
+        params = arguments.select do |k,v|
+          [ :prefer_local ].include?(k)
+        end
+        # Normalize Ruby 1.8 and Ruby 1.9 Hash#select behaviour
+        params = Hash[params] unless params.is_a?(Hash)
+        body   = arguments[:body]
+
+        perform_request(method, path, params, body).body
+      end
+    end
+  end
+end
---
+++ elasticsearch-api/test/unit/percolate_test.rb
@@ -0,0 +1,49 @@
+require 'test_helper'
+
+module Elasticsearch
+  module Test
+    class PercolateTest < ::Test::Unit::TestCase
+
+      context "Percolate" do
+        subject { FakeClient.new(nil) }
+
+        should "require the :index argument" do
+          assert_raise ArgumentError do
+            subject.percolate :type => 'bar', :body => {}
+          end
+        end
+
+        should "require the :body argument" do
+          assert_raise ArgumentError do
+            subject.percolate :index => 'bar'
+          end
+        end
+
+        should "have default document type" do
+          assert_nothing_raised do
+            subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal 'foo/document/_percolate', url
+            true
+          end.returns(FakeResponse.new)
+
+            subject.percolate :index => 'foo', :body => {}
+          end
+        end
+
+        should "perform correct request" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal 'GET', method
+            assert_equal 'foo/bar/_percolate', url
+            assert_equal Hash.new, params
+            assert_equal 'bar', body[:doc][:foo]
+            true
+          end.returns(FakeResponse.new)
+
+          subject.percolate :index => 'foo', :type => 'bar', :body => { :doc => { :foo => 'bar' } }
+        end
+
+      end
+
+    end
+  end
+end
