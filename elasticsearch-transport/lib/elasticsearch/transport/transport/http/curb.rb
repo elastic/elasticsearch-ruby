8b22af4a403f3a1f8cb02ec930a2b12fad8112fd
Jan Suchal <johno@jsmf.net>
2013-10-04 14:33:10 +0200

    [FIX] Properly set request body for GET requests when using Curb
    This fixes a serious issue when sending GET requests to Curb transport backend.
    
    Basically request body was discarded and not used at all. For example any search would yield default search - returning all docs.
    
    Closes #3

--- elasticsearch-transport/lib/elasticsearch/transport/transport/http/curb.rb
+++ elasticsearch-transport/lib/elasticsearch/transport/transport/http/curb.rb
@@ -20,14 +20,13 @@ module Elasticsearch
               connection.connection.url = url
 
               case method
-                when 'HEAD', 'GET' then connection.connection.http method.downcase.to_sym
-                when 'PUT'         then connection.connection.http_put serializer.dump(body)
-                when 'DELETE'      then connection.connection.http_delete
-                when 'POST'
-                  connection.connection.post_body = __convert_to_json(body) if body
-                  connection.connection.http_post
+                when 'HEAD'
+                when 'GET', 'POST', 'PUT', 'DELETE'
+                  connection.connection.put_data = __convert_to_json(body) if body
                 else raise ArgumentError, "Unsupported HTTP method: #{method}"
               end
+
+              connection.connection.http(method.to_sym)
 
               Response.new connection.connection.response_code, connection.connection.body_str
             end
--- elasticsearch-transport/test/unit/transport_curb_test.rb
+++ elasticsearch-transport/test/unit/transport_curb_test.rb
@@ -23,23 +23,36 @@ class Elasticsearch::Transport::Transport::HTTP::FaradayTest < Test::Unit::TestC
       @transport.perform_request 'GET', '/'
     end
 
+    should "set body for GET request" do
+      @transport.connections.first.connection.expects(:put_data=).with('{"foo":"bar"}')
+      @transport.connections.first.connection.expects(:http).with(:GET).returns(stub_everything)
+      @transport.perform_request 'GET', '/', {}, '{"foo":"bar"}'
+    end
+
+    should "set body for PUT request" do
+      @transport.connections.first.connection.expects(:put_data=)
+      @transport.connections.first.connection.expects(:http).with(:PUT).returns(stub_everything)
+      @transport.perform_request 'PUT', '/', {}, {:foo => 'bar'}
+    end
+
     should "serialize the request body" do
-      @transport.connections.first.connection.expects(:http_post).returns(stub_everything)
+      @transport.connections.first.connection.expects(:http).with(:POST).returns(stub_everything)
       @transport.serializer.expects(:dump)
       @transport.perform_request 'POST', '/', {}, {:foo => 'bar'}
     end
 
     should "not serialize a String request body" do
-      @transport.connections.first.connection.expects(:http_post).returns(stub_everything)
+      @transport.connections.first.connection.expects(:http).with(:POST).returns(stub_everything)
       @transport.serializer.expects(:dump).never
       @transport.perform_request 'POST', '/', {}, '{"foo":"bar"}'
     end
 
     should "handle HTTP methods" do
-      @transport.connections.first.connection.expects(:http).twice.returns(stub_everything)
-      @transport.connections.first.connection.expects(:http_put).returns(stub_everything)
-      @transport.connections.first.connection.expects(:http_post).returns(stub_everything)
-      @transport.connections.first.connection.expects(:http_delete).returns(stub_everything)
+      @transport.connections.first.connection.expects(:http).with(:HEAD).returns(stub_everything)
+      @transport.connections.first.connection.expects(:http).with(:GET).returns(stub_everything)
+      @transport.connections.first.connection.expects(:http).with(:PUT).returns(stub_everything)
+      @transport.connections.first.connection.expects(:http).with(:POST).returns(stub_everything)
+      @transport.connections.first.connection.expects(:http).with(:DELETE).returns(stub_everything)
 
       %w| HEAD GET PUT POST DELETE |.each { |method| @transport.perform_request method, '/' }
 
