7235b644b1929dbcdc83a07a6dae846abc5c47bb
Karel Minarik <karel.minarik@elasticsearch.org>
2014-01-06 18:19:47 +0100

    [API] Added documentation for the internal `force` and `full` parameters in the "Indices Flush" API

--- elasticsearch-api/lib/elasticsearch/api/actions/indices/flush.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/indices/flush.rb
@@ -11,8 +11,10 @@ module Elasticsearch
         # @note The flush operation is handled automatically by Elasticsearch, you don't need to perform it manually.
         #
         # @option arguments [List] :index A comma-separated list of index names; use `_all` or empty string for all indices
-        # @option arguments [Boolean] :force TODO: ?
-        # @option arguments [Boolean] :full TODO: ?
+        # @option arguments [Boolean] :force Whether a flush should be forced even if it is not necessarily needed ie.
+        #                                    if no changes will be committed to the index. (Internal)
+        # @option arguments [Boolean] :full If set to true a new index writer is created and settings that have been
+        #                                   changed related to the index writer will be refreshed. (Internal)
         # @option arguments [Boolean] :allow_no_indices Whether to ignore if a wildcard indices expression resolves into
         #                                               no concrete indices. (This includes `_all` string or when no
         #                                               indices have been specified)
