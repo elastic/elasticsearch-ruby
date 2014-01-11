45893d66d18f4f14e023d00c340c2ccf8c8a0df1
Karel Minarik <karel.minarik@elasticsearch.org>
2014-01-11 21:49:44 +0100

    Release 0.4.7

--- elasticsearch-api/lib/elasticsearch/api/version.rb
+++ elasticsearch-api/lib/elasticsearch/api/version.rb
@@ -1,5 +1,5 @@
 module Elasticsearch
   module API
-    VERSION = "0.4.6"
+    VERSION = "0.4.7"
   end
 end
--- elasticsearch-transport/lib/elasticsearch/transport/version.rb
+++ elasticsearch-transport/lib/elasticsearch/transport/version.rb
@@ -1,5 +1,5 @@
 module Elasticsearch
   module Transport
-    VERSION = "0.4.6"
+    VERSION = "0.4.7"
   end
 end
