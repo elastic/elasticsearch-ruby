9977a712a76b5630637350a4779c03e55bb1b39c
Karel Minarik <karel.minarik@elasticsearch.org>
2014-01-12 14:25:32 +0100

    [API] Added the `flat_settings` parameter to "Get Cluster Settings" API
    Related: elasticsearch/elasticsearch#4140, elasticsearch/elasticsearch#4497

--- elasticsearch-api/lib/elasticsearch/api/actions/cluster/get_settings.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/cluster/get_settings.rb
@@ -5,12 +5,22 @@ module Elasticsearch
 
         # Get the cluster settings (previously set with {Cluster::Actions#put_settings})
         #
+        # @example Get cluster settings
+        #
+        #     client.cluster.get_settings
+        #
+        # @option arguments [Boolean] :flat_settings Return settings in flat format (default: false)
+        #
         # @see http://elasticsearch.org/guide/reference/api/admin-cluster-update-settings/
         #
         def get_settings(arguments={})
+          valid_params = [
+            :flat_settings
+          ]
+
           method = 'GET'
           path   = "_cluster/settings"
-          params = {}
+          params = Utils.__validate_and_extract_params arguments, valid_params
           body   = nil
 
           perform_request(method, path, params, body).body
