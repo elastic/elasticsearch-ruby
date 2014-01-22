f9994bb12802ef8f89753e21b706ce9f8797c3bd
Karel Minarik <karel.minarik@elasticsearch.com>
2014-01-22 16:33:03 +0100

    [API] [1.0] Updated the "Get Index Aliases" API to the 1.0 changes
    Related: elasticsearch/elasticsearch#4743
    
    See: http://www.elasticsearch.org/guide/en/elasticsearch/reference/master/_indices_apis.html

--- elasticsearch-api/test/unit/indices/get_aliases_test.rb
+++ elasticsearch-api/test/unit/indices/get_aliases_test.rb
@@ -28,6 +28,16 @@ module Elasticsearch
           subject.indices.get_aliases :index => 'foo'
         end
 
+        should "get a specific alias" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal 'foo/_aliases/bar', url
+            assert_equal Hash.new, params
+            true
+          end.returns(FakeResponse.new)
+
+          subject.indices.get_aliases :index => 'foo', :name => 'bar'
+        end
+
         should "URL-escape the parts" do
           subject.expects(:perform_request).with do |method, url, params, body|
             assert_equal 'foo%5Ebar/_aliases', url
