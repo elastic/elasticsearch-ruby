593e02348f6a03e4c3ef30a738cf45750d0ef077
Karel Minarik <karel.minarik@elasticsearch.com>
2014-01-24 21:46:10 +0100

    [EXT] Use the 1.0 compatible URL when getting `_nodes` info in the testing cluster

--- elasticsearch-extensions/lib/elasticsearch/extensions/test/cluster.rb
+++ elasticsearch-extensions/lib/elasticsearch/extensions/test/cluster.rb
@@ -241,7 +241,7 @@ module Elasticsearch
         #
         def __print_cluster_info(port)
           health = JSON.parse(Net::HTTP.get(URI("http://localhost:#{port}/_cluster/health")))
-          nodes  = JSON.parse(Net::HTTP.get(URI("http://localhost:#{port}/_nodes/?process&http")))
+          nodes  = JSON.parse(Net::HTTP.get(URI("http://localhost:#{port}/_nodes/process,http")))
           master = JSON.parse(Net::HTTP.get(URI("http://localhost:#{port}/_cluster/state")))['master_node']
 
           puts "\n",
