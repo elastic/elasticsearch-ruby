64c4734da668143656e1a403cf51839ccc59738e
Karel Minarik <karel.minarik@elasticsearch.com>
2014-02-11 08:56:45 +0100

    [API] Added the `thread_pool` method for the Cat API

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/cat/thread_pool.rb
@@ -0,0 +1,52 @@
+module Elasticsearch
+  module API
+    module Cat
+      module Actions
+
+        # Display thread pool statistics across nodes (use the `help` parameter to display a list
+        # of avaialable thread pools)
+        #
+        # @example Display information about all thread pools across nodes
+        #
+        #     puts client.cat.thread_pool
+        #
+        # @example Display information about the indexing thread pool
+        #
+        #     puts client.cat.thread_pool h: %w(h ip index.active index.size index.queue index.rejected), v: true
+        #
+        # @example Display information about the indexing and search threads, using the short names
+        #
+        #     puts client.cat.thread_pool h: 'host,ia,is,iq,ir,sa,ss,sq,sr', v: true
+        #
+        # @option arguments [Boolean] :full_id Display the complete node ID
+        # @option arguments [List] :h Comma-separated list of column names to display -- see the `help` argument
+        # @option arguments [Boolean] :v Display column headers as part of the output
+        # @option arguments [String] :format The output format. Options: 'text', 'json'; default: 'text'
+        # @option arguments [Boolean] :help Return information about headers
+        # @option arguments [Boolean] :local Return local information, do not retrieve the state from master node
+        #                                    (default: false)
+        # @option arguments [Time] :master_timeout Explicit operation timeout for connection to master node
+        #
+        # @see http://www.elasticsearch.org/guide/en/elasticsearch/reference/master/cat-thread-pool.html
+        #
+        def thread_pool(arguments={})
+          valid_params = [
+            :full_id,
+            :local,
+            :master_timeout,
+            :h,
+            :help,
+            :v ]
+
+          method = 'GET'
+          path   = "_cat/thread_pool"
+          params = Utils.__validate_and_extract_params arguments, valid_params
+          params[:h] = Utils.__listify(params[:h]) if params[:h]
+          body   = nil
+
+          perform_request(method, path, params, body).body
+        end
+      end
+    end
+  end
+end
