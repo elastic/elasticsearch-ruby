0557aeb5c66ae6724104ef28857a829542a1f36c
Karel Minarik <karel.minarik@elasticsearch.com>
2014-03-05 15:04:43 +0100

    [EXAMPLE] Added example for RabbitMQ consumer/publisher
    Using Bunny (the synchronous client)
    
    See: http://rubybunny.info/articles/getting_started.html

---
+++ examples/rabbitmq/consumer-publisher.rb
@@ -0,0 +1,47 @@
+# Example of listening to a RabbitMQ queue and indexing the payload
+#
+# Usage:
+#
+#     $ bundle install
+#     $ bundle exec ruby consume-publish.rb
+#
+
+require 'multi_json'
+require 'oj'
+
+require 'elasticsearch'
+
+require 'bunny'
+
+connection = Bunny.new
+
+connection.start
+
+channel  = connection.create_channel
+queue    = channel.queue 'examples.elasticsearch', auto_delete: true
+exchange = channel.default_exchange
+
+elasticsearch = Elasticsearch::Client.new log:true
+
+elasticsearch.indices.delete index: 'rabbit' rescue nil
+
+queue.subscribe do |delivery_info, metadata, payload|
+  hash = MultiJson.load(payload)
+  elasticsearch.index index: 'rabbit', type: 'event', id: hash.delete(:id), body: hash
+end
+
+(1..10).each do |i|
+  exchange.publish MultiJson.dump({id: i, title: "Test #{i}"}), routing_key: queue.name
+end
+
+sleep 1.0
+
+puts "Enter some words to index (use Ctrl+C to exit):"
+
+[:INT, :TERM].each do |signal| trap(signal) { puts "\nExiting..."; exit } end
+
+while input = gets
+  exchange.publish MultiJson.dump({title: input.chomp}), routing_key: queue.name unless input =~ /^\s*$/
+end
+
+connection.close
