dadc5db3d617871ec5b06cbff573db200e2b1496
Karel Minarik <karel.minarik@elasticsearch.com>
2014-04-02 10:20:09 +0200

    [API] Improved the documentation for the "scroll" API
    Closes #49

--- elasticsearch-api/lib/elasticsearch/api/actions/scroll.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/scroll.rb
@@ -9,13 +9,30 @@ module Elasticsearch
       #
       # "Scrolling" the results is frequently used with the `scan` search type.
       #
-      # @example Scroll results
+      # @example A basic example
       #
       #     result = client.search index: 'scrollindex',
       #                            scroll: '5m',
       #                            body: { query: { match: { title: 'test' } }, sort: '_id' }
       #
-      #     client.scroll scroll: '5m', scroll_id: result['_scroll_id']
+      #     result = client.scroll scroll: '5m', scroll_id: result['_scroll_id']
+      #
+      # @example Call the `scroll` API until all the documents are returned
+      #
+      #     # Index 1,000 documents
+      #     client.indices.delete index: 'test'
+      #     1_000.times do |i| client.index index: 'test', type: 'test', id: i+1, body: {title: "Test #{i}"} end
+      #     client.indices.refresh index: 'test'
+      #
+      #     # Open the "view" of the index with the `scan` search_type
+      #     r = client.search index: 'test', search_type: 'scan', scroll: '5m', size: 10
+      #
+      #     # Call the `scroll` API until empty results are returned
+      #     while r = client.scroll(scroll_id: r['_scroll_id'], scroll: '5m') and not r['hits']['hits'].empty? do
+      #       puts "--- BATCH #{defined?($i) ? $i += 1 : $i = 1} -------------------------------------------------"
+      #       puts r['hits']['hits'].map { |d| d['_source']['title'] }.inspect
+      #       puts
+      #     end
       #
       # @option arguments [String] :scroll_id The scroll ID
       # @option arguments [Hash] :body The scroll ID if not passed by URL or query parameter.
@@ -23,6 +40,7 @@ module Elasticsearch
       #                                      should be maintained for scrolled search
       # @option arguments [String] :scroll_id The scroll ID for scrolled search
       #
+      # @see http://www.elasticsearch.org/guide/en/elasticsearch/guide/current/scan-scroll.html#scan-scroll
       # @see http://www.elasticsearch.org/guide/reference/api/search/scroll/
       # @see http://www.elasticsearch.org/guide/reference/api/search/search-type/
       #
