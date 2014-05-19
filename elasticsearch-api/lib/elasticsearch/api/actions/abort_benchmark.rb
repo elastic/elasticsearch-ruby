6ab726c4a23e5c4bdece181b9c6a0f43b95a473e
Karel Minarik <karel.minarik@elasticsearch.com>
2014-05-19 12:04:36 +0200

    [API] Added the "Benchmark" API
    See: http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/search-benchmark.html
    
    Related: elasticsearch/elasticsearch#5406

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/abort_benchmark.rb
@@ -0,0 +1,27 @@
+module Elasticsearch
+  module API
+    module Actions
+
+      # Abort a running benchmark
+      #
+      # @example
+      #
+      #     client.abort_benchmark name: 'my_benchmark'
+      #
+      # @option arguments [String] :name A benchmark name
+      #
+      # @see http://www.elasticsearch.org/guide/en/elasticsearch/reference/master/search-benchmark.html
+      #
+      def abort_benchmark(arguments={})
+        valid_params = [
+           ]
+        method = 'POST'
+        path   = "_bench/abort/#{arguments[:name]}"
+        params = {}
+        body   = nil
+
+        perform_request(method, path, params, body).body
+      end
+    end
+  end
+end
