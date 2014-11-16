a65ca61dac2d9612d044c24532c3b61a4ffbb22c
Karel Minarik <karel.minarik@elasticsearch.com>
2014-11-16 19:04:46 +0100

    [DSL] Added BaseAggregationComponent with common logic for aggregation classes

---
+++ elasticsearch-dsl/lib/elasticsearch/dsl/search/base_aggregation_component.rb
@@ -0,0 +1,65 @@
+module Elasticsearch
+  module DSL
+    module Search
+
+      # Module containing common functionality for DSL classes
+      #
+      module BaseAggregationComponent
+
+        def self.included(base)
+          base.__send__ :include, BaseComponent
+          base.__send__ :include, InstanceMethods
+        end
+
+        module InstanceMethods
+
+          attr_reader :aggregations
+
+          # Looks up the corresponding class for a method being invoked, and initializes it
+          #
+          # @raise [NoMethodError] When the corresponding class cannot be found
+          #
+          def method_missing(name, *args, &block)
+            klass = name.capitalize
+            if Aggregations.const_defined? klass
+              @value = Aggregations.const_get(klass).new *args, &block
+            else
+              raise NoMethodError, "undefined method '#{name}' for #{self}"
+            end
+          end
+
+          # Adds a nested aggregation into the aggregation definition
+          #
+          # @return [self]
+          #
+          def aggregation(*args, &block)
+            @aggregations ||= {}
+            @aggregations.update args.first => Aggregation.new(*args, &block)
+            self
+          end
+
+          # Convert the aggregations to a Hash
+          #
+          # A default implementation, DSL classes can overload it.
+          #
+          # @return [Hash]
+          #
+          def to_hash(options={})
+            call
+
+            @hash = { name => @args } unless @hash && @hash[name] && ! @hash[name].empty?
+
+            if @aggregations
+              @hash[:aggregations] = {}
+              @aggregations.map { |name, value| @hash[:aggregations][name] = value.to_hash }
+            end
+            @hash
+          end
+        end
+
+      end
+
+    end
+  end
+end
+
