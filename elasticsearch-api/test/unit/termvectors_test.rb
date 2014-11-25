elastic/elasticsearch-ruby

commit 025faf2ff2c4deadfe7a5e042b66f9297d3ed624

[API] Renamed the `termvector` API to `termvectors`
Related: elasticsearch/elasticsearch@1959275622ca35997420213d979a1724944aa4f6

elasticsearch-api/lib/elasticsearch/api/actions/termvectors.rb
@@ -22,7 +22,7 @@ module Elasticsearch
       #
       #     client.index index: 'my_index', type: 'my_type', id: '1', body: { text: 'Foo Bar Fox' }
       #
-      #     client.termvector index: 'my_index', type: 'my_type', id: '1'
+      #     client.termvectors index: 'my_index', type: 'my_type', id: '1'
       #     # => { ..., "term_vectors" => { "text" => { "field_statistics" => { ... }, "terms" => { "bar" => ... } } }
       #
       # @option arguments [String] :index The name of the index (*Required*)
@@ -46,7 +46,7 @@ module Elasticsearch
       #
       # @see http://www.elasticsearch.org/guide/en/elasticsearch/reference/master/docs-termvectors.html
       #
-      def termvector(arguments={})
+      def termvectors(arguments={})
         raise ArgumentError, "Required argument 'index' missing" unless arguments[:index]
         raise ArgumentError, "Required argument 'type' missing" unless arguments[:type]
         raise ArgumentError, "Required argument 'id' missing" unless arguments[:id]
@@ -68,7 +68,7 @@ module Elasticsearch
         path   = Utils.__pathify Utils.__escape(arguments[:index]),
                                  Utils.__escape(arguments[:type]),
                                  arguments[:id],
-                                 '_termvector'
+                                 '_termvectors'
 
         params = Utils.__validate_and_extract_params arguments, valid_params
         body   = arguments[:body]

elasticsearch-api/test/unit/termvectors_test.rb
@@ -2,39 +2,39 @@ require 'test_helper'
 
 module Elasticsearch
   module Test
-    class TermvectorTest < ::Test::Unit::TestCase
+    class TermvectorsTest < ::Test::Unit::TestCase
 
-      context "Termvector" do
+      context "Termvectors" do
         subject { FakeClient.new }
 
         should "require the :index argument" do
           assert_raise ArgumentError do
-            subject.termvector :type => 'bar', :id => '1'
+            subject.termvectors :type => 'bar', :id => '1'
           end
         end
 
         should "require the :type argument" do
           assert_raise ArgumentError do
-            subject.termvector :index => 'foo', :id => '1'
+            subject.termvectors :index => 'foo', :id => '1'
           end
         end
 
         should "require the :id argument" do
           assert_raise ArgumentError do
-            subject.termvector :index => 'foo', :type => 'bar'
+            subject.termvectors :index => 'foo', :type => 'bar'
           end
         end
 
         should "perform correct request" do
           subject.expects(:perform_request).with do |method, url, params, body|
             assert_equal 'GET', method
-            assert_equal 'foo/bar/123/_termvector', url
+            assert_equal 'foo/bar/123/_termvectors', url
             assert_equal Hash.new, params
             assert_equal Hash.new, body
             true
           end.returns(FakeResponse.new)
 
-          subject.termvector :index => 'foo', :type => 'bar', :id => '123', :body => {}
+          subject.termvectors :index => 'foo', :type => 'bar', :id => '123', :body => {}
         end
 
       end

commit 5c63b3e7a85817f3b9c6c660ee11931931bf55f4

[API] Added singular version of the `termvectors` API for compability with older versions
Fixes: 025faf2

elasticsearch-api/lib/elasticsearch/api/actions/termvectors.rb
@@ -75,6 +75,12 @@ module Elasticsearch
 
         perform_request(method, path, params, body).body
       end
+
+      # @deprecated Use the plural version, {#termvectors}
+      #
+      def termvector(arguments={})
+        termvectors(arguments)
+      end
     end
   end
 end

elasticsearch-api/test/unit/termvectors_test.rb
@@ -37,6 +37,11 @@ module Elasticsearch
           subject.termvectors :index => 'foo', :type => 'bar', :id => '123', :body => {}
         end
 
+        should "be aliased to singular for older versions" do
+          subject.expects(:termvectors)
+          subject.termvector :index => 'foo', :type => 'bar', :id => '1'
+        end
+
       end
 
     end