10f35b5e11f7f340a0fcc519f1be6c0b699bf0b3
Gregory Potamianos <greg.potamianos@gmail.com>
2014-12-16 17:42:13 +0200

    [CLIENT] Added handling the `::Curl::Err::TimeoutError` exception for Curb transport
    When an Elasticsearch node is down, the client doesn't mark it as dead and the `Curl::Err::TimeoutError` is raised.
    
    With this patch, the node gets marked as dead and a custom selector can exclude it and retry with another healthy node.
    
    Closes #121

--- elasticsearch-transport/lib/elasticsearch/transport/transport/http/curb.rb
+++ elasticsearch-transport/lib/elasticsearch/transport/transport/http/curb.rb
@@ -71,7 +71,14 @@ module Elasticsearch
           # @return [Array]
           #
           def host_unreachable_exceptions
-            [::Curl::Err::HostResolutionError, ::Curl::Err::ConnectionFailedError, ::Curl::Err::GotNothingError, ::Curl::Err::RecvError, ::Curl::Err::SendError]
+            [
+              ::Curl::Err::HostResolutionError,
+              ::Curl::Err::ConnectionFailedError,
+              ::Curl::Err::GotNothingError,
+              ::Curl::Err::RecvError,
+              ::Curl::Err::SendError,
+              ::Curl::Err::TimeoutError
+            ]
           end
         end
 
