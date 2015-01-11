8fd66fea267a13e0e39e7261364895a8616b60fc
Karel Minarik <karel.minarik@elasticsearch.com>
2015-01-11 15:59:56 +0100

    [API] Added the "Cat Segments" API

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/cat/segments.rb
@@ -0,0 +1,34 @@
+module Elasticsearch
+  module API
+    module Cat
+      module Actions
+
+        # Display information about the segments in the shards of an index
+        #
+        # @example Display information for all indices
+        #
+        #     puts client.cat.segments
+        #
+        # @option arguments [List] :index A comma-separated list of index names to limit the returned information
+        # @option arguments [List] :h Comma-separated list of column names to display
+        # @option arguments [Boolean] :help Return help information
+        # @option arguments [Boolean] :v Verbose mode. Display column headers
+        #
+        # @see http://www.elasticsearch.org/guide/en/elasticsearch/reference/master/cat-segments.html
+        #
+        def segments(arguments={})
+          valid_params = [
+            :h,
+            :help,
+            :v ]
+          method = 'GET'
+          path   = "_cat/segments"
+          params = Utils.__validate_and_extract_params arguments, valid_params
+          body   = nil
+
+          perform_request(method, path, params, body).body
+        end
+      end
+    end
+  end
+end
