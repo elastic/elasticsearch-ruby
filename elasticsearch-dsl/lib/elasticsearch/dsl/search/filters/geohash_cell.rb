4129fd8be3eeff987bdaf46a9b478f3f0ae639c1
Karel Minarik <karel.minarik@elasticsearch.com>
2015-02-24 16:33:37 +0100

    [DSL] Fixed the incomplete implementation of the `geohash_cell` filter

--- elasticsearch-dsl/lib/elasticsearch/dsl/search/filters/geohash_cell.rb
+++ elasticsearch-dsl/lib/elasticsearch/dsl/search/filters/geohash_cell.rb
@@ -12,10 +12,16 @@ module Elasticsearch
         class GeohashCell
           include BaseComponent
 
-          option_method :lat
-          option_method :lon
-        end
+          option_method :precision, lambda { |*args| @hash[self.name.to_sym].update precision: args.pop }
+          option_method :lat,       lambda { |*args| @hash[self.name.to_sym][@args].update lat: args.pop }
+          option_method :lon,       lambda { |*args| @hash[self.name.to_sym][@args].update lon: args.pop }
+          option_method :neighbors, lambda { |*args| @hash[self.name.to_sym].update neighbors: args.pop }
 
+          def initialize(*args, &block)
+            super
+            @hash[self.name.to_sym] = { @args => {} } unless @args.empty?
+          end
+        end
       end
     end
   end
--- elasticsearch-dsl/test/unit/filters/geohash_cell_test.rb
+++ elasticsearch-dsl/test/unit/filters/geohash_cell_test.rb
@@ -12,18 +12,24 @@ module Elasticsearch
           should "be converted to a Hash" do
             assert_equal({ geohash_cell: {} }, subject.to_hash)
           end
-                          
+
           should "have option methods" do
             subject = GeohashCell.new :foo
-            
+
+            subject.precision 'bar'
+            subject.neighbors 'bar'
             subject.lat 'bar'
             subject.lon 'bar'
-          
+
+            assert_equal %w[ foo neighbors precision ],
+                         subject.to_hash[:geohash_cell].keys.map(&:to_s).sort
             assert_equal %w[ lat lon ],
                          subject.to_hash[:geohash_cell][:foo].keys.map(&:to_s).sort
+
+            assert_equal 'bar', subject.to_hash[:geohash_cell][:precision]
             assert_equal 'bar', subject.to_hash[:geohash_cell][:foo][:lat]
           end
-          
+
           should "take a block" do
             subject = GeohashCell.new :foo do
               lat 'bar'
