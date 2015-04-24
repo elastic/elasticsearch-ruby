9d9617df4d74796627bd8ea755843e21391e736b
Karel Minarik <karel.minarik@elasticsearch.com>
2015-04-24 13:54:29 +0200

    [DSL] Added additional option methods to the "Multi Match" query
    Related #153

--- elasticsearch-dsl/lib/elasticsearch/dsl/search/queries/multi_match.rb
+++ elasticsearch-dsl/lib/elasticsearch/dsl/search/queries/multi_match.rb
@@ -22,13 +22,21 @@ module Elasticsearch
         class MultiMatch
           include BaseComponent
 
-          option_method :query
+          option_method :analyzer
+          option_method :boost
+          option_method :cutoff_frequency
           option_method :fields
-          option_method :operator
-          option_method :type
-          option_method :slop
           option_method :fuzziness
+          option_method :max_expansions
+          option_method :minimum_should_match
+          option_method :operator
+          option_method :prefix_length
+          option_method :query
+          option_method :rewrite
+          option_method :slop
+          option_method :type
           option_method :use_dis_max
+          option_method :zero_terms_query
         end
 
       end
