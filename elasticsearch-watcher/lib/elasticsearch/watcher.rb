5e92507787e2f0383bc49165d1beedd637a8d96d
Karel Minarik <karel.minarik@elasticsearch.com>
2015-05-05 14:50:32 +0200

    [WATCHER] Added the automatic loading into API and Client when the gem is required

--- elasticsearch-watcher/lib/elasticsearch/watcher.rb
+++ elasticsearch-watcher/lib/elasticsearch/watcher.rb
@@ -3,6 +3,14 @@ require "elasticsearch/watcher/version"
 Dir[ File.expand_path('../watcher/api/actions/**/*.rb', __FILE__) ].each   { |f| require f }
 
 module Elasticsearch
+  module Watcher
+    def self.included(base)
+      base.__send__ :include, Elasticsearch::API::Watcher
+    end
+  end
+end
+
+module Elasticsearch
   module API
     module Watcher
       module Actions; end
@@ -10,7 +18,9 @@ module Elasticsearch
         # Client for the "watcher" namespace (includes the {Watcher::Actions} methods)
         #
         class WatcherClient
-          include Common::Client, Common::Client::Base, Watcher::Actions
+          include Elasticsearch::API::Common::Client,
+                  Elasticsearch::API::Common::Client::Base,
+                  Elasticsearch::API::Watcher::Actions
         end
 
         # Proxy method for {WatcherClient}, available in the receiving object
@@ -23,3 +33,5 @@ module Elasticsearch
 end
 
 Elasticsearch::API.__send__ :include, Elasticsearch::API::Watcher
+
+Elasticsearch::Transport::Client.__send__ :include, Elasticsearch::API::Watcher if defined?(Elasticsearch::Transport::Client)
