4811fc9702f5c220d93fe9d03a58c19442dfa329
Kristopher Bredemeier <kb@i22.de>
2015-10-07 18:50:41 +0200

    [DSL] Changed global aggregation to behave like a nested aggregation

--- elasticsearch-dsl/test/unit/aggregations/global_test.rb
+++ elasticsearch-dsl/test/unit/aggregations/global_test.rb
@@ -12,7 +12,25 @@ module Elasticsearch
           should "be converted to a Hash" do
             assert_equal({ global: {} }, subject.to_hash)
           end
-          
+
+          should "take a block" do
+            subject = Global.new do
+            end
+            assert_equal({global: {} }, subject.to_hash)
+          end
+
+          should "define aggregations" do
+            subject = Global.new do
+              aggregation :foo do
+                terms field: "bar"
+              end
+            end
+            expected = {
+              aggregations: { foo: { terms: { field: "bar" } } },
+              global: {}
+            }
+            assert_equal(expected, subject.to_hash)
+          end
         end
       end
     end
