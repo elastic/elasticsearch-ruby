93c97df27d2cc8f89662db87c8054c113348f0a5
Karel Minarik <karel.minarik@elasticsearch.com>
2015-10-25 09:12:49 +0100

    [API] Fixed and error in the YAML runner handling of ENV['TEST_CLUSTER_PARAMS']

--- elasticsearch-api/test/integration/yaml_test_runner.rb
+++ elasticsearch-api/test/integration/yaml_test_runner.rb
@@ -24,7 +24,7 @@ Turn.config.format = :pretty
 # Launch test cluster
 #
 if ENV['SERVER'] and not Elasticsearch::Extensions::Test::Cluster.running?
-  es_params = "-D es.repositories.url.allowed_urls=http://snapshot.test* -D es.path.repo=/tmp -D es.node.testattr=test " + ENV['TEST_CLUSTER_PARAMS']
+  es_params = "-D es.repositories.url.allowed_urls=http://snapshot.test* -D es.path.repo=/tmp -D es.node.testattr=test " + ENV['TEST_CLUSTER_PARAMS'].to_s
   Elasticsearch::Extensions::Test::Cluster.start(nodes: 1, es_params: es_params )
 end
 
