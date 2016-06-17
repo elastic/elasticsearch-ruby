60e0d7f706035dfd5c2e91c20537d361e40df3d7
Karel Minarik <karel.minarik@elasticsearch.com>
2016-06-17 09:30:57 +0200

    [API] Added the `:verbose` option to the "Indices Segments" API and fixed formatting

--- elasticsearch-api/lib/elasticsearch/api/actions/indices/segments.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/indices/segments.rb
@@ -10,15 +10,16 @@ module Elasticsearch
         #
         # @option arguments [List] :index A comma-separated list of index names; use `_all` or empty string
         #                                 to perform the operation on all indices
-        # @option arguments [Boolean] :allow_no_indices Whether to ignore if a wildcard indices expression resolves into
-        #                                               no concrete indices. (This includes `_all` string or when no
-        #                                               indices have been specified)
-        # @option arguments [String] :expand_wildcards Whether to expand wildcard expression to concrete indices that
-        #                                              are open, closed or both. (options: open, closed)
+        # @option arguments [Boolean] :allow_no_indices Whether to ignore if a wildcard indices expression
+        #                                               resolves into no concrete indices. (This includes `_all`
+        #                                               string or when no indices have been specified)
+        # @option arguments [String] :expand_wildcards Whether to expand wildcard expression to concrete indices
+        #                                              that are open, closed or both. (options: open, closed)
         # @option arguments [String] :ignore_indices When performed on multiple indices, allows to ignore
         #                                            `missing` ones (options: none, missing) @until 1.0
         # @option arguments [Boolean] :ignore_unavailable Whether specified concrete indices should be ignored when
         #                                                 unavailable (missing, closed, etc)
+        # @option arguments [Boolean] :verbose Whether to include detailed memory usage by Lucene (default: false)
         #
         # @see http://elasticsearch.org/guide/reference/api/admin-indices-segments/
         #
@@ -27,7 +28,8 @@ module Elasticsearch
             :ignore_indices,
             :ignore_unavailable,
             :allow_no_indices,
-            :expand_wildcards
+            :expand_wildcards,
+            :verbose
           ]
 
           method = HTTP_GET
