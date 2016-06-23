1aad21f6c8e2be97d48e463402632fdf6c3c2c53
Karel Minarik <karel.minarik@elasticsearch.com>
2016-06-23 18:26:19 +0200

    [API] Added the "Indices Rollover" API

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/indices/rollover.rb
@@ -0,0 +1,39 @@
+module Elasticsearch
+  module API
+    module Indices
+      module Actions
+
+        # The rollover index API rolls an alias over to a new index when the existing index
+        # is considered to be too large or too old
+        #
+        # @option arguments [String] :alias The name of the alias to rollover (*Required*)
+        # @option arguments [String] :new_index The name of the rollover index
+        # @option arguments [Hash] :body The conditions that needs to be met for executing rollover
+        # @option arguments [Time] :timeout Explicit operation timeout
+        # @option arguments [Time] :master_timeout Specify timeout for connection to master
+        #
+        # @see http://www.elastic.co/guide/en/elasticsearch/reference/master/indices-rollover-index.html
+        #
+        def rollover(arguments={})
+          raise ArgumentError, "Required argument 'alias' missing" unless arguments[:alias]
+
+          valid_params = [
+            :timeout,
+            :master_timeout ]
+
+          arguments = arguments.clone
+
+          source = arguments.delete(:alias)
+          target = arguments.delete(:new_index)
+
+          method = HTTP_POST
+          path   = Utils.__pathify source, '_rollover', target
+          params = Utils.__validate_and_extract_params arguments, valid_params
+          body   = arguments[:body]
+
+          perform_request(method, path, params, body).body
+        end
+      end
+    end
+  end
+end
