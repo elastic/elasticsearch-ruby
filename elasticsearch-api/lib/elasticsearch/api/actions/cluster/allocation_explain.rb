4c59bcd142732248b69928b8690d91bc2101035e
Karel Minarik <karel.minarik@elasticsearch.com>
2016-07-15 12:03:55 +0200

    [API] Added the `include_disk_info` parameter to the "Cluster Allocation Explain" API

--- elasticsearch-api/lib/elasticsearch/api/actions/cluster/allocation_explain.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/cluster/allocation_explain.rb
@@ -7,12 +7,15 @@ module Elasticsearch
         #
         # @option arguments [Hash] :body The index, shard, and primary flag to explain. Empty means 'explain the first unassigned shard'
         # @option arguments [Boolean] :include_yes_decisions Return 'YES' decisions in explanation (default: false)
+        # @option arguments [Boolean] :include_disk_info Return information about disk usage and shard sizes
+        #                                                (default: false)
         #
         # @see http://www.elastic.co/guide/en/elasticsearch/reference/master/cluster-allocation-explain.html
         #
         def allocation_explain(arguments={})
           valid_params = [
-            :include_yes_decisions ]
+            :include_yes_decisions,
+            :include_disk_info ]
           method = 'GET'
           path   = "_cluster/allocation/explain"
           params = Utils.__validate_and_extract_params arguments, valid_params
