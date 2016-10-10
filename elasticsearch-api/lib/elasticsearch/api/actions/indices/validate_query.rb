e9282534d530bc7ec614bac36594508c0aa5bcc8
brett <adnexius@gmail.com>
2016-10-10 22:47:58 +0800

    [API] Added the 'rewrite' option to "Indices Validate Query" API
    With rewrite set to true, the explanation is more detailed showing the
    actual Lucene query that will be executed.
    
    Closes #359

--- elasticsearch-api/lib/elasticsearch/api/actions/indices/validate_query.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/indices/validate_query.rb
@@ -13,9 +13,12 @@ module Elasticsearch
         #
         #     client.indices.validate_query index: 'myindex', q: '[[[ BOOM! ]]]', explain: true
         #
-        # @example Validate a DSL query (with explanation)
+        # @example Validate a DSL query (with explanation and rewrite). With rewrite set to true, the 
+        #                                explanation is more detailed showing the actual Lucene query that will 
+        #                                be executed.
         #
         #     client.indices.validate_query index: 'myindex',
+        #                                   rewrite: true,
         #                                   explain: true,
         #                                   body: {
         #                                     filtered: {
@@ -64,6 +67,7 @@ module Elasticsearch
         #
         def validate_query(arguments={})
           valid_params = [
+            :rewrite,
             :explain,
             :ignore_unavailable,
             :allow_no_indices,
