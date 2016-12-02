eb5d37dab28e8971935c6c18839f3c41e3f5985b
Sylvain MINA <sylvain.mina@gmail.com>
2016-12-02 16:58:43 +0100

    Fix doc for number_of_nodes

--- elasticsearch-extensions/lib/elasticsearch/extensions/test/cluster.rb
+++ elasticsearch-extensions/lib/elasticsearch/extensions/test/cluster.rb
@@ -173,18 +173,18 @@ module Elasticsearch
 
           # Create a new instance of the Cluster class
           #
-          # @option arguments [String]  :cluster_name Cluster name (default: `elasticsearch_test`)
-          # @option arguments [Integer] :nodes        Number of desired nodes (default: 2)
-          # @option arguments [String]  :command      Elasticsearch command (default: `elasticsearch`)
-          # @option arguments [String]  :port         Starting port number; will be auto-incremented (default: 9250)
-          # @option arguments [String]  :node_name    The node name (will be appended with a number)
-          # @option arguments [String]  :path_data    Path to the directory to store data in
-          # @option arguments [String]  :path_work    Path to the directory with auxiliary files
-          # @option arguments [String]  :path_logs    Path to the directory with log files
+          # @option arguments [String]  :cluster_name      Cluster name (default: `elasticsearch_test`)
+          # @option arguments [Integer] :number_of_nodes   Number of desired nodes (default: 2)
+          # @option arguments [String]  :command           Elasticsearch command (default: `elasticsearch`)
+          # @option arguments [String]  :port              Starting port number; will be auto-incremented (default: 9250)
+          # @option arguments [String]  :node_name         The node name (will be appended with a number)
+          # @option arguments [String]  :path_data         Path to the directory to store data in
+          # @option arguments [String]  :path_work         Path to the directory with auxiliary files
+          # @option arguments [String]  :path_logs         Path to the directory with log files
           # @option arguments [Boolean] :multicast_enabled Whether multicast is enabled (default: true)
-          # @option arguments [Integer] :timeout      Timeout when starting the cluster (default: 30)
-          # @option arguments [String]  :network_host The host that nodes will bind on and publish to
-          # @option arguments [Boolean] :clear_cluster Wipe out cluster content on startup (default: true)
+          # @option arguments [Integer] :timeout           Timeout when starting the cluster (default: 30)
+          # @option arguments [String]  :network_host      The host that nodes will bind on and publish to
+          # @option arguments [Boolean] :clear_cluster     Wipe out cluster content on startup (default: true)
           #
           # You can also use environment variables to set the constructor options (see source).
           #
@@ -224,7 +224,7 @@ module Elasticsearch
           # @example Start a cluster with a custom configuration
           #      Elasticsearch::Extensions::Test::Cluster::Cluster.new(
           #        cluster_name: 'my-cluster',
-          #        nodes: 3,
+          #        number_of_nodes: 3,
           #        node_name: 'my-node',
           #        port: 9350
           #      ).start
