6d8bf79bec2747599f03107f4080c03fe872545b
Matt Griffin <matt@griffinonline.org>
2017-01-03 17:04:36 -0500

    [EXT] Fixed `Errno::ESRCH` exception when killing the process in Test::Cluster::Cluster#__determine_version
    Rescue failures in process cleanup when checking ES version in the test cluster: normally, the process
    already exited, so `Process.kill` raises an `Errno::ESRCH` exception.
    
    Closes #383

--- elasticsearch-extensions/lib/elasticsearch/extensions/test/cluster.rb
+++ elasticsearch-extensions/lib/elasticsearch/extensions/test/cluster.rb
@@ -465,7 +465,9 @@ module Elasticsearch
                 __log "Running [#{arguments[:command]} -v] to determine version" if ENV['DEBUG']
                 output = `#{arguments[:command]} -v`
               ensure
-                Process.kill('INT', pid) if pid
+                if pid
+                  Process.kill('INT', pid) rescue Errno::ESRCH # Most likely the process has terminated already
+                end
                 wout.close unless wout.closed?
                 rout.close unless rout.closed?
               end
