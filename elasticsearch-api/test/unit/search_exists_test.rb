bf3d63f7e8f5e3be72faaa2008052dd5ee0b0b3e
Wojciech Korzeniowski <korzeniowski.wojciech@gmail.com>
2017-02-10 21:29:47 +0100

    [API] Added, that `index` and `type` parameters are respected in the "Search Exists" API
    Related: #401

--- elasticsearch-api/test/unit/search_exists_test.rb
+++ elasticsearch-api/test/unit/search_exists_test.rb
@@ -19,6 +19,38 @@ module Elasticsearch
           subject.search_exists :q => 'foo'
         end
 
+        should "perform request against an index" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal 'POST', method
+            assert_equal 'books/_search/exists', url
+            assert_nil   body
+            true
+          end.returns(FakeResponse.new)
+
+          subject.search_exists :index => 'books'
+        end
+
+        should "perform request against an index and type" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal 'POST', method
+            assert_equal 'books/holly/_search/exists', url
+            assert_nil   body
+            true
+          end.returns(FakeResponse.new)
+
+          subject.search_exists :index => 'books', :type => 'holly'
+        end
+
+        should "perform request against default index if type given" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal 'POST', method
+            assert_equal '_all/holly/_search/exists', url
+            assert_nil   body
+            true
+          end.returns(FakeResponse.new)
+
+          subject.search_exists :type => 'holly'
+        end
       end
 
     end
