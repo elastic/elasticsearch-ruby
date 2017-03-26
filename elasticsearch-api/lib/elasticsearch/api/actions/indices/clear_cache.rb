572d037ae999570d6b5804198c3d1725eb1d121f
Karel Minarik <karel.minarik@elasticsearch.com>
2017-03-26 17:55:41 +0200

    [API] Added missing parameters to the "Indices Clear Cache" API

--- elasticsearch-api/lib/elasticsearch/api/actions/indices/clear_cache.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/indices/clear_cache.rb
@@ -23,27 +23,17 @@ module Elasticsearch
         #     client.indices.clear_cache field_data: true, fields: 'created_at', filter_cache: false, id_cache: false
         #
         # @option arguments [List] :index A comma-separated list of index name to limit the operation
-        # @option arguments [Boolean] :allow_no_indices Whether to ignore if a wildcard indices expression resolves into
-        #                                               no concrete indices. (This includes `_all` string or when no
-        #                                               indices have been specified)
-        # @option arguments [String] :expand_wildcards Whether to expand wildcard expression to concrete indices that
-        #                                              are open, closed or both. (options: open, closed)
         # @option arguments [Boolean] :field_data Clear field data
         # @option arguments [Boolean] :fielddata Clear field data
-        # @option arguments [List] :fields A comma-separated list of fields to clear when using the
-        #                                  `field_data` parameter(default: all)
-        # @option arguments [Boolean] :filter Clear filter caches
-        # @option arguments [Boolean] :filter_cache Clear filter caches
-        # @option arguments [List] :filter_keys A comma-separated list of keys to clear when using the
-        #                                          `filter_cache` parameter (default: all)
-        # @option arguments [Boolean] :id Clear ID caches for parent/child
-        # @option arguments [Boolean] :id_cache Clear ID caches for parent/child
-        # @option arguments [String] :ignore_indices When performed on multiple indices, allows to ignore
-        #                                            `missing` ones (options: none, missing) @until 1.0
-        # @option arguments [Boolean] :ignore_unavailable Whether specified concrete indices should be ignored when
-        #                                                 unavailable (missing, closed, etc)
+        # @option arguments [List] :fields A comma-separated list of fields to clear when using the `field_data` parameter (default: all)
+        # @option arguments [Boolean] :query Clear query caches
+        # @option arguments [Boolean] :ignore_unavailable Whether specified concrete indices should be ignored when unavailable (missing or closed)
+        # @option arguments [Boolean] :allow_no_indices Whether to ignore if a wildcard indices expression resolves into no concrete indices. (This includes `_all` string or when no indices have been specified)
+        # @option arguments [String] :expand_wildcards Whether to expand wildcard expression to concrete indices that are open, closed or both. (options: open, closed, none, all)
         # @option arguments [List] :index A comma-separated list of index name to limit the operation
         # @option arguments [Boolean] :recycler Clear the recycler cache
+        # @option arguments [Boolean] :request_cache Clear request cache
+        # @option arguments [Boolean] :request Clear request cache
         #
         # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/indices-clearcache.html
         #
@@ -52,16 +42,14 @@ module Elasticsearch
             :field_data,
             :fielddata,
             :fields,
-            :filter,
-            :filter_cache,
-            :filter_keys,
-            :id,
-            :id_cache,
-            :ignore_indices,
+            :query,
             :ignore_unavailable,
             :allow_no_indices,
             :expand_wildcards,
-            :recycler ]
+            :index,
+            :recycler,
+            :request_cache,
+            :request ]
 
           method = HTTP_POST
           path   = Utils.__pathify Utils.__listify(arguments[:index]), '_cache/clear'
