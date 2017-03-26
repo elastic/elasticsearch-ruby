6cf650d2200304ec988528683a4b3ef621348d7c
Karel Minarik <karel.minarik@elasticsearch.com>
2017-03-26 19:34:52 +0200

    [EXT] Improved the integration test for the `Test::Cluster` extension

--- elasticsearch-extensions/test/test/cluster/integration/cluster_test.rb
+++ elasticsearch-extensions/test/test/cluster/integration/cluster_test.rb
@@ -5,7 +5,11 @@ require 'elasticsearch/extensions/test/cluster'
 
 class Elasticsearch::Extensions::TestClusterIntegrationTest < Test::Unit::TestCase
   context "The Test::Cluster" do
-    PATH_TO_BUILDS = Pathname(File.expand_path('../../../../../../tmp/builds', __FILE__))
+    PATH_TO_BUILDS = if ENV['PATH_TO_BUILDS']
+      Pathname(ENV['PATH_TO_BUILDS'])
+    else
+      Pathname(File.expand_path('../../../../../../tmp/builds', __FILE__))
+    end
 
     unless PATH_TO_BUILDS.exist?
       puts "Path to builds doesn't exist, skipping TestClusterIntegrationTest"
@@ -18,16 +22,20 @@ class Elasticsearch::Extensions::TestClusterIntegrationTest < Test::Unit::TestCa
       []
     end
 
+    STDOUT.puts %Q|Builds: \n#{@builds.map { |b| "  * #{b}"}.join("\n")}|
+
     @builds.each do |build|
       should "start and stop #{build.to_s}" do
         puts ("----- #{build.to_s} " + "-"*(80-7-build.to_s.size)).to_s.ansi(:bold)
-        Elasticsearch::Extensions::Test::Cluster.start command: PATH_TO_BUILDS.join(build.join('bin/elasticsearch')).to_s
+        begin
+          Elasticsearch::Extensions::Test::Cluster.start command: PATH_TO_BUILDS.join(build.join('bin/elasticsearch')).to_s
 
-        # Index some data to create the data directory
-        client = Elasticsearch::Client.new host: "localhost:9250"
-        client.index index: 'test1', type: 'd', id: 1, body: { title: 'TEST' }
-
-        Elasticsearch::Extensions::Test::Cluster.stop command: PATH_TO_BUILDS.join(build.join('bin/elasticsearch')).to_s
+          # Index some data to create the data directory
+          client = Elasticsearch::Client.new host: "localhost:9250"
+          client.index index: 'test1', type: 'd', id: 1, body: { title: 'TEST' }
+        ensure
+          Elasticsearch::Extensions::Test::Cluster.stop command: PATH_TO_BUILDS.join(build.join('bin/elasticsearch')).to_s
+        end
       end
     end
   end
