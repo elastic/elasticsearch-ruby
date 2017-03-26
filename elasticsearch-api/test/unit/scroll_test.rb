4251a956bffd9d442b31daf524e2f3dfe9e9d95b
Karel Minarik <karel.minarik@elasticsearch.com>
2017-03-26 20:33:35 +0200

    [API] Fixed the failing unit tests for "Scroll" APIs
    Related: 919d23f

--- elasticsearch-api/test/unit/scroll_test.rb
+++ elasticsearch-api/test/unit/scroll_test.rb
@@ -11,7 +11,8 @@ module Elasticsearch
           subject.expects(:perform_request).with do |method, url, params, body|
             assert_equal 'GET', method
             assert_equal '_search/scroll', url
-            assert_equal 'cXVlcn...', body
+            assert_equal 'cXVlcn...', params[:scroll_id]
+            assert_equal nil, body
             true
           end.returns(FakeResponse.new)
 
