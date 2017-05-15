64a31865abcab4fcc6539455a34521d1bd4e941f
Karel Minarik <karel.minarik@elasticsearch.com>
2017-05-15 17:26:42 +0200

    [API] Removed unsupported parameters from the "Indices Flush" API
    Closes #430

--- elasticsearch-api/lib/elasticsearch/api/actions/indices/flush.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/indices/flush.rb
@@ -13,30 +13,25 @@ module Elasticsearch
         # @option arguments [List] :index A comma-separated list of index names; use `_all` or empty string for all indices
         # @option arguments [Boolean] :force Whether a flush should be forced even if it is not necessarily needed ie.
         #                                    if no changes will be committed to the index. (Internal)
-        # @option arguments [Boolean] :full If set to true a new index writer is created and settings that have been
-        #                                   changed related to the index writer will be refreshed. (Internal)
         # @option arguments [Boolean] :allow_no_indices Whether to ignore if a wildcard indices expression resolves into
         #                                               no concrete indices. (This includes `_all` string or when no
         #                                               indices have been specified)
         # @option arguments [String] :expand_wildcards Whether to expand wildcard expression to concrete indices that
         #                                              are open, closed or both. (options: open, closed)
-        # @option arguments [String] :ignore_indices When performed on multiple indices, allows to ignore
-        #                                            `missing` ones (options: none, missing) @until 1.0
+        # @option arguments [Boolean] :wait_if_ongoing If set to true the flush operation will block until the flush can be executed if another flush operation is already executing. The default is true. If set to false the flush will be skipped iff if another flush operation is already running.
         # @option arguments [Boolean] :ignore_unavailable Whether specified concrete indices should be ignored when
         #                                                 unavailable (missing, closed, etc)
-        # @option arguments [Boolean] :refresh Refresh the index after performing the operation
         #
         # @see http://www.elasticsearch.org/guide/reference/api/admin-indices-flush/
         #
         def flush(arguments={})
           valid_params = [
             :force,
-            :full,
-            :ignore_indices,
+            :wait_if_ongoing,
             :ignore_unavailable,
             :allow_no_indices,
-            :expand_wildcards,
-            :refresh ]
+            :expand_wildcards
+          ]
 
           method = HTTP_POST
           path   = Utils.__pathify Utils.__listify(arguments[:index]), '_flush'
