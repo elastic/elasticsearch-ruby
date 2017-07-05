7a39ea2965b51a86cc4794a1ca35c7131b79a474
Aaron Gunderson <aaron@bookbub.com>
2017-07-05 11:41:33 -0400

    Add match_phrase query

---
+++ elasticsearch-dsl/lib/elasticsearch/dsl/search/queries/match_phrase.rb
@@ -0,0 +1,32 @@
+
+module Elasticsearch
+  module DSL
+    module Search
+      module Queries
+
+        # A query which matches all documents
+        #
+        # @example
+        #
+        #     search do
+        #       query do
+        #         match_phrase :content do
+        #           query 'example content'
+        #         end
+        #       end
+        #     end
+        #
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-match-query-phrase.html
+        #
+        class MatchPhrase
+          include BaseComponent
+
+          option_method :query
+          option_method :analyzer
+          option_method :boost
+        end
+
+      end
+    end
+  end
+end
