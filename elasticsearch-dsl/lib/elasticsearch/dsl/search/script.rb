2455611e9e00aa141bc08298f10d332da506709b
abdoushopify <abdou.sarr@shopify.com>
2017-08-31 16:11:07 -0400

    Add Example

--- elasticsearch-dsl/lib/elasticsearch/dsl/search/script.rb
+++ elasticsearch-dsl/lib/elasticsearch/dsl/search/script.rb
@@ -6,13 +6,24 @@ module Elasticsearch
       #
       # @see https://www.elastic.co/guide/en/elasticsearch/reference/5.5/query-dsl-script-query.html
       #
+
+      #
+      # @example
+      #
+      #     search do
+      #       script do
+      #         inline 'test'
+      #         params [value: 7]
+      #       end
+      #     end
+      #
       class Script
         include BaseComponent
 
         option_method :script
         option_method :inline
         option_method :params
-        
+
       end
     end
   end
