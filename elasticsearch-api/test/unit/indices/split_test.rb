7af1cd670805fc9522dfcd86a87df1619783c736
Karel Minarik <karel.minarik@elasticsearch.com>
2017-11-14 10:38:17 +0100

    [API] Added the "Indices Split" API

---
+++ elasticsearch-api/test/unit/indices/split_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+module Elasticsearch
+  module Test
+    class IndicesSplitTest < ::Test::Unit::TestCase
+
+      context "Indices: Split" do
+        subject { FakeClient.new }
+
+        should "perform correct request" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal 'PUT', method
+            assert_equal 'foo/_split/bar', url
+            assert_equal Hash.new, params
+            assert_equal nil, body
+            true
+          end.returns(FakeResponse.new)
+
+          subject.indices.split :index => 'foo', :target => 'bar'
+        end
+
+      end
+
+    end
+  end
+end
