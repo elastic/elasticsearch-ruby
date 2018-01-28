ba5f6cf32605e75781aeb6c455afa0c05ed72989
Karel Minarik <karel.minarik@elastic.co>
2018-01-28 12:47:12 +0100

    [API] Fixed the incorrect `create` method
    Previously, the "Create" API has required the `:id` argument, although it is _not_ required
    [https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-index_.html#_automatic_id_generation].
    
    The REST API specs list it as required, though: <https://github.com/elastic/elasticsearch/blob/b47b399f000fce42eac0e00b9fdcf969122660ab/rest-api-spec/src/main/resources/rest-api-spec/api/create.json#L11>
    
    This patch changes the logic of calling the `index` API method: when the `:id` is present,
    it adds the `op_type` parameter, if it's missing, it simply calls the `index` method with parameters "as is".
    
    Closes #491
    
    (cherry picked from commit b1a848292ea5aa45c443fa2bee60ad1b53d9258a)

--- elasticsearch-api/lib/elasticsearch/api/actions/create.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/create.rb
@@ -2,33 +2,40 @@ module Elasticsearch
   module API
     module Actions
 
-      # Create a document.
+      # Create a new document.
       #
-      # Enforce the _create_ operation when indexing a document --
-      # the operation will return an error when the document already exists.
+      # The API will create new document, if it doesn't exist yet -- in that case, it will return
+      # a `409` error (`version_conflict_engine_exception`).
       #
-      # @example Create a document
+      # You can leave out the `:id` parameter for the ID to be generated automatically
+      #
+      # @example Create a document with an ID
       #
       #     client.create index: 'myindex',
-      #                   type: 'mytype',
-      #                   id: '1',
-      #                   body: {
-      #                    title: 'Test 1',
-      #                    tags: ['y', 'z'],
-      #                    published: true,
-      #                    published_at: Time.now.utc.iso8601,
-      #                    counter: 1
-      #                   }
+      #                  type: 'doc',
+      #                  id: '1',
+      #                  body: {
+      #                   title: 'Test 1'
+      #                 }
+      #
+      # @example Create a document with an auto-generated ID
+      #
+      #     client.create index: 'myindex',
+      #                  type: 'doc',
+      #                  body: {
+      #                   title: 'Test 1'
+      #                 }
       #
       # @option (see Actions#index)
       #
-      # (The `:op_type` argument is ignored.)
-      #
-      # @see http://elasticsearch.org/guide/reference/api/index_/
+      # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-index_.html#_automatic_id_generation
       #
       def create(arguments={})
-        raise ArgumentError, "Required argument 'id' missing"  unless arguments[:id]
-        index arguments.update :op_type => 'create'
+        if arguments[:id]
+          index arguments.update :op_type => 'create'
+        else
+          index arguments
+        end
       end
     end
   end
