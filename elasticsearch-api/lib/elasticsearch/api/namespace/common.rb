4dfe8cf397b3abf84a9bc794c384fdd5efeb5257
Hiroshi Hatake <hatake@clear-code.com>
2018-03-22 12:10:03 +0900

    [API] Allow passing headers

--- elasticsearch-api/lib/elasticsearch/api/namespace/common.rb
+++ elasticsearch-api/lib/elasticsearch/api/namespace/common.rb
@@ -17,8 +17,8 @@ module Elasticsearch
 
         # Delegates the `perform_request` method to the wrapped client
         #
-        def perform_request(method, path, params={}, body=nil)
-          client.perform_request method, path, params, body
+        def perform_request(method, path, params={}, body=nil, headers=nil)
+          client.perform_request method, path, params, body, headers
         end
       end
 
