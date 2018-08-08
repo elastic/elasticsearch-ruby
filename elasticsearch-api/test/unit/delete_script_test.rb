b29c3455c5252347079fd70e0d139c1b636e5898
uno4ki <uno4ki@users.noreply.github.com>
2018-08-08 15:24:34 +0300

    Stored scripts lang namespace deprecation (#523)

--- elasticsearch-api/test/unit/delete_script_test.rb
+++ elasticsearch-api/test/unit/delete_script_test.rb
@@ -7,7 +7,7 @@ module Elasticsearch
       context "Delete script" do
         subject { FakeClient.new }
 
-        should "perform correct request" do
+        should "perform correct request with specified lang" do
           subject.expects(:perform_request).with do |method, url, params, body|
             assert_equal 'DELETE', method
             assert_equal '_scripts/groovy/foo', url
@@ -17,6 +17,18 @@ module Elasticsearch
           end.returns(FakeResponse.new)
 
           subject.delete_script :lang => "groovy", :id => "foo"
+        end
+
+        should "perform correct request w/o lang" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal 'DELETE', method
+            assert_equal '_scripts/foo', url
+            assert_equal Hash.new, params
+            assert_nil   body
+            true
+          end.returns(FakeResponse.new)
+
+          subject.delete_script :id => "foo"
         end
 
       end
--- elasticsearch-api/test/unit/put_script_test.rb
+++ elasticsearch-api/test/unit/put_script_test.rb
@@ -7,7 +7,7 @@ module Elasticsearch
       context "Put script" do
         subject { FakeClient.new }
 
-        should "perform correct request" do
+        should "perform correct request with specified lang" do
           subject.expects(:perform_request).with do |method, url, params, body|
             assert_equal 'PUT', method
             assert_equal '_scripts/groovy/foo', url
@@ -17,6 +17,18 @@ module Elasticsearch
           end.returns(FakeResponse.new)
 
           subject.put_script :lang => 'groovy', :id => 'foo', :body => { :script => 'bar' }
+        end
+
+        should "perform correct request w/o lang" do
+          subject.expects(:perform_request).with do |method, url, params, body|
+            assert_equal 'PUT', method
+            assert_equal '_scripts/foo', url
+            assert_equal Hash.new, params
+            assert_equal 'bar', body[:script]
+            true
+          end.returns(FakeResponse.new)
+
+          subject.put_script :id => 'foo', :body => { :script => 'bar' }
         end
 
       end
