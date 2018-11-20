548990b530282caa04b01cb3cc0603651953786b
Emily Stolfo <emstolfo@gmail.com>
2018-11-20 15:46:07 +0100

    [CLIENT] Add flexible configuration in spec helper

--- elasticsearch-transport/spec/spec_helper.rb
+++ elasticsearch-transport/spec/spec_helper.rb
@@ -3,16 +3,35 @@ require 'elasticsearch-transport'
 require 'logger'
 require 'ansi/code'
 
-RSpec.configure do |config|
-  config.formatter = 'documentation'
-  config.color = true
-end
+# The hosts to use for creating a elasticsearch client.
+#
+# @since 7.0.0
+ELASTICSEARCH_HOSTS = ENV['ELASTICSEARCH_HOSTS'] ? ENV['ELASTICSEARCH_HOSTS'].split(',').freeze : [ '127.0.0.1:9250' ].freeze
 
-TEST_PORTS = [9250, 9200]
-TEST_HOSTS = TEST_PORTS.map { |port| "localhost:#{port}" }
-
+# Are we testing on JRuby?
+#
+# @return [ true, false ] Whether JRuby is being used.
+#
+# @since 7.0.0
 def jruby?
   RUBY_PLATFORM =~ /\bjava\b/
 end
 
 require 'patron' unless jruby?
+
+module Config
+
+  def self.included(context)
+
+    # Get the hosts to use to connect an elasticsearch client.
+    #
+    # @since 7.0.0
+    context.let(:hosts) { ELASTICSEARCH_HOSTS }
+  end
+end
+
+RSpec.configure do |config|
+  config.include(Config)
+  config.formatter = 'documentation'
+  config.color = true
+end
