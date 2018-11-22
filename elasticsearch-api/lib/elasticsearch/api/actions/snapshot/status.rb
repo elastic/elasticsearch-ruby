1aa7b37add7af299a502b81067a90a8524e2d1a6
Emily Stolfo <emstolfo@gmail.com>
2018-11-22 16:02:20 +0100

    Add license header to more files

--- elasticsearch-api/lib/elasticsearch/api/actions/snapshot/status.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/snapshot/status.rb
@@ -1,3 +1,20 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#	http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
 module Elasticsearch
   module API
     module Snapshot
@@ -15,7 +32,8 @@ module Elasticsearch
         #
         # @option arguments [String] :repository A repository name
         # @option arguments [List] :snapshot A comma-separated list of snapshot names
-        # @option arguments [Boolean] :ignore_unavailable Whether to ignore unavailable snapshots, defaults to #                                                 false which means a SnapshotMissingException is thrown
+        # @option arguments [Boolean] :ignore_unavailable Whether to ignore unavailable snapshots, defaults to
+        #   false which means a SnapshotMissingException is thrown
         # @option arguments [Time] :master_timeout Explicit operation timeout for connection to master node
         # @option arguments [Number,List] :ignore The list of HTTP errors to ignore
         #
