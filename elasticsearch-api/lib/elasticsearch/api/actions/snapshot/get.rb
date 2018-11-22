442f942f9219f64fe499c3972f9d4985c05a1a71
Emily Stolfo <emstolfo@gmail.com>
2018-11-22 16:02:20 +0100

    Add license header to more files

--- elasticsearch-api/lib/elasticsearch/api/actions/snapshot/get.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/snapshot/get.rb
@@ -1,3 +1,20 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#	http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
 module Elasticsearch
   module API
     module Snapshot
@@ -19,7 +36,8 @@ module Elasticsearch
         #
         # @option arguments [String] :repository A repository name (*Required*)
         # @option arguments [List] :snapshot A comma-separated list of snapshot names (*Required*)
-        # @option arguments [Boolean] :ignore_unavailable Whether to ignore unavailable snapshots, defaults to #                                                 false which means a SnapshotMissingException is thrown
+        # @option arguments [Boolean] :ignore_unavailable Whether to ignore unavailable snapshots, defaults to
+        #   false which means a SnapshotMissingException is thrown
         # @option arguments [Time] :master_timeout Explicit operation timeout for connection to master node
         # @option arguments [Boolean] :verbose Whether to show verbose snapshot info or only show the basic info found in the repository index blob
         #
