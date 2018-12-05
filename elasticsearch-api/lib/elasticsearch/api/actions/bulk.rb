ef362acad1d1f717033897696d66640bf55de6d1
Emily Stolfo <emstolfo@gmail.com>
2018-12-05 16:32:09 +0100

    [API] Note that a non-empty body argument is required for the bulk api

--- elasticsearch-api/lib/elasticsearch/api/actions/bulk.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/bulk.rb
@@ -8,6 +8,8 @@ module Elasticsearch
       # or the conveniency "combined" format where data is passed along with the header
       # in a single item in a custom `:data` key.
       #
+      # @note The body argument is required and cannot be empty.
+      #
       # @example Perform three operations in a single request, passing actions and data as an array of hashes
       #
       #     client.bulk body: [
@@ -47,7 +49,7 @@ module Elasticsearch
       #
       # @option arguments [String] :index Default index for items which don't provide one
       # @option arguments [String] :type Default document type for items which don't provide one
-      # @option arguments [Hash] :body The operation definition and data (action-data pairs), separated by newlines (*Required*)
+      # @option arguments [Hash] :body The operation definition and data (action-data pairs), separated by newlines (*Required*). Note that this cannot be empty.
       # @option arguments [String] :wait_for_active_shards Sets the number of shard copies that must be active before proceeding with the bulk operation. Defaults to 1, meaning the primary shard only. Set to `all` for all shard copies, otherwise set to any non-negative value less than or equal to the total number of copies for the shard (number of replicas + 1)
       # @option arguments [String] :refresh If `true` then refresh the effected shards to make this operation visible to search, if `wait_for` then wait for a refresh to make this operation visible to search, if `false` (the default) then do nothing with refreshes. (options: true, false, wait_for)
       # @option arguments [String] :routing Specific routing value
