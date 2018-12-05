32b54335b0a4c37159ffae2c09c86980af54b3b9
Emily Stolfo <emstolfo@gmail.com>
2018-12-05 16:36:24 +0100

    [API] Add note about empty body in yard documentation

--- elasticsearch-api/lib/elasticsearch/api/actions/bulk.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/bulk.rb
@@ -66,7 +66,7 @@ module Elasticsearch
       #
       # @option arguments [String] :index Default index for items which don't provide one
       # @option arguments [String] :type Default document type for items which don't provide one
-      # @option arguments [Hash] :body The operation definition and data (action-data pairs), separated by newlines (*Required*)
+      # @option arguments [Hash] :body The operation definition and data (action-data pairs), separated by newlines (*Required*). Note that this cannot be empty.
       # @option arguments [Boolean] :include_type_name Whether a type should be expected in the body of the mappings.
       # @option arguments [String] :wait_for_active_shards Sets the number of shard copies that must be active before proceeding with the bulk operation. Defaults to 1, meaning the primary shard only. Set to `all` for all shard copies, otherwise set to any non-negative value less than or equal to the total number of copies for the shard (number of replicas + 1)
       # @option arguments [String] :refresh If `true` then refresh the effected shards to make this operation visible to search, if `wait_for` then wait for a refresh to make this operation visible to search, if `false` (the default) then do nothing with refreshes. (options: true, false, wait_for)
