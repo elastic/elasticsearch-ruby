b594f9b49870eea250cc9ba52aa69da68a2aa16e
Emily Stolfo <emstolfo@gmail.com>
2019-03-14 12:39:14 +0100

    [XPACK] Support set_upgrade_mode machine learning API

--- elasticsearch-xpack/lib/elasticsearch/xpack.rb
+++ elasticsearch-xpack/lib/elasticsearch/xpack.rb
@@ -51,6 +51,10 @@ module Elasticsearch
       def security
         @security ||= xpack.security
       end
+
+      def ml
+        @ml ||= xpack.ml
+      end
     end
   end
 end if defined?(Elasticsearch::Transport::Client)
--- elasticsearch-xpack/spec/spec_helper.rb
+++ elasticsearch-xpack/spec/spec_helper.rb
@@ -22,6 +22,8 @@ if hosts = ENV['TEST_ES_SERVER'] || ENV['ELASTICSEARCH_HOSTS']
   end
 
   TEST_HOST, TEST_PORT = split_hosts.first.split(':')
+else
+  TEST_HOST, TEST_PORT = 'localhost', '9200'
 end
 
 raw_certificate = File.read(File.join(PROJECT_PATH, '/.ci/certs/testnode.crt'))
@@ -30,32 +32,35 @@ certificate = OpenSSL::X509::Certificate.new(raw_certificate)
 raw_key = File.read(File.join(PROJECT_PATH, '/.ci/certs/testnode.key'))
 key = OpenSSL::PKey::RSA.new(raw_key)
 
-if TEST_SUITE == 'security'
-  TRANSPORT_OPTIONS.merge!(:ssl => { verify: false,
-                                     client_cert: certificate,
-                                     client_key: key,
-                                     ca_file: '.ci/certs/ca.crt'})
 
-  password = ENV['ELASTIC_PASSWORD']
-  URL = "https://elastic:#{password}@#{TEST_HOST}:#{TEST_PORT}"
-else
-  URL = "http://#{TEST_HOST}:#{TEST_PORT}"
-end
+if defined?(TEST_HOST) && defined?(TEST_PORT)
+  if TEST_SUITE == 'security'
+    TRANSPORT_OPTIONS.merge!(:ssl => { verify: false,
+                                       client_cert: certificate,
+                                       client_key: key,
+                                       ca_file: '.ci/certs/ca.crt'})
 
-ADMIN_CLIENT = Elasticsearch::Client.new(host: URL, transport_options: TRANSPORT_OPTIONS)
+    password = ENV['ELASTIC_PASSWORD']
+    URL = "https://elastic:#{password}@#{TEST_HOST}:#{TEST_PORT}"
+  else
+    URL = "http://#{TEST_HOST}:#{TEST_PORT}"
+  end
 
-if ENV['QUIET'] == 'true'
-  DEFAULT_CLIENT = Elasticsearch::Client.new(host: URL, transport_options: TRANSPORT_OPTIONS)
-else
-  DEFAULT_CLIENT = Elasticsearch::Client.new(host: URL,
-                                             transport_options: TRANSPORT_OPTIONS,
-                                             tracer: Logger.new($stdout))
+  ADMIN_CLIENT = Elasticsearch::Client.new(host: URL, transport_options: TRANSPORT_OPTIONS)
+
+  if ENV['QUIET'] == 'true'
+    DEFAULT_CLIENT = Elasticsearch::Client.new(host: URL, transport_options: TRANSPORT_OPTIONS)
+  else
+    DEFAULT_CLIENT = Elasticsearch::Client.new(host: URL,
+                                               transport_options: TRANSPORT_OPTIONS,
+                                               tracer: Logger.new($stdout))
+  end
 end
 
 
 
 YAML_FILES_DIRECTORY = "#{File.expand_path(File.dirname('..'), '..')}" +
-    "/tmp/elasticsearch/x-pack/plugin/src/test/resources/rest-api-spec/test/indices.freeze"
+    "/tmp/elasticsearch/x-pack/plugin/src/test/resources/rest-api-spec/test"
 skipped_files = []
 
 # Respone from Elasticsearch includes the ca.crt, so length doesn't match.
@@ -80,7 +85,14 @@ skipped_files += Dir.glob("#{YAML_FILES_DIRECTORY}/token/10_basic.yml")
 skipped_files += Dir.glob("#{YAML_FILES_DIRECTORY}/monitoring/bulk/10_basic.yml")
 skipped_files += Dir.glob("#{YAML_FILES_DIRECTORY}/monitoring/bulk/20_privileges.yml")
 
-REST_API_YAML_FILES = Dir.glob("#{YAML_FILES_DIRECTORY}/**/*.yml") - skipped_files
+
+SINGLE_TEST = nil
+# Uncomment the following line and set it to a file when a single test should be run.
+# SINGLE_TEST = ["#{File.expand_path(File.dirname('..'), '..')}" +
+#  "/tmp/elasticsearch/x-pack/plugin/src/test/resources/rest-api-spec/test/rollup/rollup_search.yml"]
+
+
+REST_API_YAML_FILES = SINGLE_TEST || Dir.glob("#{YAML_FILES_DIRECTORY}/**/*.yml") - skipped_files
 REST_API_YAML_SKIP_FEATURES = ['warnings'].freeze
 
 
@@ -91,7 +103,7 @@ REST_API_YAML_SKIP_FEATURES = ['warnings'].freeze
 #
 # @return [ Object ] The value at the nested key.
 #
-# @since 6.1.1
+# @since 6.2.0
 def find_value_in_document(chain, document)
   return document[chain[0]] unless chain.size > 1
   find_value_in_document(chain[1..-1], document[chain[0]]) if document[chain[0]]
@@ -110,9 +122,37 @@ end
 #
 # @return [ Array<Object> ] A list of the nested keys.
 #
-# @since 6.1.1
+# @since 6.2.0
 def split_and_parse_key(key)
   key.split(/(?<!\\)\./).map do |key|
     (key =~ /\A[-+]?[0-9]+\z/) ? key.to_i: key.gsub('\\', '')
   end.reject { |k| k == '$body' }
+end
+
+
+module HelperModule
+  def self.included(context)
+
+    context.let(:client_double) do
+      Class.new { include Elasticsearch::XPack::API }.new.tap do |client|
+        expect(client).to receive(:perform_request).with(*expected_args).and_return(response_double)
+      end
+    end
+
+    context.let(:client) do
+      Class.new { include Elasticsearch::XPack::API }.new.tap do |client|
+        expect(client).to receive(:perform_request).with(*expected_args).and_return(response_double)
+      end
+    end
+
+    context.let(:response_double) do
+      double('response', status: 200, body: {}, headers: {})
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.include(HelperModule)
+  config.formatter = 'documentation'
+  config.color = true
 end
