aad0d540ff0a1f6e7030f1ff9bebff92e2efb042
Emily Stolfo <emstolfo@gmail.com>
2019-03-15 18:24:53 +0100

    [XPACK] Support api key methods

---
+++ elasticsearch-xpack/lib/elasticsearch/xpack/api/actions/security/create_api_key.rb
@@ -0,0 +1,49 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#	http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+module Elasticsearch
+  module XPack
+    module API
+      module Security
+        module Actions
+
+          # Creates an API key for access without requiring basic authentication.
+          #
+          # @option arguments [String] :refresh If `true` (the default) then refresh the affected shards to make
+          #   this operation visible to search, if `wait_for` then wait for a refresh to make this operation visible to
+          #   search, if `false` then do nothing with refreshes.
+          #
+          # @option arguments [Hash] :body The api key request to create an API key. (*Required*)
+          #
+          # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/security-api-create-api-key.html
+          #
+          def create_api_key(arguments={})
+            raise ArgumentError, "Required argument 'body' missing" unless arguments[:body]
+
+            valid_params = [ :refresh ]
+
+            method = Elasticsearch::API::HTTP_PUT
+            path   = "_security/api_key"
+            params = Elasticsearch::API::Utils.__validate_and_extract_params arguments, valid_params
+
+            perform_request(method, path, params, arguments[:body]).body
+          end
+        end
+      end
+    end
+  end
+end
---
+++ elasticsearch-xpack/spec/xpack/security/get_api_key_spec.rb
@@ -0,0 +1,56 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#	http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+require 'spec_helper'
+
+describe 'client#security#get_api_key' do
+
+  let(:expected_args) do
+    [
+        'GET',
+        '_security/api_key',
+        params,
+        nil,
+        nil
+    ]
+  end
+
+  let(:params) do
+    {}
+  end
+
+  it 'performs the request' do
+    expect(client_double.security.get_api_key).to eq({})
+  end
+
+  context 'when params are specified' do
+
+    let(:params) do
+      { id: '1',
+        username: 'user',
+        name: 'my-api-key',
+        realm_name: '_es_api_key' }
+    end
+
+    it 'performs the request' do
+      expect(client_double.security.get_api_key(id: '1',
+                                                   username: 'user',
+                                                   name: 'my-api-key',
+                                                   realm_name: '_es_api_key')).to eq({})
+    end
+  end
+end
