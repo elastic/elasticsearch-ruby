44ffc9a4730b4b7f7952e444090d121ed602518b
Emily Stolfo <emstolfo@gmail.com>
2019-03-25 16:44:43 +0100

    [API] Update clear_scroll API

--- elasticsearch-api/lib/elasticsearch/api/actions/clear_scroll.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/clear_scroll.rb
@@ -6,6 +6,7 @@ module Elasticsearch
       #
       # @option arguments [List] :scroll_id A comma-separated list of scroll IDs to clear;
       #                                     use `_all` clear all scroll search contexts
+      # @option arguments [Hash] :body A comma-separated list of scroll IDs to clear if none was specified via the scroll_id parameter
       #
       # @see http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/search-request-search-type.html#clear-scroll
       #
@@ -19,6 +20,12 @@ module Elasticsearch
 
         perform_request(method, path, params, body).body
       end
+
+      # Register this action with its valid params when the module is loaded.
+      #
+      # @since 6.2.0
+      ParamsRegistry.register(:clear_scroll, [
+      ].freeze)
     end
   end
 end
