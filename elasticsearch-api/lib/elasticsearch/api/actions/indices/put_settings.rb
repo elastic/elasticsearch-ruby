0ee286f6f8edd9d2677579e487bcef1a08187fc6
Emily Stolfo <emstolfo@gmail.com>
2019-03-26 12:40:39 +0100

    [API] Fix minor issues with API methods

--- elasticsearch-api/lib/elasticsearch/api/actions/indices/put_settings.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/indices/put_settings.rb
@@ -25,16 +25,16 @@ module Elasticsearch
         #                                 body: { 'index.routing.allocation.require.tag' => 'bigbox' }
         #
         # @option arguments [List] :index A comma-separated list of index names; use `_all` or empty string to perform the operation on all indices
-        # @option arguments [List] :name The name of the settings that should be included
+        # @option arguments [Hash] :body The index settings to be updated (*Required*)
         # @option arguments [Time] :master_timeout Specify timeout for connection to master
+        # @option arguments [Time] :timeout Explicit operation timeout
+        # @option arguments [Boolean] :preserve_existing Whether to update existing settings. If set to `true` existing settings on an index remain unchanged, the default is `false`
         # @option arguments [Boolean] :ignore_unavailable Whether specified concrete indices should be ignored when unavailable (missing or closed)
         # @option arguments [Boolean] :allow_no_indices Whether to ignore if a wildcard indices expression resolves into no concrete indices. (This includes `_all` string or when no indices have been specified)
         # @option arguments [String] :expand_wildcards Whether to expand wildcard expression to concrete indices that are open, closed or both. (options: open, closed, none, all)
         # @option arguments [Boolean] :flat_settings Return settings in flat format (default: false)
-        # @option arguments [Boolean] :local Return local information, do not retrieve the state from master node (default: false)
-        # @option arguments [Boolean] :include_defaults Whether to return all default setting for each of the indices.
         #
-        # @see http://www.elastic.co/guide/en/elasticsearch/reference/master/indices-get-settings.html
+        # @see http://www.elastic.co/guide/en/elasticsearch/reference/master/indices-update-settings.html
         #
         def put_settings(arguments={})
           raise ArgumentError, "Required argument 'body' missing" unless arguments[:body]
@@ -49,14 +49,14 @@ module Elasticsearch
         # Register this action with its valid params when the module is loaded.
         #
         # @since 6.2.0
-        ParamsRegistry.register(:get_settings, [
+        ParamsRegistry.register(:put_settings, [
             :master_timeout,
+            :timeout,
+            :preserve_existing,
             :ignore_unavailable,
             :allow_no_indices,
             :expand_wildcards,
-            :flat_settings,
-            :local,
-            :include_defaults ].freeze)
+            :flat_settings ].freeze)
       end
     end
   end
