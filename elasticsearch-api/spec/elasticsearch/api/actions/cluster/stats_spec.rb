9f520b2b80d6a716dd8cb91cc0caadb2d7054a6d
Emily Stolfo <emstolfo@gmail.com>
2019-03-27 10:17:58 +0100

    [API] Add support for node_id in cluster/stats

--- elasticsearch-api/spec/elasticsearch/api/actions/cluster/stats_spec.rb
+++ elasticsearch-api/spec/elasticsearch/api/actions/cluster/stats_spec.rb
@@ -15,4 +15,21 @@ describe 'client.cluster#stats' do
   it 'performs the request' do
     expect(client_double.cluster.stats).to eq({})
   end
+
+  context 'when a node_id is specified' do
+
+    let(:expected_args) do
+      [
+          'GET',
+          '_cluster/stats/nodes/1',
+          {},
+          nil,
+          nil
+      ]
+    end
+
+    it 'performs the request' do
+      expect(client_double.cluster.stats(node_id: 1)).to eq({})
+    end
+  end
 end
