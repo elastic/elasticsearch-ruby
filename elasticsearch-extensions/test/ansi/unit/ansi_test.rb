209bfc0c17eca244daf669ae4b3a338bb538907d
Emily Stolfo <emstolfo@gmail.com>
2019-04-09 18:26:24 +0200

    Update test and remove reference to get_aliases endpoint

--- elasticsearch-extensions/test/ansi/unit/ansi_test.rb
+++ elasticsearch-extensions/test/ansi/unit/ansi_test.rb
@@ -24,10 +24,10 @@ class Elasticsearch::Extensions::AnsiTest < Elasticsearch::Test::UnitTestCase
       @client = Elasticsearch::Client.new
       @client.stubs(:perform_request).returns \
         Elasticsearch::Transport::Transport::Response.new(200, { "ok" => true, "status" => 200, "name" => "Hit-Maker",
-            "version" => { "number"     => "0.90.7",
-                           "build_hash" => "abc123",
-                           "build_timestamp"=>"2013-11-13T12:06:54Z", "build_snapshot"=>false, "lucene_version"=>"4.5.1" },
-            "tagline"=>"You Know, for Search" })
+                                                                 "version" => { "number"     => "0.90.7",
+                                                                                "build_hash" => "abc123",
+                                                                                "build_timestamp"=>"2013-11-13T12:06:54Z", "build_snapshot"=>false, "lucene_version"=>"4.5.1" },
+                                                                 "tagline"=>"You Know, for Search" })
     end
 
     should "wrap the response" do
@@ -47,7 +47,7 @@ class Elasticsearch::Extensions::AnsiTest < Elasticsearch::Test::UnitTestCase
     should "call the 'awesome_inspect' method when available and no handler found" do
       @client.stubs(:perform_request).returns \
         Elasticsearch::Transport::Transport::Response.new(200, {"index-1"=>{"aliases"=>{}}})
-      response = @client.indices.get_aliases
+      response = @client.cat.aliases
 
       response.instance_eval do
         def awesome_inspect; "---PRETTY---"; end
@@ -58,7 +58,7 @@ class Elasticsearch::Extensions::AnsiTest < Elasticsearch::Test::UnitTestCase
     should "call `to_s` method when no pretty printer or handler found" do
       @client.stubs(:perform_request).returns \
         Elasticsearch::Transport::Transport::Response.new(200, {"index-1"=>{"aliases"=>{}}})
-      response = @client.indices.get_aliases
+      response = @client.cat.aliases
 
       assert_equal '{"index-1"=>{"aliases"=>{}}}', response.to_ansi
     end
