cd8e2ae7e53b5d2808599b9fef838c91ca96138a
Emily Stolfo <emstolfo@gmail.com>
2019-04-30 13:53:40 +0900

    [API] Add indices/get_aliases back

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/indices/get_aliases.rb
@@ -0,0 +1,37 @@
+module Elasticsearch
+  module API
+    module Indices
+      module Actions
+
+        # Get a list of all aliases, or aliases for a specific index.
+        #
+        # @example Get a list of all aliases
+        #
+        #     client.indices.get_aliases
+        #
+        # @option arguments [List] :index A comma-separated list of index names to filter aliases
+        # @option arguments [List] :name A comma-separated list of alias names to filter
+        # @option arguments [Time] :timeout Explicit timestamp for the document
+        # @option arguments [Boolean] :local Return local information,
+        #                                    do not retrieve the state from master node (default: false)
+        #
+        # @see http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/indices-aliases.html
+        #
+        def get_aliases(arguments={})
+          method = HTTP_GET
+          path   = Utils.__pathify Utils.__listify(arguments[:index]), '_aliases', Utils.__listify(arguments[:name])
+
+          params = Utils.__validate_and_extract_params arguments, ParamsRegistry.get(__method__)
+          body   = nil
+
+          perform_request(method, path, params, body).body
+        end
+
+        # Register this action with its valid params when the module is loaded.
+        #
+        # @since 6.2.0
+        ParamsRegistry.register(:get_aliases, [ :timeout, :local ].freeze)
+      end
+    end
+  end
+end
