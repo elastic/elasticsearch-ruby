28e888db8a3d08276d9b45b4ddd8e6a5d30f4152
Emily Stolfo <emstolfo@gmail.com>
2019-06-18 14:15:01 +0200

    [XPACK] Add data_frame API

---
+++ elasticsearch-xpack/lib/elasticsearch/xpack/api/actions/data_frame/preview_data_frame_transform.rb
@@ -0,0 +1,43 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#	http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+module Elasticsearch
+  module XPack
+    module API
+      module DataFrame
+        module Actions
+
+          # Previews a data frame transform.
+          #
+          # @option arguments [Hash] :body The definition for the data_frame transform to preview
+          #
+          # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/preview-data-frame-transform.html
+          #
+          def preview_data_frame_transform(arguments={})
+            raise ArgumentError, "Required argument 'body' missing" unless arguments[:body]
+            method = Elasticsearch::API::HTTP_POST
+            path   = '_data_frame/transforms/_preview'
+            params = {}
+            body   = arguments[:body]
+
+            perform_request(method, path, params, body).body
+          end
+        end
+      end
+    end
+  end
+end
