ebe6e42c0f452abaccb2a5face503f934dfa2c3c
Emily Stolfo <emstolfo@gmail.com>
2019-06-24 13:25:21 +0200

    [CLIENT] Add documentation of 'compression' option on client

--- elasticsearch-transport/lib/elasticsearch/transport/client.rb
+++ elasticsearch-transport/lib/elasticsearch/transport/client.rb
@@ -108,6 +108,8 @@ module Elasticsearch
       #
       # @option arguments [String] :send_get_body_as Specify the HTTP method to use for GET requests with a body.
       #                                              (Default: GET)
+      # @option arguments [true, false] :compression Whether to compress requests. Responses will automatically be
+      #   inflated if they are compressed. Gzip compression will be used. The default is false.
       #
       # @yield [faraday] Access and configure the `Faraday::Connection` instance directly with a block
       #
