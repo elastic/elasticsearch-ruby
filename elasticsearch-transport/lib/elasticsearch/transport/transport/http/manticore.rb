4b06c59cb4e619a7baf296b2bb0db92a82ccfcfb
Emily Stolfo <emstolfo@gmail.com>
2019-06-24 18:10:17 +0200

    [CLIENT] Only add Accept-Encording header if compression option is true

--- elasticsearch-transport/lib/elasticsearch/transport/transport/http/manticore.rb
+++ elasticsearch-transport/lib/elasticsearch/transport/transport/http/manticore.rb
@@ -158,7 +158,7 @@ module Elasticsearch
             headers = (options && options[:headers]) || {}
             headers[CONTENT_TYPE_STR] = find_value(headers, CONTENT_TYPE_REGEX) || DEFAULT_CONTENT_TYPE
             headers[USER_AGENT_STR] = find_value(headers, USER_AGENT_REGEX) || user_agent_header
-            headers[ACCEPT_ENCODING] = GZIP
+            headers[ACCEPT_ENCODING] = GZIP if use_compression?
             request_options.merge!(headers: headers)
           end
 
