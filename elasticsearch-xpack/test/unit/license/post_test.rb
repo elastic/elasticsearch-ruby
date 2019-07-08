ca9f436b5daec76276130446c3de5b5e55cb35b3
Emily Stolfo <emstolfo@gmail.com>
2019-07-08 13:09:05 +0200

    [XPACK] Update tests for path changes for xpack license endpoints

--- elasticsearch-xpack/test/unit/license/post_test.rb
+++ elasticsearch-xpack/test/unit/license/post_test.rb
@@ -27,7 +27,7 @@ module Elasticsearch
         should "perform correct request" do
           subject.expects(:perform_request).with do |method, url, params, body|
             assert_equal 'PUT', method
-            assert_equal '_xpack/license', url
+            assert_equal '_license', url
             assert_equal Hash.new, params
             assert_equal [], body
             true
