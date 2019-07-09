e1df5458d97ae5f0e61208348a2e31601f3e6aed
Emily Stolfo <emstolfo@gmail.com>
2019-07-09 14:56:56 +0200

    [XPACK] Update tests for changed xpack paths

--- elasticsearch-xpack/test/unit/graph/explore_test.rb
+++ elasticsearch-xpack/test/unit/graph/explore_test.rb
@@ -27,7 +27,7 @@ module Elasticsearch
         should "perform correct request" do
           subject.expects(:perform_request).with do |method, url, params, body|
             assert_equal 'GET', method
-            assert_equal '_xpack/_graph/_explore', url
+            assert_equal '_graph/explore', url
             assert_equal Hash.new, params
             assert_equal nil, body
             true
