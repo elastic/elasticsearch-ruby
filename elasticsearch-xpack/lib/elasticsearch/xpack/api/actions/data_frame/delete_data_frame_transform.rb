fb2e96a11934fc118ff9229c921ba0e224f31923
Emily Stolfo <emstolfo@gmail.com>
2019-07-25 16:57:20 +0200

    [XPACK] Add support for :force option in delete_data_frame_transform

--- elasticsearch-xpack/lib/elasticsearch/xpack/api/actions/data_frame/delete_data_frame_transform.rb
+++ elasticsearch-xpack/lib/elasticsearch/xpack/api/actions/data_frame/delete_data_frame_transform.rb
@@ -11,17 +11,23 @@ module Elasticsearch
           # Deletes an existing data frame transform.
           #
           # @option arguments [String] :transform_id The id of the transform to delete. *Required*
+          # @option arguments [Boolean] :force When `true`, the transform is deleted regardless of its current state.
+          #   The default value is `false`, meaning that the transform must be `stopped` before it can be deleted.
           #
           # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/delete-data-frame-transform.html
           #
           # @since 7.2.0
           def delete_data_frame_transform(arguments={})
             raise ArgumentError, "Required argument 'transform_id' missing" unless arguments[:transform_id]
-            transform_id = URI.escape(arguments[:transform_id])
+            arguments = arguments.clone
+            transform_id = URI.escape(arguments.delete(:transform_id))
+
+            valid_params = [
+                :force]
 
             method = Elasticsearch::API::HTTP_DELETE
             path   = "_data_frame/transforms/#{transform_id}"
-            params = {}
+            params = Elasticsearch::API::Utils.__validate_and_extract_params(arguments, valid_params)
             body   = nil
 
             perform_request(method, path, params, body).body
