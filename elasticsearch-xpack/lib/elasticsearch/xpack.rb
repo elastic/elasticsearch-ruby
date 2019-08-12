7797a8a868214016d5fea088cca70c682813f201
Emily Stolfo <emstolfo@gmail.com>
2019-08-12 12:48:42 +0200

    [XPACK] Add ParamsRegistry in each direcotry and for Xpack top-level API

--- elasticsearch-xpack/lib/elasticsearch/xpack.rb
+++ elasticsearch-xpack/lib/elasticsearch/xpack.rb
@@ -4,7 +4,9 @@
 
 require "elasticsearch/api"
 require "elasticsearch/xpack/version"
+require "elasticsearch/xpack/api/actions/params_registry"
 
+Dir[ File.expand_path('../xpack/actions/**/params_registry.rb', __FILE__) ].each   { |f| require f }
 Dir[ File.expand_path('../xpack/api/actions/**/*.rb', __FILE__) ].each   { |f| require f }
 Dir[ File.expand_path('../xpack/api/namespace/**/*.rb', __FILE__) ].each { |f| require f }
 
--- elasticsearch-xpack/lib/elasticsearch/xpack/api/actions/usage.rb
+++ elasticsearch-xpack/lib/elasticsearch/xpack/api/actions/usage.rb
@@ -13,14 +13,19 @@ module Elasticsearch
         #
         #
         def usage(arguments={})
-          valid_params = [ :master_timeout ]
           method = Elasticsearch::API::HTTP_GET
           path   = "_xpack/usage"
-          params = Elasticsearch::API::Utils.__validate_and_extract_params arguments, valid_params
+          params = Elasticsearch::API::Utils.__validate_and_extract_params arguments, ParamsRegistry.get(__method__)
           body   = nil
 
           perform_request(method, path, params, body).body
         end
+
+
+        # Register this action with its valid params when the module is loaded.
+        #
+        # @since 7.4.0
+        ParamsRegistry.register(:usage, [ :master_timeout ].freeze)
       end
     end
   end
