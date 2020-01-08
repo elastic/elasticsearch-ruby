7f7489e66d2c84f308d71b42a5cd1157941a9049
Fernando Briano <fernando@picandocodigo.net>
2020-01-08 11:00:42 +0000

    [API] Generator: run rubocop when updating api source code

--- elasticsearch-api/utils/update_files.sh
+++ elasticsearch-api/utils/update_files.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
+echo "Copying generated files into lib..."
 cp ../tmp/out/api/* ../lib/elasticsearch/api/actions/ -R
+echo "Running Rubocop..."
+rubocop -x ../lib/elasticsearch/api/actions
