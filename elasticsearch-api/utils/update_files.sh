e3dc77e29b036bba449f9ea5a0dd78ad39adbfd4
Fernando Briano <fernando@picandocodigo.net>
2020-01-22 16:58:53 +0000

    [API] Generator: Refactor - remove bash script, move to Thor task

--- elasticsearch-api/utils/update_files.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "Copying generated files into lib..."
-cp ../tmp/out/api/* ../lib/elasticsearch/api/actions/ -R
-echo "Running Rubocop..."
-rubocop -x ../lib/elasticsearch/api/actions
