1cbc077b7c72b23045aea286e8c7526aee9777f7
Fernando Briano <fernando@picandocodigo.net>
2020-02-17 12:57:36 +0000

    [API] Generator: Generate X-Pack

--- elasticsearch-api/utils/thor/generator/files_helper.rb
+++ elasticsearch-api/utils/thor/generator/files_helper.rb
@@ -3,23 +3,30 @@ require 'pathname'
 module Elasticsearch
   module API
     module FilesHelper
-      SRC_PATH = '../../../../../tmp/elasticsearch/rest-api-spec/src/main/resources/rest-api-spec/api/'.freeze
-      OUTPUT_DIR = '../../elasticsearch-api/lib/elasticsearch/api/actions'.freeze
+      OSS_SRC_PATH   = '../../../../../tmp/elasticsearch/rest-api-spec/src/main/resources/rest-api-spec/api/'.freeze
+      OSS_OUTPUT_DIR = '../../elasticsearch-api/lib/elasticsearch/api/actions'.freeze
+
+      XPACK_SRC_PATH   = '../../../../../tmp/elasticsearch/x-pack/plugin/src/test/resources/rest-api-spec/api'.freeze
+      XPACK_OUTPUT_DIR = '../../elasticsearch-xpack/lib/elasticsearch/xpack/api/actions'.freeze
 
       # Path to directory with JSON API specs
-      def self.input_dir
-        input_dir = File.expand_path(SRC_PATH, __FILE__)
+      def self.input_dir(xpack = false)
+        input_dir = if xpack
+                      File.expand_path(XPACK_SRC_PATH, __FILE__)
+                    else
+                      File.expand_path(OSS_SRC_PATH, __FILE__)
+                    end
         Pathname(input_dir)
       end
 
       # Path to directory to copy generated files
-      def self.output_dir
-        Pathname(OUTPUT_DIR)
+      def self.output_dir(xpack = false)
+        xpack ? Pathname(XPACK_OUTPUT_DIR) : Pathname(OSS_OUTPUT_DIR)
       end
 
       # Only get JSON files and remove hidden files
-      def self.files
-        Dir.entries(input_dir.to_s).reject do |f|
+      def self.files(xpack = false)
+        Dir.entries(input_dir(xpack).to_s).reject do |f|
           f.start_with?('.') ||
             f.start_with?('_') ||
             File.extname(f) != '.json'
