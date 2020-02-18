e99a763ab2769f9e61efeb0e7e0aa376fd7a3560
Fernando Briano <fernando@picandocodigo.net>
2020-02-18 16:27:20 +0000

    [XPACK] Update sql endpoints

--- elasticsearch-xpack/lib/elasticsearch/xpack/api/actions/sql/clear_cursor.rb
+++ elasticsearch-xpack/lib/elasticsearch/xpack/api/actions/sql/clear_cursor.rb
@@ -7,23 +7,25 @@ module Elasticsearch
     module API
       module SQL
         module Actions
-
           # TODO: Description
-          #
+
           # @option arguments [Hash] :body Specify the cursor value in the `cursor` element to clean the cursor. (*Required*)
           #
           # @see Clear SQL cursor
           #
-          def clear_cursor(arguments={})
+          def clear_cursor(arguments = {})
             raise ArgumentError, "Required argument 'body' missing" unless arguments[:body]
-            method = Elasticsearch::API::HTTP_POST
-            path   = "_xpack/sql/close"
-            params = {}
-            body   = arguments[:body]
 
+            arguments = arguments.clone
+
+            method = Elasticsearch::API::HTTP_POST
+            path   = "_sql/close"
+            params = {}
+
+            body = arguments[:body]
             perform_request(method, path, params, body).body
           end
-        end
+      end
       end
     end
   end
--- elasticsearch-xpack/lib/elasticsearch/xpack/api/actions/sql/query.rb
+++ elasticsearch-xpack/lib/elasticsearch/xpack/api/actions/sql/query.rb
@@ -7,29 +7,35 @@ module Elasticsearch
     module API
       module SQL
         module Actions
-
           # TODO: Description
+
           #
-          # @option arguments [Hash] :body Use the `query` element to start a query. Use the `cursor` element to continue a query. (*Required*)
           # @option arguments [String] :format a short version of the Accept header, e.g. json, yaml
+
+          # @option arguments [Hash] :body Use the `query` element to start a query. Use the `cursor` element to continue a query. (*Required*)
           #
           # @see Execute SQL
           #
-          def query(arguments={})
+          def query(arguments = {})
             raise ArgumentError, "Required argument 'body' missing" unless arguments[:body]
-            method = Elasticsearch::API::HTTP_POST
-            path   = "_xpack/sql"
-            params = Elasticsearch::API::Utils.__validate_and_extract_params arguments, ParamsRegistry.get(__method__)
-            body   = arguments[:body]
 
+            arguments = arguments.clone
+
+            method = Elasticsearch::API::HTTP_POST
+            path   = "_sql"
+            params = Elasticsearch::API::Utils.__validate_and_extract_params arguments, ParamsRegistry.get(__method__)
+
+            body = arguments[:body]
             perform_request(method, path, params, body).body
           end
 
           # Register this action with its valid params when the module is loaded.
           #
-          # @since 7.4.0
-          ParamsRegistry.register(:query, [ :format ].freeze)
-        end
+          # @since 6.2.0
+          ParamsRegistry.register(:query, [
+            :format
+          ].freeze)
+      end
       end
     end
   end
