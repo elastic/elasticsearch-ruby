ef4e705e012da048aaa87d9069d116e7c3c1c882
Fernando Briano <fernando@picandocodigo.net>
2020-03-27 16:03:10 +0000

    [API] YAML runner minimal cleanup

--- elasticsearch-api/spec/elasticsearch/api/rest_api_yaml_spec.rb
+++ elasticsearch-api/spec/elasticsearch/api/rest_api_yaml_spec.rb
@@ -6,29 +6,23 @@ require 'spec_helper'
 require 'rest_yaml_tests_helper'
 
 describe 'Rest API YAML tests' do
-
+  # Traverse YAML files and create TestFile object:
   REST_API_YAML_FILES.each do |file|
-
     test_file = Elasticsearch::RestAPIYAMLTests::TestFile.new(file, REST_API_YAML_SKIP_FEATURES)
 
     context "#{file.gsub("#{YAML_FILES_DIRECTORY}/", '')}" do
-
       test_file.tests.each do |test|
 
         context "#{test.description}" do
-
           if test.skip_test?(ADMIN_CLIENT)
             skip 'Test contains feature(s) not yet supported or version is not satisfied'
-
           else
-
             let(:client) do
               DEFAULT_CLIENT
             end
 
             # Runs once before each test in a test file
             before(:all) do
-              # Runs once before each test in a test file
               Elasticsearch::RestAPIYAMLTests::TestFile.clear_data(ADMIN_CLIENT)
               test_file.setup(ADMIN_CLIENT)
             end
@@ -39,21 +33,18 @@ describe 'Rest API YAML tests' do
             end
 
             test.task_groups.each do |task_group|
-
               before do
                 task_group.run(client)
               end
 
               # 'catch' is in the task group definition
               if task_group.catch_exception?
-
                 it 'sends the request and throws the expected error' do
                   expect(task_group.exception).to match_error(task_group.expected_exception_message)
                 end
 
                 # 'match' on error description is in the task group definition
                 if task_group.has_match_clauses?
-
                   task_group.match_clauses.each do |match|
                     it 'contains the expected error in the request response' do
                       expect(task_group.exception.message).to match(Regexp.new(Regexp.escape(match['match'].values.first.to_s)))
@@ -64,7 +55,6 @@ describe 'Rest API YAML tests' do
 
                 # 'match' is in the task group definition
                 if task_group.has_match_clauses?
-
                   task_group.match_clauses.each do |match|
                     it "has the expected value (#{match['match'].values.join(',')}) in the response field (#{match['match'].keys.join(',')})" do
                       expect(task_group.response).to match_response(match['match'], test)
@@ -74,7 +64,6 @@ describe 'Rest API YAML tests' do
 
                 # 'length' is in the task group definition
                 if task_group.has_length_match_clauses?
-
                   task_group.length_match_clauses.each do |match|
                     it "the '#{match['length'].keys.join(',')}' field have the expected length" do
                       expect(task_group.response).to match_response_field_length(match['length'], test)
@@ -84,7 +73,6 @@ describe 'Rest API YAML tests' do
 
                 # 'is_true' is in the task group definition
                 if task_group.has_true_clauses?
-
                   task_group.true_clauses.each do |match|
                     it "sends the request and the '#{match['is_true']}' field is set to true" do
                       expect(task_group.response).to match_true_field(match['is_true'], test)
@@ -94,7 +82,6 @@ describe 'Rest API YAML tests' do
 
                 # 'is_false' is in the task group definition
                 if task_group.has_false_clauses?
-
                   task_group.false_clauses.each do |match|
                     it "sends the request and the '#{match['is_false']}' field is set to true" do
                       expect(task_group.response).to match_false_field(match['is_false'], test)
@@ -104,7 +91,6 @@ describe 'Rest API YAML tests' do
 
                 # 'gte' is in the task group definition
                 if task_group.has_gte_clauses?
-
                   task_group.gte_clauses.each do |match|
                     it "sends the request and the '#{match['gte']}' field is greater than or equal to the expected value" do
                       expect(task_group.response).to match_gte_field(match['gte'], test)
@@ -114,7 +100,6 @@ describe 'Rest API YAML tests' do
 
                 # 'gt' is in the task group definition
                 if task_group.has_gt_clauses?
-
                   task_group.gt_clauses.each do |match|
                     it "sends the request and the '#{match['gt']}' field is greater than the expected value" do
                       expect(task_group.response).to match_gt_field(match['gt'], test)
@@ -124,7 +109,6 @@ describe 'Rest API YAML tests' do
 
                 # 'lte' is in the task group definition
                 if task_group.has_lte_clauses?
-
                   task_group.lte_clauses.each do |match|
                     it "sends the request and the '#{match['lte']}' field is less than or equal to the expected value" do
                       expect(task_group.response).to match_lte_field(match['lte'], test)
@@ -134,7 +118,6 @@ describe 'Rest API YAML tests' do
 
                 # 'lt' is in the task group definition
                 if task_group.has_lt_clauses?
-
                   task_group.lt_clauses.each do |match|
                     it "sends the request and the '#{match['lt']}' field is less than the expected value" do
                       expect(task_group.response).to match_lt_field(match['lt'], test)
