351a61c38b682e7d67fa7e006c1166136d01a5ee
Fernando Briano <fernando@picandocodigo.net>
2020-04-03 18:37:56 +0100

    [X-Pack] Adds X-Pack cat Endpoint API

---
+++ elasticsearch-xpack/lib/elasticsearch/xpack/api/actions/cat/ml_datafeeds.rb
@@ -0,0 +1,60 @@
+# Licensed to Elasticsearch B.V under one or more agreements.
+# Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+# See the LICENSE file in the project root for more information
+
+module Elasticsearch
+  module XPack
+    module API
+      module Cat
+        module Actions
+          # TODO: Description
+
+          #
+          # @option arguments [String] :datafeed_id The ID of the datafeeds stats to fetch
+          # @option arguments [Boolean] :allow_no_datafeeds Whether to ignore if a wildcard expression matches no datafeeds. (This includes `_all` string or when no datafeeds have been specified)
+          # @option arguments [String] :format a short version of the Accept header, e.g. json, yaml
+          # @option arguments [List] :h Comma-separated list of column names to display
+          # @option arguments [Boolean] :help Return help information
+          # @option arguments [List] :s Comma-separated list of column names or column aliases to sort by
+          # @option arguments [String] :time The unit in which to display time values
+          #   (options: d (Days),h (Hours),m (Minutes),s (Seconds),ms (Milliseconds),micros (Microseconds),nanos (Nanoseconds))
+
+          # @option arguments [Boolean] :v Verbose mode. Display column headers
+
+          #
+          # @see http://www.elastic.co/guide/en/elasticsearch/reference/current/cat-datafeeds.html
+          #
+          def ml_datafeeds(arguments = {})
+            arguments = arguments.clone
+
+            _datafeed_id = arguments.delete(:datafeed_id)
+
+            method = Elasticsearch::API::HTTP_GET
+            path   = if _datafeed_id
+                       "_cat/ml/datafeeds/#{Elasticsearch::API::Utils.__listify(_datafeed_id)}"
+                     else
+                       "_cat/ml/datafeeds"
+  end
+            params = Elasticsearch::API::Utils.__validate_and_extract_params arguments, ParamsRegistry.get(__method__)
+
+            body = nil
+            perform_request(method, path, params, body).body
+          end
+
+          # Register this action with its valid params when the module is loaded.
+          #
+          # @since 6.2.0
+          ParamsRegistry.register(:ml_datafeeds, [
+            :allow_no_datafeeds,
+            :format,
+            :h,
+            :help,
+            :s,
+            :time,
+            :v
+          ].freeze)
+      end
+    end
+    end
+  end
+end
