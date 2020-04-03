7464732295073424db518455f02a330868cb228a
Fernando Briano <fernando@picandocodigo.net>
2020-04-03 17:14:50 +0100

    [X-Pack] Adds transform endpoint

---
+++ elasticsearch-xpack/lib/elasticsearch/xpack/api/actions/transform/preview_transform.rb
@@ -0,0 +1,32 @@
+# Licensed to Elasticsearch B.V under one or more agreements.
+# Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+# See the LICENSE file in the project root for more information
+
+module Elasticsearch
+  module XPack
+    module API
+      module Transform
+        module Actions
+          # TODO: Description
+
+          # @option arguments [Hash] :body The definition for the transform to preview (*Required*)
+          #
+          # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/preview-transform.html
+          #
+          def preview_transform(arguments = {})
+            raise ArgumentError, "Required argument 'body' missing" unless arguments[:body]
+
+            arguments = arguments.clone
+
+            method = Elasticsearch::API::HTTP_POST
+            path   = "_transform/_preview"
+            params = {}
+
+            body = arguments[:body]
+            perform_request(method, path, params, body).body
+          end
+      end
+    end
+    end
+  end
+end
---
+++ elasticsearch-xpack/lib/elasticsearch/xpack/api/namespace/transform.rb
@@ -0,0 +1,22 @@
+# Licensed to Elasticsearch B.V under one or more agreements.
+# Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+# See the LICENSE file in the project root for more information
+
+module Elasticsearch
+  module XPack
+    module API
+      module Transform
+        module Actions; end
+
+        class TransformClient
+          include Elasticsearch::API::Common::Client, Elasticsearch::API::Common::Client::Base, Transform::Actions
+        end
+
+        def transform
+          @transform ||= TransformClient.new(self)
+        end
+
+      end
+    end
+  end
+end
