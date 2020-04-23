06e3b29f8a16cf9e995c1633b5d533e50e4b6417
Fernando Briano <fernando@picandocodigo.net>
2020-04-23 15:24:51 +0100

    Add missing license headers

--- examples/rabbitmq/consumer-publisher.rb
+++ examples/rabbitmq/consumer-publisher.rb
@@ -1,3 +1,7 @@
+# Licensed to Elasticsearch B.V under one or more agreements.
+# Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+# See the LICENSE file in the project root for more information
+
 # Example of listening to a RabbitMQ queue and indexing the payload
 #
 # Usage:
