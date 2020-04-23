9ea9f4f28ae27b01ba1c108d22f1c1518586be98
Fernando Briano <fernando@picandocodigo.net>
2020-04-23 14:49:22 +0100

    Add missing license headers

--- elasticsearch-api/utils/thor/generator/endpoint_specifics.rb
+++ elasticsearch-api/utils/thor/generator/endpoint_specifics.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # Licensed to Elasticsearch B.V under one or more agreements.
 # Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 # See the LICENSE file in the project root for more information
