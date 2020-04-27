7b76546112cbddbdf9f84cdcd252bce73572831d
Fernando Briano <fernando@picandocodigo.net>
2020-04-27 11:29:51 +0100

    Add missing license headers (#858)

--- elasticsearch-extensions/test/test_helper.rb
+++ elasticsearch-extensions/test/test_helper.rb
@@ -1,3 +1,7 @@
+# Licensed to Elasticsearch B.V under one or more agreements.
+# Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+# See the LICENSE file in the project root for more information
+
 JRUBY    = defined?(JRUBY_VERSION)
 
 ELASTICSEARCH_HOSTS = if hosts = ENV['TEST_ES_SERVER'] || ENV['ELASTICSEARCH_HOSTS']
--- elasticsearch-xpack/test/test_helper.rb
+++ elasticsearch-xpack/test/test_helper.rb
@@ -1,3 +1,7 @@
+# Licensed to Elasticsearch B.V under one or more agreements.
+# Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+# See the LICENSE file in the project root for more information
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 require 'pathname'
