d549feabe1467dbcf0ae2aa1432e9098e4d9a6b5
Fernando Briano <fernando@picandocodigo.net>
2020-06-11 13:49:43 +0100

    [XPACK] Adds enrich endpoints

---
+++ elasticsearch-xpack/lib/elasticsearch/xpack/api/actions/enrich/delete_policy.rb
@@ -0,0 +1,50 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+module Elasticsearch
+  module XPack
+    module API
+      module Enrich
+        module Actions
+          # Deletes an existing enrich policy and its enrich index.
+          #
+          # @option arguments [String] :name The name of the enrich policy
+          # @option arguments [Hash] :headers Custom HTTP headers
+          #
+          # @see https://www.elastic.co/guide/en/elasticsearch/reference/7.x/delete-enrich-policy-api.html
+          #
+          def delete_policy(arguments = {})
+            raise ArgumentError, "Required argument 'name' missing" unless arguments[:name]
+
+            headers = arguments.delete(:headers) || {}
+
+            arguments = arguments.clone
+
+            _name = arguments.delete(:name)
+
+            method = Elasticsearch::API::HTTP_DELETE
+            path   = "_enrich/policy/#{Elasticsearch::API::Utils.__listify(_name)}"
+            params = {}
+
+            body = nil
+            perform_request(method, path, params, body, headers).body
+          end
+      end
+    end
+    end
+  end
+end
