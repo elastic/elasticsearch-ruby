99d7f0b7d368b569c3b72b262c6a4b9858966686
Fernando Briano <fernando@picandocodigo.net>
2020-06-11 11:30:09 +0100

    [XPACK] Adds Cross-Cluster Replication endpoints

---
+++ elasticsearch-xpack/lib/elasticsearch/xpack/api/actions/cross_cluster_replication/pause_auto_follow_pattern.rb
@@ -0,0 +1,50 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+module Elasticsearch
+  module XPack
+    module API
+      module CrossClusterReplication
+        module Actions
+          # Pauses an auto-follow pattern
+          #
+          # @option arguments [String] :name The name of the auto follow pattern that should pause discovering new indices to follow.
+          # @option arguments [Hash] :headers Custom HTTP headers
+          #
+          # @see https://www.elastic.co/guide/en/elasticsearch/reference/7.x/ccr-pause-auto-follow-pattern.html
+          #
+          def pause_auto_follow_pattern(arguments = {})
+            raise ArgumentError, "Required argument 'name' missing" unless arguments[:name]
+
+            headers = arguments.delete(:headers) || {}
+
+            arguments = arguments.clone
+
+            _name = arguments.delete(:name)
+
+            method = Elasticsearch::API::HTTP_POST
+            path   = "_ccr/auto_follow/#{Elasticsearch::API::Utils.__listify(_name)}/pause"
+            params = {}
+
+            body = nil
+            perform_request(method, path, params, body, headers).body
+          end
+      end
+    end
+    end
+  end
+end
