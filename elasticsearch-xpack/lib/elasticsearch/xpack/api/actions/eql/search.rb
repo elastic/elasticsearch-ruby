58fd52e9abe96a0309f287848ce39e86a7b4f5f1
Fernando Briano <fernando@picandocodigo.net>
2020-06-12 11:30:55 +0100

    [XPACK] Adds EQL endpoint

---
+++ elasticsearch-xpack/lib/elasticsearch/xpack/api/actions/eql/search.rb
@@ -0,0 +1,53 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+module Elasticsearch
+  module XPack
+    module API
+      module Eql
+        module Actions
+          # Returns results matching a query expressed in Event Query Language (EQL)
+          #
+          # @option arguments [String] :index The name of the index to scope the operation
+          # @option arguments [Hash] :headers Custom HTTP headers
+          # @option arguments [Hash] :body Eql request body. Use the `query` to limit the query scope. (*Required*)
+          #
+          # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/eql-search-api.html
+          #
+          def search(arguments = {})
+            raise ArgumentError, "Required argument 'body' missing" unless arguments[:body]
+            raise ArgumentError, "Required argument 'index' missing" unless arguments[:index]
+
+            headers = arguments.delete(:headers) || {}
+
+            arguments = arguments.clone
+            arguments[:index] = UNDERSCORE_ALL if !arguments[:index] && arguments[:type]
+
+            _index = arguments.delete(:index)
+
+            method = Elasticsearch::API::HTTP_GET
+            path   = "#{Elasticsearch::API::Utils.__listify(_index)}/_eql/search"
+            params = {}
+
+            body = arguments[:body]
+            perform_request(method, path, params, body, headers).body
+          end
+      end
+    end
+    end
+  end
+end
