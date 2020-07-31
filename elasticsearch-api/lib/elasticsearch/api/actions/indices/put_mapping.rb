5db35f3d7ac3d240c70fca593ed0c9ca2f1dc77a
Fernando Briano <fernando@picandocodigo.net>
2020-07-31 10:19:46 +0100

    [API] Regenerate code with doc improvements

--- elasticsearch-api/lib/elasticsearch/api/actions/indices/put_mapping.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/indices/put_mapping.rb
@@ -22,14 +22,13 @@ module Elasticsearch
         # Updates the index mappings.
         #
         # @option arguments [List] :index A comma-separated list of index names the mapping should be added to (supports wildcards); use `_all` or omit to add the mapping on all indices.
-        # @option arguments [String] :type The name of the document type   *Deprecated*
+        # @option arguments [String] :type The name of the document type *Deprecated*
         # @option arguments [Boolean] :include_type_name Whether a type should be expected in the body of the mappings.
         # @option arguments [Time] :timeout Explicit operation timeout
         # @option arguments [Time] :master_timeout Specify timeout for connection to master
         # @option arguments [Boolean] :ignore_unavailable Whether specified concrete indices should be ignored when unavailable (missing or closed)
         # @option arguments [Boolean] :allow_no_indices Whether to ignore if a wildcard indices expression resolves into no concrete indices. (This includes `_all` string or when no indices have been specified)
-        # @option arguments [String] :expand_wildcards Whether to expand wildcard expression to concrete indices that are open, closed or both.
-        #   (options: open,closed,hidden,none,all)
+        # @option arguments [String] :expand_wildcards Whether to expand wildcard expression to concrete indices that are open, closed or both. (options: open, closed, hidden, none, all)
         # @option arguments [Boolean] :write_index_only When true, applies mappings only to the write index of an alias or data stream
         # @option arguments [Hash] :headers Custom HTTP headers
         # @option arguments [Hash] :body The mapping definition (*Required*)
