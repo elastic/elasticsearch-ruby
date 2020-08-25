1d683589cd488d4f23051ee69d03289bb54909e0
Fernando Briano <fernando@picandocodigo.net>
2020-08-25 22:32:48 +0100

    [XPACK] Adds open_point_in_time and close_point_in_time endpoints

---
+++ elasticsearch-xpack/lib/elasticsearch/xpack/api/actions/open_point_in_time.rb
@@ -0,0 +1,66 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+module Elasticsearch
+  module XPack
+    module API
+      module Actions
+        # Open a point in time that can be used in subsequent searches
+        #
+        # @option arguments [List] :index A comma-separated list of index names to open point in time; use `_all` or empty string to perform the operation on all indices
+        # @option arguments [String] :preference Specify the node or shard the operation should be performed on (default: random)
+        # @option arguments [String] :routing Specific routing value
+        # @option arguments [Boolean] :ignore_unavailable Whether specified concrete indices should be ignored when unavailable (missing or closed)
+        # @option arguments [String] :expand_wildcards Whether to expand wildcard expression to concrete indices that are open, closed or both. (options: open, closed, hidden, none, all)
+        # @option arguments [String] :keep_alive Specific the time to live for the point in time
+        # @option arguments [Hash] :headers Custom HTTP headers
+        #
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/master/point-in-time.html
+        #
+        def open_point_in_time(arguments = {})
+          headers = arguments.delete(:headers) || {}
+
+          arguments = arguments.clone
+
+          _index = arguments.delete(:index)
+
+          method = Elasticsearch::API::HTTP_POST
+          path   = if _index
+                     "#{Elasticsearch::API::Utils.__listify(_index)}/_pit"
+                   else
+                     "_pit"
+                   end
+          params = Elasticsearch::API::Utils.__validate_and_extract_params arguments, ParamsRegistry.get(__method__)
+
+          body = nil
+          perform_request(method, path, params, body, headers).body
+        end
+
+        # Register this action with its valid params when the module is loaded.
+        #
+        # @since 6.2.0
+        ParamsRegistry.register(:open_point_in_time, [
+          :preference,
+          :routing,
+          :ignore_unavailable,
+          :expand_wildcards,
+          :keep_alive
+        ].freeze)
+      end
+    end
+  end
+end
