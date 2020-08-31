3d714c43079d968188df06de33ff00ab172c8fa9
Fernando Briano <fernando@picandocodigo.net>
2020-08-31 20:04:51 +0100

    [CLIENT] Adds test for trailing $ sign in cloud_id

--- elasticsearch-transport/spec/elasticsearch/transport/client_spec.rb
+++ elasticsearch-transport/spec/elasticsearch/transport/client_spec.rb
@@ -325,7 +325,11 @@ describe Elasticsearch::Transport::Client do
 
   context 'when cloud credentials are provided' do
     let(:client) do
-      described_class.new(cloud_id: 'name:bG9jYWxob3N0JGFiY2QkZWZnaA==', user: 'elastic', password: 'changeme')
+      described_class.new(
+        cloud_id: 'name:bG9jYWxob3N0JGFiY2QkZWZnaA==',
+        user: 'elastic',
+        password: 'changeme'
+      )
     end
 
     let(:hosts) do
@@ -341,12 +345,19 @@ describe Elasticsearch::Transport::Client do
     end
 
     it 'creates the correct full url' do
-      expect(client.transport.__full_url(client.transport.hosts[0])).to eq('https://elastic:changeme@abcd.localhost:9243')
+      expect(
+        client.transport.__full_url(client.transport.hosts[0])
+      ).to eq('https://elastic:changeme@abcd.localhost:9243')
     end
 
     context 'when a port is specified' do
       let(:client) do
-        described_class.new(cloud_id: 'name:bG9jYWxob3N0JGFiY2QkZWZnaA==', user: 'elastic', password: 'changeme', port: 9250 )
+        described_class.new(
+          cloud_id: 'name:bG9jYWxob3N0JGFiY2QkZWZnaA==',
+          user: 'elastic',
+          password: 'changeme',
+          port: 9250
+        )
       end
 
       it 'sets the specified port along with the cloud credentials' do
@@ -364,7 +375,11 @@ describe Elasticsearch::Transport::Client do
 
     context 'when the cluster has alternate names' do
       let(:client) do
-        described_class.new(cloud_id: 'myCluster:bG9jYWxob3N0JGFiY2QkZWZnaA==', user: 'elasticfantastic', password: 'tobechanged')
+        described_class.new(
+          cloud_id: 'myCluster:bG9jYWxob3N0JGFiY2QkZWZnaA==',
+          user: 'elasticfantastic',
+          password: 'tobechanged'
+        )
       end
 
       let(:hosts) do
@@ -380,9 +395,38 @@ describe Elasticsearch::Transport::Client do
       end
 
       it 'creates the correct full url' do
-        expect(client.transport.__full_url(client.transport.hosts[0])).to eq('https://elasticfantastic:tobechanged@abcd.localhost:9243')
+        expect(
+          client.transport.__full_url(client.transport.hosts[0])
+        ).to eq('https://elasticfantastic:tobechanged@abcd.localhost:9243')
+      end
+    end
+
+    context 'when decoded cloud id has a trailing dollar sign' do
+      let(:client) do
+        described_class.new(
+          cloud_id: 'a_cluster:bG9jYWxob3N0JGFiY2Qk',
+          user: 'elasticfantastic',
+          password: 'changeme'
+        )
       end
 
+      let(:hosts) do
+        client.transport.hosts
+      end
+
+      it 'extracts the cloud credentials' do
+        expect(hosts[0][:host]).to eq('abcd.localhost')
+        expect(hosts[0][:protocol]).to eq('https')
+        expect(hosts[0][:user]).to eq('elasticfantastic')
+        expect(hosts[0][:password]).to eq('changeme')
+        expect(hosts[0][:port]).to eq(9243)
+      end
+
+      it 'creates the correct full url' do
+        expect(
+          client.transport.__full_url(client.transport.hosts[0])
+        ).to eq('https://elasticfantastic:changeme@abcd.localhost:9243')
+      end
     end
   end
 
