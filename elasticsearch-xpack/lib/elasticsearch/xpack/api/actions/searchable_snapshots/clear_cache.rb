cba1563a7345b5f30835c8f05983d70ca7d2f5df
Fernando Briano <fernando@picandocodigo.net>
2020-10-19 16:16:40 +0100

    [XPACK] Update API removing duplicate documented parameters

--- elasticsearch-xpack/lib/elasticsearch/xpack/api/actions/searchable_snapshots/clear_cache.rb
+++ elasticsearch-xpack/lib/elasticsearch/xpack/api/actions/searchable_snapshots/clear_cache.rb
@@ -30,7 +30,6 @@ module Elasticsearch
           # @option arguments [Boolean] :ignore_unavailable Whether specified concrete indices should be ignored when unavailable (missing or closed)
           # @option arguments [Boolean] :allow_no_indices Whether to ignore if a wildcard indices expression resolves into no concrete indices. (This includes `_all` string or when no indices have been specified)
           # @option arguments [String] :expand_wildcards Whether to expand wildcard expression to concrete indices that are open, closed or both. (options: open, closed, none, all)
-          # @option arguments [List] :index A comma-separated list of index name to limit the operation
           # @option arguments [Hash] :headers Custom HTTP headers
           #
           # @see https://www.elastic.co/guide/en/elasticsearch/reference/7.x/searchable-snapshots-apis.html
