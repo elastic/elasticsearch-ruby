68b9287835abc49bbcf3a058b5d08947c196ab6e
Fernando Briano <fernando@picandocodigo.net>
2020-10-21 11:25:34 +0100

    [XPACK] Adds security.grant_api_key endpoint

---
+++ elasticsearch-xpack/lib/elasticsearch/xpack/api/actions/security/grant_api_key.rb
@@ -0,0 +1,56 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+module Elasticsearch
+  module XPack
+    module API
+      module Security
+        module Actions
+          # Creates an API key on behalf of another user.
+          #
+          # @option arguments [String] :refresh If `true` (the default) then refresh the affected shards to make this operation visible to search, if `wait_for` then wait for a refresh to make this operation visible to search, if `false` then do nothing with refreshes. (options: true, false, wait_for)
+          # @option arguments [Hash] :headers Custom HTTP headers
+          # @option arguments [Hash] :body The api key request to create an API key (*Required*)
+          #
+          # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/security-api-grant-api-key.html
+          #
+          def grant_api_key(arguments = {})
+            raise ArgumentError, "Required argument 'body' missing" unless arguments[:body]
+
+            headers = arguments.delete(:headers) || {}
+
+            arguments = arguments.clone
+
+            method = Elasticsearch::API::HTTP_POST
+            path   = "_security/api_key/grant"
+            params = Elasticsearch::API::Utils.__validate_and_extract_params arguments, ParamsRegistry.get(__method__)
+
+            body = arguments[:body]
+            perform_request(method, path, params, body, headers).body
+          end
+
+          # Register this action with its valid params when the module is loaded.
+          #
+          # @since 6.2.0
+          ParamsRegistry.register(:grant_api_key, [
+            :refresh
+          ].freeze)
+        end
+      end
+    end
+  end
+end
