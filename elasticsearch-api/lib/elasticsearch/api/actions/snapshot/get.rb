19264902b1b453677daa1f1243944bfae39715f1
Fernando Briano <fernando@picandocodigo.net>
2021-04-20 22:43:40 +0100

    [API] Adds index_details parameter to snapshot.get

--- elasticsearch-api/lib/elasticsearch/api/actions/snapshot/get.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/snapshot/get.rb
@@ -25,6 +25,7 @@ module Elasticsearch
         # @option arguments [List] :snapshot A comma-separated list of snapshot names
         # @option arguments [Time] :master_timeout Explicit operation timeout for connection to master node
         # @option arguments [Boolean] :ignore_unavailable Whether to ignore unavailable snapshots, defaults to false which means a SnapshotMissingException is thrown
+        # @option arguments [Boolean] :index_details Whether to include details of each index in the snapshot, if those details are available. Defaults to false.
         # @option arguments [Boolean] :verbose Whether to show verbose snapshot info or only show the basic info found in the repository index blob
         # @option arguments [Hash] :headers Custom HTTP headers
         #
@@ -60,6 +61,7 @@ module Elasticsearch
         ParamsRegistry.register(:get, [
           :master_timeout,
           :ignore_unavailable,
+          :index_details,
           :verbose
         ].freeze)
       end
