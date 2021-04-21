a9879694c63c27edc5152c32504261132b447c47
Fernando Briano <fernando@picandocodigo.net>
2021-04-21 10:03:47 +0100

    [API] Adds service accounts endpoints

---
+++ elasticsearch-xpack/lib/elasticsearch/xpack/api/actions/security/get_service_accounts.rb
@@ -0,0 +1,61 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+module Elasticsearch
+  module XPack
+    module API
+      module Security
+        module Actions
+          # Retrieves information about service accounts.
+          # This functionality is in Beta and is subject to change. The design and
+          # code is less mature than official GA features and is being provided
+          # as-is with no warranties. Beta features are not subject to the support
+          # SLA of official GA features.
+          #
+          # @option arguments [String] :namespace An identifier for the namespace
+          # @option arguments [String] :service An identifier for the service name
+          # @option arguments [Hash] :headers Custom HTTP headers
+          #
+          # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/security-api-get-service-accounts.html
+          #
+          def get_service_accounts(arguments = {})
+            headers = arguments.delete(:headers) || {}
+
+            arguments = arguments.clone
+
+            _namespace = arguments.delete(:namespace)
+
+            _service = arguments.delete(:service)
+
+            method = Elasticsearch::API::HTTP_GET
+            path   = if _namespace && _service
+                       "_security/service/#{Elasticsearch::API::Utils.__listify(_namespace)}/#{Elasticsearch::API::Utils.__listify(_service)}"
+                     elsif _namespace
+                       "_security/service/#{Elasticsearch::API::Utils.__listify(_namespace)}"
+                     else
+                       "_security/service"
+                     end
+            params = {}
+
+            body = nil
+            perform_request(method, path, params, body, headers).body
+          end
+        end
+      end
+    end
+  end
+end
