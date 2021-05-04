2451c50336bafb33ac282f49362225e1d7702357
Fernando Briano <fernando@picandocodigo.net>
2021-05-04 11:25:50 +0100

    Cleanup

--- elasticsearch-api/spec/rest_yaml_tests_helper.rb
+++ elasticsearch-api/spec/rest_yaml_tests_helper.rb
@@ -51,7 +51,6 @@ YAML_FILES_DIRECTORY = "#{PROJECT_PATH}/../tmp/rest-api-spec/test/free"
 
 SINGLE_TEST = if ENV['SINGLE_TEST'] && !ENV['SINGLE_TEST'].empty?
                 test_target = ENV['SINGLE_TEST']
-                path = File.expand_path(File.dirname('..'), '..')
 
                 if test_target.match?(/\.yml$/)
                   ["#{PROJECT_PATH}/../tmp/rest-api-spec/test/free/#{test_target}"]
