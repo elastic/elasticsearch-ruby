e1469593003f3f31c35f0ccab4e13faaae4a6386
Fernando Briano <fernando@picandocodigo.net>
2021-06-03 15:26:14 +0100

    [GEM] Refactors Gem and Transport relationship
    Initial commit on extracting Elasticsearch specific code from elasticsearch-transport and into the
    elasticsearch gem. We're now using transport for elastic-enterprise-search too.

--- elasticsearch-transport/lib/elasticsearch/transport/client.rb
+++ elasticsearch-transport/lib/elasticsearch/transport/client.rb
@@ -179,7 +179,6 @@ module Elasticsearch
                        end
         end
       end
-
       # Performs a request through delegation to {#transport}.
       #
       def perform_request(method, path, params = {}, body = nil, headers = nil)
