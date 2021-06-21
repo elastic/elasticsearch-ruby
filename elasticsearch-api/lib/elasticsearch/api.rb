98b3c5b26be641d8c6b3394ae76d8073e7551a1f
Fernando Briano <fernando@picandocodigo.net>
2021-06-21 17:21:23 +0100

    [API] Starts writing aliases from x-pack to api

--- elasticsearch-api/lib/elasticsearch/api.rb
+++ elasticsearch-api/lib/elasticsearch/api.rb
@@ -80,7 +80,31 @@ module Elasticsearch
                 Elasticsearch::API::Remote,
                 Elasticsearch::API::DanglingIndices,
                 Elasticsearch::API::Features,
-                Elasticsearch::API::Shutdown
+                Elasticsearch::API::Shutdown,
+                Elasticsearch::API::AsyncSearch,
+                Elasticsearch::API::Autoscaling,
+                Elasticsearch::API::CrossClusterReplication,
+                Elasticsearch::API::DataFrameTransformDeprecated,
+                Elasticsearch::API::Enrich,
+                Elasticsearch::API::Eql,
+                Elasticsearch::API::Fleet,
+                Elasticsearch::API::Graph,
+                Elasticsearch::API::IndexLifecycleManagement,
+                Elasticsearch::API::License,
+                Elasticsearch::API::Logstash,
+                Elasticsearch::API::Migration,
+                Elasticsearch::API::MachineLearning,
+                Elasticsearch::API::Monitoring,
+                Elasticsearch::API::Rollup,
+                Elasticsearch::API::SearchableSnapshots,
+                Elasticsearch::API::Security,
+                Elasticsearch::API::SnapshotLifecycleManagement,
+                Elasticsearch::API::SQL,
+                Elasticsearch::API::SSL,
+                Elasticsearch::API::TextStructure,
+                Elasticsearch::API::Transform,
+                Elasticsearch::API::Watcher,
+                Elasticsearch::API::XPack
     end
 
     # The serializer class
--- elasticsearch-xpack/lib/elasticsearch/xpack.rb
+++ elasticsearch-xpack/lib/elasticsearch/xpack.rb
@@ -16,131 +16,20 @@
 # under the License.
 
 require 'elasticsearch/api'
-require 'elasticsearch/xpack/version'
-require 'elasticsearch/xpack/api/actions/params_registry'
+
+# Since XPack is being deprecated, alias Elasticsearch::XPack::API to Elasticsearch::API:
+Elasticsearch::XPack::API = Elasticsearch::API
 
 module Elasticsearch
-  module Transport
-    class Client
-      # When a method is called on the client, if it's one of the xpack root
-      # namespace methods, send them to the xpack client.
-      # E.g.: client.xpack.usage => client.usage
-      # Excluding `info` since OSS and XPACK both have info endpoints.
-      TOP_LEVEL_METHODS = [
-        :usage,
-        :terms_enum
-      ].freeze
-
-      def method_missing(method, *args, &block)
-        return xpack.send(method, *args, &block) if TOP_LEVEL_METHODS.include?(method)
-
-        super
-      end
-
-      def respond_to_missing?(method_name, *args)
-        TOP_LEVEL_METHODS.include?(method_name) || super
-      end
-
-      def xpack
-        warn('Warning: The XPack namespace is being deprecated and will go away in version 8.0.0')
-        @xpack ||= Elasticsearch::XPack::API::Client.new(self)
-      end
-
-      def security
-        @security ||= xpack.security
-      end
-
-      def ml
-        @ml ||= xpack.ml
-      end
-
-      def rollup
-        @rollup ||= xpack.rollup
-      end
-
-      def watcher
-        @watcher ||= xpack.watcher
-      end
-
-      def graph
-        @graph ||= xpack.graph
-      end
-
-      def migration
-        @migration ||= xpack.migration
-      end
-
-      def sql
-        @sql ||= xpack.sql
-      end
-
-      def deprecation
-        @deprecation ||= xpack.deprecation
-      end
-
-      def data_frame
-        @data_frame ||= xpack.data_frame
-      end
-
-      def ilm
-        @ilm ||= xpack.ilm
-      end
-
-      def license
-        @license ||= xpack.license
-      end
-
-      def transform
-        @transform ||= xpack.transform
-      end
-
-      def async_search
-        @async_search ||= xpack.async_search
-      end
-
-      def cat
-        @cat ||= xpack.cat
-      end
-
-      def indices
-        @indices ||= xpack.indices
-      end
-
-      def searchable_snapshots
-        @searchable_snapshots ||= xpack.searchable_snapshots
-      end
-
-      def cross_cluster_replication
-        @cross_cluster_replication ||= xpack.cross_cluster_replication
-      end
-
-      def autoscaling
-        @autoscaling ||= xpack.autoscaling
-      end
-
-      def enrich
-        @enrich ||= xpack.enrich
-      end
-
-      def eql
-        @eql ||= xpack.eql
-      end
-
-      def snapshot_lifecycle_management
-        @snapshot_lifecycle_management ||= xpack.snapshot_lifecycle_management
-      end
-
-      def text_structure
-        @text_structure ||= xpack.text_structure
-      end
-
-      def logstash
-        @logstash ||= xpack.logstash
-      end
-
-      def fleet
-        @fleet ||= xpack.fleet
+  if defined?(Elasticsearch::Transport::Client)
+    module Transport
+      class Client
+        # The client.xpack namespace
+        def xpack
+          warn('Warning: The XPack namespace is being deprecated and will go away in version 8.0.0')
+          @xpack ||= Elasticsearch::API::Client.new(self)
+        end
       end
     end
   end
-end if defined?(Elasticsearch::Transport::Client)
+end
