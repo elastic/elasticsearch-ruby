3db1d835bffaf463f280f0fa402ad46251aa8c40
Seth Michael Larson <seth.larson@elastic.co>
2021-09-01 11:56:41 -0500

    Bumps 7.14.1 to 7.14.2

--- elasticsearch-api/lib/elasticsearch/api/version.rb
+++ elasticsearch-api/lib/elasticsearch/api/version.rb
@@ -17,6 +17,6 @@
 
 module Elasticsearch
   module API
-    VERSION = '7.14.1'.freeze
+    VERSION = '7.14.2'.freeze
   end
 end
--- elasticsearch-transport/lib/elasticsearch/transport/version.rb
+++ elasticsearch-transport/lib/elasticsearch/transport/version.rb
@@ -17,6 +17,6 @@
 
 module Elasticsearch
   module Transport
-    VERSION = '7.14.1'.freeze
+    VERSION = '7.14.2'.freeze
   end
 end
