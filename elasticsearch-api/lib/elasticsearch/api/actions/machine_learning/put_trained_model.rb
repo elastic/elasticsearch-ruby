577dc7cc93d8cc06dcccc2ba6e7f5847eac6e8f8
Fernando Briano <fernando@picandocodigo.net>
2021-09-09 17:07:28 +0100

    [API] Adds defer_definition_decompression to ml.put_trained_model

--- elasticsearch-api/lib/elasticsearch/api/actions/machine_learning/put_trained_model.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/machine_learning/put_trained_model.rb
@@ -22,6 +22,7 @@ module Elasticsearch
         # Creates an inference trained model.
         #
         # @option arguments [String] :model_id The ID of the trained models to store
+        # @option arguments [Boolean] :defer_definition_decompression If set to `true` and a `compressed_definition` is provided, the request defers definition decompression and skips relevant validations.
         # @option arguments [Hash] :headers Custom HTTP headers
         # @option arguments [Hash] :body The trained model configuration (*Required*)
         #
@@ -39,11 +40,18 @@ module Elasticsearch
 
           method = Elasticsearch::API::HTTP_PUT
           path   = "_ml/trained_models/#{Utils.__listify(_model_id)}"
-          params = {}
+          params = Utils.__validate_and_extract_params arguments, ParamsRegistry.get(__method__)
 
           body = arguments[:body]
           perform_request(method, path, params, body, headers).body
         end
+
+        # Register this action with its valid params when the module is loaded.
+        #
+        # @since 6.2.0
+        ParamsRegistry.register(:put_trained_model, [
+          :defer_definition_decompression
+        ].freeze)
       end
     end
   end
