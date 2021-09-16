606a58d3b97a1a4b7f4b1466bad5ffaa784c37a4
Fernando Briano <fernando@picandocodigo.net>
2021-09-16 10:59:31 +0100

    [API] Updates elasticsearch-api for elastic-transport

--- elasticsearch-api/api-spec-testing/rspec_matchers.rb
+++ elasticsearch-api/api-spec-testing/rspec_matchers.rb
@@ -295,12 +295,12 @@ RSpec::Matchers.define :match_error do |expected_error|
       message =~ /\[400\]/ ||
           actual_error.is_a?(ArgumentError)
     when 'unauthorized'
-      actual_error.is_a?(Elasticsearch::Transport::Transport::Errors::Unauthorized)
+      actual_error.is_a?(Elastic::Transport::Transport::Errors::Unauthorized)
     when 'forbidden'
-      actual_error.is_a?(Elasticsearch::Transport::Transport::Errors::Forbidden)
+      actual_error.is_a?(Elastic::Transport::Transport::Errors::Forbidden)
     when /error parsing field/
       message =~ /\[400\]/ ||
-        actual_error.is_a?(Elasticsearch::Transport::Transport::Errors::BadRequest)
+        actual_error.is_a?(Elastic::Transport::Transport::Errors::BadRequest)
     else
       message =~ /#{expected_error}/
     end
