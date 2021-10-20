61ccaab9c22bc515b4c3de9d2de1a874a774c56b
Fernando Briano <fernando@picandocodigo.net>
2021-10-20 11:58:25 +0100

    [API] Adds indices.modify_data_stream

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/indices/modify_data_stream.rb
@@ -0,0 +1,47 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+module Elasticsearch
+  module API
+    module Indices
+      module Actions
+        # Modifies a data stream
+        #
+        # @option arguments [Hash] :headers Custom HTTP headers
+        # @option arguments [Hash] :body The data stream modifications (*Required*)
+        #
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/master/data-streams.html
+        #
+        def modify_data_stream(arguments = {})
+          raise ArgumentError, "Required argument 'body' missing" unless arguments[:body]
+
+          headers = arguments.delete(:headers) || {}
+
+          body = arguments.delete(:body)
+
+          arguments = arguments.clone
+
+          method = Elasticsearch::API::HTTP_POST
+          path   = "_data_stream/_modify"
+          params = {}
+
+          perform_request(method, path, params, body, headers).body
+        end
+      end
+    end
+  end
+end
