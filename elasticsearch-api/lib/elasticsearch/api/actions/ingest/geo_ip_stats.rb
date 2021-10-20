ebd8f8d7034e5ed6e2a94728715e6c564e168947
Fernando Briano <fernando@picandocodigo.net>
2021-10-20 16:45:13 +0100

    [API] Updates 7.x to 7.16

--- elasticsearch-api/lib/elasticsearch/api/actions/ingest/geo_ip_stats.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/ingest/geo_ip_stats.rb
@@ -23,7 +23,7 @@ module Elasticsearch
         #
         # @option arguments [Hash] :headers Custom HTTP headers
         #
-        # @see https://www.elastic.co/guide/en/elasticsearch/reference/7.x/geoip-stats-api.html
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/7.16/geoip-stats-api.html
         #
         def geo_ip_stats(arguments = {})
           headers = arguments.delete(:headers) || {}
