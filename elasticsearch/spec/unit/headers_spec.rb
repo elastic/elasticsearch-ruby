5e2e994466910b725899e46d65f9276604bfeca9
Fernando Briano <fernando@picandocodigo.net>
2021-10-21 11:14:33 +0100

    [Client] Updates tests

--- elasticsearch/spec/unit/headers_spec.rb
+++ elasticsearch/spec/unit/headers_spec.rb
@@ -26,7 +26,7 @@ describe Elasticsearch::Client do
       allow(client).to receive(:perform_request) { OpenStruct.new(body: '') }
       expect { client.search(headers: headers) }.not_to raise_error
       expect(client).to have_received(:perform_request)
-                          .with('GET', '_search', {}, nil, headers)
+        .with('GET', '_search', {}, nil, headers)
     end
   end
 
@@ -35,17 +35,19 @@ describe Elasticsearch::Client do
       described_class.new(
         host: 'http://localhost:9200',
         transport_options: { headers: instance_headers }
-      )
+      ).tap do |client|
+        client.instance_variable_set('@verified', true)
+      end
     end
     let(:instance_headers) { { set_in_instantiation: 'header value' } }
-    let(:param_headers) {{'user-agent' => 'My Ruby Tests', 'set-on-method-call' => 'header value'}}
+    let(:param_headers) { { 'user-agent' => 'My Ruby Tests', 'set-on-method-call' => 'header value' } }
 
     it 'performs the request with the header' do
       expected_headers = client.transport.connections.connections.first.connection.headers.merge(param_headers)
 
       expect_any_instance_of(Faraday::Connection)
         .to receive(:run_request)
-              .with(:get, "http://localhost:9200/_search", nil, expected_headers) { OpenStruct.new(body: '')}
+          .with(:get, 'http://localhost:9200/_search', nil, expected_headers) { OpenStruct.new(body: '') }
 
       client.search(headers: param_headers)
     end
