0e213fe4318ce0ff00a0eedf50f9d2e1e55141a0
Fernando Briano <fernando@picandocodigo.net>
2022-02-21 17:13:51 +0000

    [API] Adds security user_profile APIs and oidc

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/security/get_user_profile.rb
@@ -0,0 +1,56 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+module Elasticsearch
+  module API
+    module Security
+      module Actions
+        # Retrieves user profile for the given unique ID.
+        # This functionality is Experimental and may be changed or removed
+        # completely in a future release. Elastic will take a best effort approach
+        # to fix any issues, but experimental features are not subject to the
+        # support SLA of official GA features.
+        #
+        # @option arguments [String] :uid An unique identifier of the user profile
+        # @option arguments [List] :data A comma-separated list of keys for which the corresponding application data are retrieved.
+        # @option arguments [Hash] :headers Custom HTTP headers
+        #
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/security-api-get-user-profile.html
+        #
+        def get_user_profile(arguments = {})
+          raise ArgumentError, "Required argument 'uid' missing" unless arguments[:uid]
+
+          headers = arguments.delete(:headers) || {}
+
+          body = nil
+
+          arguments = arguments.clone
+
+          _uid = arguments.delete(:uid)
+
+          method = Elasticsearch::API::HTTP_GET
+          path   = "_security/profile/#{Utils.__listify(_uid)}"
+          params = Utils.process_params(arguments)
+
+          Elasticsearch::API::Response.new(
+            perform_request(method, path, params, body, headers)
+          )
+        end
+      end
+    end
+  end
+end
---
+++ elasticsearch-api/lib/elasticsearch/api/actions/security/update_user_profile_data.rb
@@ -0,0 +1,60 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+module Elasticsearch
+  module API
+    module Security
+      module Actions
+        # Update application specific data for the user profile of the given unique ID.
+        # This functionality is Experimental and may be changed or removed
+        # completely in a future release. Elastic will take a best effort approach
+        # to fix any issues, but experimental features are not subject to the
+        # support SLA of official GA features.
+        #
+        # @option arguments [String] :uid An unique identifier of the user profile
+        # @option arguments [Number] :if_seq_no only perform the update operation if the last operation that has changed the document has the specified sequence number
+        # @option arguments [Number] :if_primary_term only perform the update operation if the last operation that has changed the document has the specified primary term
+        # @option arguments [String] :refresh If `true` (the default) then refresh the affected shards to make this operation visible to search, if `wait_for` then wait for a refresh to make this operation visible to search, if `false` then do nothing with refreshes. (options: true, false, wait_for)
+        # @option arguments [Hash] :headers Custom HTTP headers
+        # @option arguments [Hash] :body The application data to update (*Required*)
+        #
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/security-api-update-user-profile-data.html
+        #
+        def update_user_profile_data(arguments = {})
+          raise ArgumentError, "Required argument 'body' missing" unless arguments[:body]
+          raise ArgumentError, "Required argument 'uid' missing" unless arguments[:uid]
+
+          headers = arguments.delete(:headers) || {}
+
+          body = arguments.delete(:body)
+
+          arguments = arguments.clone
+
+          _uid = arguments.delete(:uid)
+
+          method = Elasticsearch::API::HTTP_POST
+          path   = "_security/profile/_data/#{Utils.__listify(_uid)}"
+          params = Utils.process_params(arguments)
+
+          Elasticsearch::API::Response.new(
+            perform_request(method, path, params, body, headers)
+          )
+        end
+      end
+    end
+  end
+end
