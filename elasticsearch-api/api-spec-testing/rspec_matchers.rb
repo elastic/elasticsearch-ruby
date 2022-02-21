ff32b428ea4a6ba31e86d19e4acde153cc781983
Fernando Briano <fernando@picandocodigo.net>
2022-02-21 15:13:26 +0000

    [API] Test Runner: Updates compare_string for big number tests

--- elasticsearch-api/api-spec-testing/rspec_matchers.rb
+++ elasticsearch-api/api-spec-testing/rspec_matchers.rb
@@ -245,7 +245,7 @@ RSpec::Matchers.define :match_response do |pairs, test|
       # When the value in the yaml test is a big number, the format is
       # different from what Ruby uses, so we transform  X.XXXXEXX to X.XXXXXe+XX
       # to be able to compare the values
-      actual_value.to_s == expected.gsub('E', 'e+')
+      actual_value.to_s == expected.gsub('E', 'e+') || actual_value == expected
     elsif expected == '' && actual_value != ''
       actual_value == response
     else
