647941ccaac810b71c9ef154fea416e43ba4525a
Fernando Briano <fernando@picandocodigo.net>
2022-03-03 09:11:25 +0000

    [API] Adds ml.get_memory_stats

---
+++ elasticsearch-api/spec/elasticsearch/api/actions/machine_learning/get_memory_stats_spec.rb
@@ -0,0 +1,47 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+require 'spec_helper'
+
+describe 'client#ml.get_memory_stats' do
+
+  let(:expected_args) do
+    [
+      'GET',
+      url,
+      {},
+      nil,
+      {}
+    ]
+  end
+
+  context 'without a node id' do
+    let(:url) { '_ml/memory/_stats' }
+
+    it 'performs the request' do
+      expect(client_double.ml.get_memory_stats).to be_a Elasticsearch::API::Response
+    end
+  end
+
+  context 'with a node id' do
+    let(:url) { '_ml/memory/foo/_stats' }
+
+    it 'performs the request' do
+      expect(client_double.ml.get_memory_stats(node_id: 'foo')).to be_a Elasticsearch::API::Response
+    end
+  end
+end
