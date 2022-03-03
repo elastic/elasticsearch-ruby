45469204887ed7c128c1fc844761f6857eee365b
Fernando Briano <fernando@picandocodigo.net>
2022-03-03 09:11:25 +0000

    [API] Adds ml.get_memory_stats

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/machine_learning/get_memory_stats.rb
@@ -0,0 +1,55 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+module Elasticsearch
+  module API
+    module MachineLearning
+      module Actions
+        # Returns information on how ML is using memory.
+        #
+        # @option arguments [String] :node_id Specifies the node or nodes to retrieve stats for.
+        # @option arguments [Time] :master_timeout Explicit operation timeout for connection to master node
+        # @option arguments [Time] :timeout Explicit operation timeout
+        # @option arguments [Hash] :headers Custom HTTP headers
+        #
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/get-ml-memory.html
+        #
+        def get_memory_stats(arguments = {})
+          headers = arguments.delete(:headers) || {}
+
+          body = nil
+
+          arguments = arguments.clone
+
+          _node_id = arguments.delete(:node_id)
+
+          method = Elasticsearch::API::HTTP_GET
+          path   = if _node_id
+                     "_ml/memory/#{Utils.__listify(_node_id)}/_stats"
+                   else
+                     "_ml/memory/_stats"
+                   end
+          params = Utils.process_params(arguments)
+
+          Elasticsearch::API::Response.new(
+            perform_request(method, path, params, body, headers)
+          )
+        end
+      end
+    end
+  end
+end
