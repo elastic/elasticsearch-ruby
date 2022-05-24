02d2c84430e4ffaae820e1d6ef3a305c2fdba18c
Elastic Machine <elasticmachine@users.noreply.github.com>
2022-05-24 13:16:58 +0000

    Bumps to version 8.2.2

--- elasticsearch-api/lib/elasticsearch/api/version.rb
+++ elasticsearch-api/lib/elasticsearch/api/version.rb
@@ -17,6 +17,6 @@
 
 module Elasticsearch
   module API
-    VERSION = '8.2.1'.freeze
+    VERSION = '8.2.2'.freeze
   end
 end
--- elasticsearch/lib/elasticsearch/version.rb
+++ elasticsearch/lib/elasticsearch/version.rb
@@ -16,5 +16,5 @@
 # under the License.
 
 module Elasticsearch
-  VERSION = '8.2.1'.freeze
+  VERSION = '8.2.2'.freeze
 end
