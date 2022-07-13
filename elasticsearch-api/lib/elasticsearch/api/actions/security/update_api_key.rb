d9472986c4e7ae84c175c3c2b304f0860b2c0a3e
Fernando Briano <fernando@picandocodigo.net>
2022-07-13 09:36:03 +0100

    [API] Adds security.update_api_key

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/security/update_api_key.rb
@@ -0,0 +1,54 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from build hash a2ee4c5393dbb184255b98f77665265e70c63c57
+# @see https://github.com/elastic/elasticsearch/tree/main/rest-api-spec
+#
+module Elasticsearch
+  module API
+    module Security
+      module Actions
+        # Updates attributes of an existing API key.
+        #
+        # @option arguments [String] :id The ID of the API key to update
+        # @option arguments [Hash] :headers Custom HTTP headers
+        # @option arguments [Hash] :body The API key request to update attributes of an API key.
+        #
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/security-api-update-api-key.html
+        #
+        def update_api_key(arguments = {})
+          raise ArgumentError, "Required argument 'id' missing" unless arguments[:id]
+
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body = arguments.delete(:body)
+
+          _id = arguments.delete(:id)
+
+          method = Elasticsearch::API::HTTP_PUT
+          path   = "_security/api_key/#{Utils.__listify(_id)}"
+          params = {}
+
+          Elasticsearch::API::Response.new(
+            perform_request(method, path, params, body, headers)
+          )
+        end
+      end
+    end
+  end
+end
