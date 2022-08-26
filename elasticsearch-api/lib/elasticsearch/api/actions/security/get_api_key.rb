448834c0f69b47e62b53562a0429b7970d3f51e8
Elastic Machine <elasticmachine@users.noreply.github.com>
2022-08-26 11:09:08 +0000

    [API] Updates security.get_api_key and security.query_api_keys
    Adds `with_limited_by` flag parameter (Boolean) to show the limited-by role descriptors of API Keys
    
    Co-authored-by: Fernando Briano <fernando@picandocodigo.net>

--- elasticsearch-api/lib/elasticsearch/api/actions/security/get_api_key.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/security/get_api_key.rb
@@ -29,6 +29,7 @@ module Elasticsearch
         # @option arguments [String] :username user name of the user who created this API key to be retrieved
         # @option arguments [String] :realm_name realm name of the user who created this API key to be retrieved
         # @option arguments [Boolean] :owner flag to query API keys owned by the currently authenticated user
+        # @option arguments [Boolean] :with_limited_by flag to show the limited-by role descriptors of API Keys
         # @option arguments [Hash] :headers Custom HTTP headers
         #
         # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/security-api-get-api-key.html
