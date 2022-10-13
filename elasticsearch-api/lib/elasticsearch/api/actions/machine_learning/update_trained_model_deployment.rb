59b88eb9fbba55501c7211909fd43b03e52ced17
Elastic Machine <elasticmachine@users.noreply.github.com>
2022-10-13 14:20:46 +0000

    Auto-generated API code

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/machine_learning/update_trained_model_deployment.rb
@@ -0,0 +1,59 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from build hash f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch/tree/main/rest-api-spec
+#
+module Elasticsearch
+  module API
+    module MachineLearning
+      module Actions
+        # Updates certain properties of trained model deployment.
+        # This functionality is in Beta and is subject to change. The design and
+        # code is less mature than official GA features and is being provided
+        # as-is with no warranties. Beta features are not subject to the support
+        # SLA of official GA features.
+        #
+        # @option arguments [String] :model_id The unique identifier of the trained model.
+        # @option arguments [Hash] :headers Custom HTTP headers
+        # @option arguments [Hash] :body The updated trained model deployment settings (*Required*)
+        #
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/ml-update-trained-model-deployment.html
+        #
+        def update_trained_model_deployment(arguments = {})
+          raise ArgumentError, "Required argument 'body' missing" unless arguments[:body]
+          raise ArgumentError, "Required argument 'model_id' missing" unless arguments[:model_id]
+
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body = arguments.delete(:body)
+
+          _model_id = arguments.delete(:model_id)
+
+          method = Elasticsearch::API::HTTP_POST
+          path   = "_ml/trained_models/#{Utils.__listify(_model_id)}/deployment/_update"
+          params = {}
+
+          Elasticsearch::API::Response.new(
+            perform_request(method, path, params, body, headers)
+          )
+        end
+      end
+    end
+  end
+end
