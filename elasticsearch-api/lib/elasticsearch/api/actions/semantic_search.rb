718a5e1979b6e198df464c80a7461384171b6309
Elastic Machine <elasticmachine@users.noreply.github.com>
2022-10-17 13:00:06 +0000

    [API] Adds semantic_search endpoint
    Co-authored-by: Fernando Briano <fernando@picandocodigo.net>

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/semantic_search.rb
@@ -0,0 +1,62 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from build hash f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch/tree/main/rest-api-spec
+#
+module Elasticsearch
+  module API
+    module Actions
+      # Semantic search API using dense vector similarity
+      # This functionality is Experimental and may be changed or removed
+      # completely in a future release. Elastic will take a best effort approach
+      # to fix any issues, but experimental features are not subject to the
+      # support SLA of official GA features.
+      #
+      # @option arguments [List] :index A comma-separated list of index names to search; use `_all` to perform the operation on all indices
+      # @option arguments [List] :routing A comma-separated list of specific routing values
+      # @option arguments [Hash] :headers Custom HTTP headers
+      # @option arguments [Hash] :body The search definition
+      #
+      # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/semantic-search.html
+      #
+      def semantic_search(arguments = {})
+        raise ArgumentError, "Required argument 'index' missing" unless arguments[:index]
+
+        arguments = arguments.clone
+        headers = arguments.delete(:headers) || {}
+
+        body   = arguments.delete(:body)
+
+        _index = arguments.delete(:index)
+
+        method = if body
+                   Elasticsearch::API::HTTP_POST
+                 else
+                   Elasticsearch::API::HTTP_GET
+                 end
+
+        path   = "#{Utils.__listify(_index)}/_semantic_search"
+        params = Utils.process_params(arguments)
+
+        Elasticsearch::API::Response.new(
+          perform_request(method, path, params, body, headers)
+        )
+      end
+    end
+  end
+end
