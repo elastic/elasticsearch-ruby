7f09d60cebbd1a0a8ff2bd1c39e609eb5a81a4bc
Fernando Briano <fernando@picandocodigo.net>
2022-11-23 11:39:11 +0000

    [CI] Buildkite: Updates run-elasticsearch.sh

--- .buildkite/run-elasticsearch.sh
+++ .buildkite/run-elasticsearch.sh
@@ -13,7 +13,16 @@ ES_NODE_NAME=es01
 
 docker pull docker.elastic.co/elasticsearch/elasticsearch:$STACK_VERSION
 docker network create elastic
-docker run --name $ES_NODE_NAME --net elastic -p 9200:9200 --detach=$DETACH -it docker.elastic.co/elasticsearch/elasticsearch:$STACK_VERSION
+docker run --name $ES_NODE_NAME \
+       --net elastic \
+       -p 9200:9200 \
+       --detach=$DETACH \
+       --health-cmd="curl --fail http://es1/_cluster/health || exit 1" \
+       --health-interval=2s \
+       --health-retries=20 \
+       --health-timeout=2s \
+       -it \
+       docker.elastic.co/elasticsearch/elasticsearch:$STACK_VERSION
 set +x
 if wait_for_container "$ES_NODE_NAME" elastic; then
   echo -e "\033[32;1mSUCCESS:\033[0m Running on: $node_url\033[0m"
