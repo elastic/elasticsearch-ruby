050d9fd11965a7848d38e4d4b3c37c1b74facb8f
Fernando Briano <fernando@picandocodigo.net>
2022-11-23 13:14:14 +0000

    [CI] Buildkite: Adds running client code

---
+++ .buildkite/run-client.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#
+# Once called Elasticsearch should be up and running
+#
+script_path=$(dirname $(realpath -s $0))
+set -euo pipefail
+repo=`pwd`
+
+docker build \
+       --file $script_path/Dockerfile \
+       --tag elastic/elasticsearch-ruby \
+       --build-arg RUBY_VERSION=$RUBY_VERSION \
+       .
+
+docker run \
+       -u "$(id -u)" \
+       --volume $repo:/usr/src/app \
+       --network="elastic" \
+       --name elasticsearch-ruby \
+       --rm \
+       elastic/elasticsearch-ruby \
+       bundle exec rake elasticsearch:health
+
