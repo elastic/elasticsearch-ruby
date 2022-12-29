5b4a0a0718d6e77e3b368a0bc2067498766873fb
Fernando Briano <fernando@picandocodigo.net>
2022-12-29 10:14:37 +0000

    [CI] Rename JUnit reports with Test suite and Ruby version

--- elasticsearch-api/spec/spec_helper.rb
+++ elasticsearch-api/spec/spec_helper.rb
@@ -59,13 +59,8 @@ end
 RSpec.configure do |config|
   config.include(HelperModule)
   config.add_formatter('documentation')
-  if ENV['TEST_SUITE'] == 'platinum'
-    config.add_formatter('RspecJunitFormatter', 'tmp/elasticsearch-platinum-junit.xml')
-    config.add_formatter('RSpec::Core::Formatters::HtmlFormatter', 'tmp/elasticsearch-platinum.html')
-  else
-    config.add_formatter('RspecJunitFormatter', 'tmp/elasticsearch-api-junit.xml')
-    config.add_formatter('RSpec::Core::Formatters::HtmlFormatter', 'tmp/elasticsearch.html')
-  end
+  config.add_formatter('RspecJunitFormatter', "tmp/elasticsearch-#{TEST_SUITE}-#{RUBY_VERSION}-junit.xml")
+  config.add_formatter('RSpec::Core::Formatters::HtmlFormatter', "tmp/elasticsearch-#{TEST_SUITE}-#{RUBY_VERSION}.html")
   config.color_mode = :on
 end
 
