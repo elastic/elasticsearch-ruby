d6d5294d36845daebafae044be7b175dbe9a714b
Fernando Briano <fernando@picandocodigo.net>
2023-01-09 14:56:45 +0000

    [CI] Buildkite: Adds JUnit reporter

---
+++ .buildkite/junit-report.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+# Script to run npm's xunit-viewer to view JUnit test reports
+#
+echo "--- JUnit Report"
+# The directorty where the JUnit report is stored
+buildkite-agent artifact download "elasticsearch-api/tmp/*.xml" .
+repo=`pwd`
+script_path=$(dirname $(realpath -s $0))
+
+docker build --file $script_path/Dockerfile-JUnit -t junit-report .
+
+docker run \
+       --volume $repo:/usr/src/app \
+       --rm \
+       junit-report
