7274ed5e9bd95ebf267fc3a90bf5e8d3b992f298
Fernando Briano <fernando@picandocodigo.net>
2023-01-31 11:05:28 +0000

    [API] New APIs:
    - health_report - Returns the health of the cluster.
    - semantic_search - Semantic search API using dense vector similarity (Experimental API)

---
+++ elasticsearch-api/spec/elasticsearch/api/actions/health_report_spec.rb
@@ -0,0 +1,50 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+require 'spec_helper'
+
+describe 'client#health_report' do
+  let(:expected_args) do
+    [
+      'GET',
+      '_health_report',
+      {},
+      nil,
+      {}
+    ]
+  end
+
+  it 'performs the request' do
+    expect(client_double.health_report).to be_a Elasticsearch::API::Response
+  end
+
+  context 'when a feature is specified' do
+    let(:expected_args) do
+      [
+        'GET',
+        '_health_report/foo',
+        {},
+        nil,
+        {}
+      ]
+    end
+
+    it 'performs the request' do
+      expect(client_double.health_report(feature: 'foo')).to be_a Elasticsearch::API::Response
+    end
+  end
+end
