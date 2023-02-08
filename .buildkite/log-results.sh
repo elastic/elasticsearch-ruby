d07f61d09d901469eee1c88df31ba9600a598c95
Fernando Briano <fernando@picandocodigo.net>
2023-02-08 08:22:05 +0000

    [CI] Buildkite: Updates log results regexps

--- .buildkite/log-results.sh
+++ .buildkite/log-results.sh
@@ -8,8 +8,8 @@ files="elasticsearch-api/tmp/*.html"
 for f in $files; do
   TEST_SUITE=`echo $f | grep -o "\(free\|platinum\)"`
   RUBY_VERSION=`echo $f | grep -Po "(\d+\.)+\d+"`
-  EXAMPLES=`cat $f | grep -o "[0-9]\+ examples" | tail -1`
-  FAILURES=`cat $f | grep -o "[0-9]\+ failures" | tail -1`
+  EXAMPLES=`cat $f | grep -o "[0-9]\+ examples\?" | tail -1`
+  FAILURES=`cat $f | grep -o "[0-9]\+ failures\?" | tail -1`
   PENDING=`cat $f | grep -o "[0-9]\+ pending" | tail -1`
   echo "+++ :ruby: $RUBY_VERSION :test_tube: $TEST_SUITE :rspec: $EXAMPLES - :x: $FAILURES - :pinched_fingers: $PENDING"
 done
