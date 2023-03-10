ff0a6b9b08d880604cd290b7251ea50a18b911d2
Fernando Briano <fernando@picandocodigo.net>
2023-03-10 14:22:46 +0000

    [CI] Buildkite: Use annotate for test results

--- .buildkite/log-results.sh
+++ .buildkite/log-results.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 #
 # This script is intended to be run after the build in a Buildkite pipeline
-echo "--- Test summary"
+buildkite-agent annotate "## :rspec: Tests summary :rspec:
+"
 buildkite-agent artifact download "elasticsearch-api/tmp/*.html" .
 
 files="elasticsearch-api/tmp/*.html"
@@ -11,5 +12,7 @@ for f in $files; do
   EXAMPLES=`cat $f | grep -o "[0-9]\+ examples\?" | tail -1`
   FAILURES=`cat $f | grep -o "[0-9]\+ failures\?" | tail -1`
   PENDING=`cat $f | grep -o "[0-9]\+ pending" | tail -1`
-  echo "+++ :ruby: $RUBY_VERSION :test_tube: $TEST_SUITE :rspec: $EXAMPLES - :x: $FAILURES - :pinched_fingers: $PENDING"
+  buildkite-agent annotate --style info --append "
+:ruby: $RUBY_VERSION :test_tube: $TEST_SUITE :rspec: $EXAMPLES - :x: $FAILURES - :suspect: $PENDING
+"
 done
