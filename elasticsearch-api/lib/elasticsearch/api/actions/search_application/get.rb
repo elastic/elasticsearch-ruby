ad430d9bd347b38c632778e55ddd5730633933c9
Fernando Briano <fernando@picandocodigo.net>
2023-04-20 09:31:07 +0100

    [API] Updates search_application APIs to be Experimental (from Beta)

--- elasticsearch-api/lib/elasticsearch/api/actions/search_application/get.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/search_application/get.rb
@@ -23,10 +23,10 @@ module Elasticsearch
     module SearchApplication
       module Actions
         # Returns the details about a search application.
-        # This functionality is in Beta and is subject to change. The design and
-        # code is less mature than official GA features and is being provided
-        # as-is with no warranties. Beta features are not subject to the support
-        # SLA of official GA features.
+        # This functionality is Experimental and may be changed or removed
+        # completely in a future release. Elastic will take a best effort approach
+        # to fix any issues, but experimental features are not subject to the
+        # support SLA of official GA features.
         #
         # @option arguments [String] :name The name of the search application
         # @option arguments [Hash] :headers Custom HTTP headers
--- elasticsearch-api/lib/elasticsearch/api/actions/search_application/list.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/search_application/list.rb
@@ -23,10 +23,10 @@ module Elasticsearch
     module SearchApplication
       module Actions
         # Returns the existing search applications.
-        # This functionality is in Beta and is subject to change. The design and
-        # code is less mature than official GA features and is being provided
-        # as-is with no warranties. Beta features are not subject to the support
-        # SLA of official GA features.
+        # This functionality is Experimental and may be changed or removed
+        # completely in a future release. Elastic will take a best effort approach
+        # to fix any issues, but experimental features are not subject to the
+        # support SLA of official GA features.
         #
         # @option arguments [String] :q Query in the Lucene query string syntax
         # @option arguments [Integer] :from Starting offset (default: 0)
