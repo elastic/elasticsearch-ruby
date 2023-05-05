731c06ab2630f399b98e2f4e80d0b0c096c363dd
Fernando Briano <fernando@picandocodigo.net>
2023-05-05 06:02:44 +0000

    [API] Updates new indices APIs
    Adds experimental status message, required parameter name
    
    - delete_data_lifecycle
    - get_data_lifecycle
    - put_data_lifecycle

--- elasticsearch-api/lib/elasticsearch/api/actions/indices/put_data_lifecycle.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/indices/put_data_lifecycle.rb
@@ -23,6 +23,10 @@ module Elasticsearch
     module Indices
       module Actions
         # Updates the data lifecycle of the selected data streams.
+        # This functionality is Experimental and may be changed or removed
+        # completely in a future release. Elastic will take a best effort approach
+        # to fix any issues, but experimental features are not subject to the
+        # support SLA of official GA features.
         #
         # @option arguments [List] :name A comma-separated list of data streams whose lifecycle will be updated; use `*` to set the lifecycle to all data streams
         # @option arguments [String] :expand_wildcards Whether wildcard expressions should get expanded to open or closed indices (default: open) (options: open, closed, hidden, none, all)
@@ -34,6 +38,8 @@ module Elasticsearch
         # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/dlm-put-lifecycle.html
         #
         def put_data_lifecycle(arguments = {})
+          raise ArgumentError, "Required argument 'name' missing" unless arguments[:name]
+
           arguments = arguments.clone
           headers = arguments.delete(:headers) || {}
 
@@ -42,11 +48,7 @@ module Elasticsearch
           _name = arguments.delete(:name)
 
           method = Elasticsearch::API::HTTP_PUT
-          path   = if _name
-                     "_data_stream/#{Utils.__listify(_name)}/_lifecycle"
-                   else
-                     "_data_stream/_lifecycle"
-                   end
+          path   = "_data_stream/#{Utils.__listify(_name)}/_lifecycle"
           params = Utils.process_params(arguments)
 
           Elasticsearch::API::Response.new(
