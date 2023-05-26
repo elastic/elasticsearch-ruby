67b0139f9d5e4e8dabe58678ef78c6fd78afeff9
Fernando Briano <fernando@picandocodigo.net>
2023-05-26 06:03:06 +0000

    [API] Adds cluster.info API

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/cluster/info.rb
@@ -0,0 +1,53 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from build hash f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch/tree/main/rest-api-spec
+#
+module Elasticsearch
+  module API
+    module Cluster
+      module Actions
+        # Returns different information about the cluster.
+        #
+        # @option arguments [List] :target Limit the information returned to the specified target. (options: _all, http)
+        # @option arguments [Hash] :headers Custom HTTP headers
+        #
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/cluster-info.html
+        #
+        def info(arguments = {})
+          raise ArgumentError, "Required argument 'target' missing" unless arguments[:target]
+
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body = nil
+
+          _target = arguments.delete(:target)
+
+          method = Elasticsearch::API::HTTP_GET
+          path   = "_info/#{Utils.__listify(_target)}"
+          params = {}
+
+          Elasticsearch::API::Response.new(
+            perform_request(method, path, params, body, headers)
+          )
+        end
+      end
+    end
+  end
+end
