941647d2d29bebf1e62ddd05e39e133e80d531b1
Fernando Briano <fernando@picandocodigo.net>
2023-05-30 04:38:24 +0100

    [API] Adds create_cross_cluster_api_key and update_cross_cluster_api_key in security

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/security/update_cross_cluster_api_key.rb
@@ -0,0 +1,59 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from build hash f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch/tree/main/rest-api-spec
+#
+module Elasticsearch
+  module API
+    module Security
+      module Actions
+        # Updates attributes of an existing cross-cluster API key.
+        # This functionality is Experimental and may be changed or removed
+        # completely in a future release. Elastic will take a best effort approach
+        # to fix any issues, but experimental features are not subject to the
+        # support SLA of official GA features.
+        #
+        # @option arguments [String] :id The ID of the cross-cluster API key to update
+        # @option arguments [Hash] :headers Custom HTTP headers
+        # @option arguments [Hash] :body The request to update attributes of a cross-cluster API key. (*Required*)
+        #
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/security-api-update-cross-cluster-api-key.html
+        #
+        def update_cross_cluster_api_key(arguments = {})
+          raise ArgumentError, "Required argument 'body' missing" unless arguments[:body]
+          raise ArgumentError, "Required argument 'id' missing" unless arguments[:id]
+
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body = arguments.delete(:body)
+
+          _id = arguments.delete(:id)
+
+          method = Elasticsearch::API::HTTP_PUT
+          path   = "_security/cross_cluster/api_key/#{Utils.__listify(_id)}"
+          params = {}
+
+          Elasticsearch::API::Response.new(
+            perform_request(method, path, params, body, headers)
+          )
+        end
+      end
+    end
+  end
+end
