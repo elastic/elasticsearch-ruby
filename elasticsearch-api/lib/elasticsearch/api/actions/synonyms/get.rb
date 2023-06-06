eeb90cd23c6dc30261d6ce47c349936ca1b3a776
Fernando Briano <fernando@picandocodigo.net>
2023-06-06 06:02:48 +0000

    [API] Adds synonyms.delete and synonyms.get

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/synonyms/get.rb
@@ -0,0 +1,59 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from build hash f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch/tree/main/rest-api-spec
+#
+module Elasticsearch
+  module API
+    module Synonyms
+      module Actions
+        # Retrieves a synonym set
+        # This functionality is Experimental and may be changed or removed
+        # completely in a future release. Elastic will take a best effort approach
+        # to fix any issues, but experimental features are not subject to the
+        # support SLA of official GA features.
+        #
+        # @option arguments [String] :synonyms_set The name of the synonyms set to be retrieved
+        # @option arguments [Integer] :from Starting offset
+        # @option arguments [Integer] :size specifies a max number of results to get
+        # @option arguments [Hash] :headers Custom HTTP headers
+        #
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/get-synonyms.html
+        #
+        def get(arguments = {})
+          raise ArgumentError, "Required argument 'synonyms_set' missing" unless arguments[:synonyms_set]
+
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body = nil
+
+          _synonyms_set = arguments.delete(:synonyms_set)
+
+          method = Elasticsearch::API::HTTP_GET
+          path   = "_synonyms/#{Utils.__listify(_synonyms_set)}"
+          params = Utils.process_params(arguments)
+
+          Elasticsearch::API::Response.new(
+            perform_request(method, path, params, body, headers)
+          )
+        end
+      end
+    end
+  end
+end
