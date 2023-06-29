d355dc889de95e2b5b1bdbeea3401155c14f38ca
Fernando Briano <fernando@picandocodigo.net>
2023-06-29 10:01:11 +0100

    [API] Adds synonym_rule.get

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/synonym_rule/get.rb
@@ -0,0 +1,61 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from build hash f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch/tree/main/rest-api-spec
+#
+module Elasticsearch
+  module API
+    module SynonymRule
+      module Actions
+        # Retrieves a synonym rule from a synonym set
+        # This functionality is Experimental and may be changed or removed
+        # completely in a future release. Elastic will take a best effort approach
+        # to fix any issues, but experimental features are not subject to the
+        # support SLA of official GA features.
+        #
+        # @option arguments [String] :synonyms_set The id of the synonym set to retrieve the synonym rule from
+        # @option arguments [String] :synonym_rule The id of the synonym rule to retrieve
+        # @option arguments [Hash] :headers Custom HTTP headers
+        #
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/get-synonym-rule.html
+        #
+        def get(arguments = {})
+          raise ArgumentError, "Required argument 'synonyms_set' missing" unless arguments[:synonyms_set]
+          raise ArgumentError, "Required argument 'synonym_rule' missing" unless arguments[:synonym_rule]
+
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body = nil
+
+          _synonyms_set = arguments.delete(:synonyms_set)
+
+          _synonym_rule = arguments.delete(:synonym_rule)
+
+          method = Elasticsearch::API::HTTP_GET
+          path   = "_synonyms/#{Utils.__listify(_synonyms_set)}/#{Utils.__listify(_synonym_rule)}"
+          params = {}
+
+          Elasticsearch::API::Response.new(
+            perform_request(method, path, params, body, headers)
+          )
+        end
+      end
+    end
+  end
+end
