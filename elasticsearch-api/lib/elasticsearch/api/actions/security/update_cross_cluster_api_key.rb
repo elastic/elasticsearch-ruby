5cd47ee02d2616d4c4f7fdbbca6109a65d782b9b
Fernando Briano <fernando@picandocodigo.net>
2023-08-22 07:34:41 +0100

    [API] Updates security create|update cross cluster api key from Experimental to Beta

--- elasticsearch-api/lib/elasticsearch/api/actions/security/update_cross_cluster_api_key.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/security/update_cross_cluster_api_key.rb
@@ -23,10 +23,10 @@ module Elasticsearch
     module Security
       module Actions
         # Updates attributes of an existing cross-cluster API key.
-        # This functionality is Experimental and may be changed or removed
-        # completely in a future release. Elastic will take a best effort approach
-        # to fix any issues, but experimental features are not subject to the
-        # support SLA of official GA features.
+        # This functionality is in Beta and is subject to change. The design and
+        # code is less mature than official GA features and is being provided
+        # as-is with no warranties. Beta features are not subject to the support
+        # SLA of official GA features.
         #
         # @option arguments [String] :id The ID of the cross-cluster API key to update
         # @option arguments [Hash] :headers Custom HTTP headers
