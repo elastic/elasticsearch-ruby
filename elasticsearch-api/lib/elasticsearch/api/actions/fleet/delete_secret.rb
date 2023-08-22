83c744af7c86115f9730bd124683b4953483bd1c
Fernando Briano <fernando@picandocodigo.net>
2023-08-22 07:35:40 +0100

    [API] Adds experimental (internal use) Fleet APIs for secrets

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/fleet/delete_secret.rb
@@ -0,0 +1,57 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from build hash f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch/tree/main/rest-api-spec
+#
+module Elasticsearch
+  module API
+    module Fleet
+      module Actions
+        # Deletes a secret stored by Fleet.
+        # This functionality is Experimental and may be changed or removed
+        # completely in a future release. Elastic will take a best effort approach
+        # to fix any issues, but experimental features are not subject to the
+        # support SLA of official GA features.
+        #
+        # @option arguments [String] :id The ID of the secret
+        # @option arguments [Hash] :headers Custom HTTP headers
+        #
+        # @see [TODO]
+        #
+        def delete_secret(arguments = {})
+          raise ArgumentError, "Required argument 'id' missing" unless arguments[:id]
+
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body = nil
+
+          _id = arguments.delete(:id)
+
+          method = Elasticsearch::API::HTTP_DELETE
+          path   = "_fleet/secret/#{Utils.__listify(_id)}"
+          params = {}
+
+          Elasticsearch::API::Response.new(
+            perform_request(method, path, params, body, headers)
+          )
+        end
+      end
+    end
+  end
+end
