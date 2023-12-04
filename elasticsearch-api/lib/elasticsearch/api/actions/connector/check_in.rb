e4a7ea4253550acd12cdcc28c5c1060ae40f218a
Fernando Briano <fernando@picandocodigo.net>
2023-12-04 09:50:46 +0000

    [API] Adds experimental APIs in connector namespace: * check_in * update_filtering * update_pipeline

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/connector/check_in.rb
@@ -0,0 +1,65 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from build hash f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch/tree/main/rest-api-spec
+#
+module Elasticsearch
+  module API
+    module Connector
+      module Actions
+        # Updates the last_seen timestamp in the connector document.
+        # This functionality is Experimental and may be changed or removed
+        # completely in a future release. Elastic will take a best effort approach
+        # to fix any issues, but experimental features are not subject to the
+        # support SLA of official GA features.
+        #
+        # @option arguments [String] :connector_id The unique identifier of the connector to be updated.
+        # @option arguments [Hash] :headers Custom HTTP headers
+        #
+        # @see https://www.elastic.co/guide/en/enterprise-search/current/connectors.html
+        #
+        def check_in(arguments = {})
+          request_opts = { endpoint: arguments[:endpoint] || "connector.check_in" }
+
+          defined_params = [:connector_id].inject({}) do |set_variables, variable|
+            set_variables[variable] = arguments[variable] if arguments.key?(variable)
+            set_variables
+          end
+          request_opts[:defined_params] = defined_params unless defined_params.empty?
+
+          raise ArgumentError, "Required argument 'connector_id' missing" unless arguments[:connector_id]
+
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body = nil
+
+          _connector_id = arguments.delete(:connector_id)
+
+          method = Elasticsearch::API::HTTP_PUT
+          path   = "_connector/#{Utils.__listify(_connector_id)}/_check_in"
+          params = {}
+
+          Elasticsearch::API::Response.new(
+            perform_request(method, path, params, body, headers, request_opts)
+          )
+        end
+      end
+    end
+  end
+end
