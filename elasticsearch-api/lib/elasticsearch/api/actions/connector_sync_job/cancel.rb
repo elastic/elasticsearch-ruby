5c0f4bf9af6ec39851eb7462bee1e2c2b27e1e69
Fernando Briano <fernando@picandocodigo.net>
2023-12-04 10:03:42 +0000

    [API] Adds experimental APIs in connector_sync_job namespace * cancel * check_in

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/connector_sync_job/cancel.rb
@@ -0,0 +1,66 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from build hash f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch/tree/main/rest-api-spec
+#
+module Elasticsearch
+  module API
+    module ConnectorSyncJob
+      module Actions
+        # Cancels a connector sync job.
+        # This functionality is Experimental and may be changed or removed
+        # completely in a future release. Elastic will take a best effort approach
+        # to fix any issues, but experimental features are not subject to the
+        # support SLA of official GA features.
+        #
+        # @option arguments [String] :connector_sync_job_id The unique identifier of the connector sync job to be canceled
+        # @option arguments [Hash] :headers Custom HTTP headers
+        #
+        # @see https://www.elastic.co/guide/en/enterprise-search/current/connectors.html
+        #
+        def cancel(arguments = {})
+          request_opts = { endpoint: arguments[:endpoint] || "connector_sync_job.cancel" }
+
+          defined_params = [:connector_sync_job_id].inject({}) do |set_variables, variable|
+            set_variables[variable] = arguments[variable] if arguments.key?(variable)
+            set_variables
+          end
+          request_opts[:defined_params] = defined_params unless defined_params.empty?
+
+          raise ArgumentError,
+                "Required argument 'connector_sync_job_id' missing" unless arguments[:connector_sync_job_id]
+
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body = nil
+
+          _connector_sync_job_id = arguments.delete(:connector_sync_job_id)
+
+          method = Elasticsearch::API::HTTP_PUT
+          path   = "_connector/_sync_job/#{Utils.__listify(_connector_sync_job_id)}/_cancel"
+          params = {}
+
+          Elasticsearch::API::Response.new(
+            perform_request(method, path, params, body, headers, request_opts)
+          )
+        end
+      end
+    end
+  end
+end
