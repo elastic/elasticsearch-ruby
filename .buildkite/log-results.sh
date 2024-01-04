b5e06118040c951e90a9cf3b824eb6356dfb3ab3
Fernando Briano <fernando@picandocodigo.net>
2024-01-04 12:53:08 +0000

    [CI] Fixes showing JRuby version in log-results

--- .buildkite/log-results.sh
+++ .buildkite/log-results.sh
@@ -9,7 +9,7 @@ buildkite-agent artifact download "elasticsearch-api/tmp/*" .
 files="elasticsearch-api/tmp/*.html"
 for f in $files; do
   TEST_SUITE=`echo $f | grep -o "\(free\|platinum\)"`
-  RUBY_VERSION=`echo $f | grep -Po "(\d+\.)+\d+"`
+  RUBY_VERSION=`echo $f | grep -Po "(jruby-|\d+\.)+\d+"`
   EXAMPLES=`cat $f | grep -o "[0-9]\+ examples\?" | tail -1`
   FAILURES=`cat $f | grep -o "[0-9]\+ failures\?" | tail -1`
   PENDING=`cat $f | grep -o "[0-9]\+ pending" | tail -1`
@@ -18,6 +18,7 @@ for f in $files; do
 "
 done
 
+# Failed tests:
 files="elasticsearch-api/tmp/*.log"
 for f in $files; do
   FAILED_TESTS=`awk 'BEGIN { FS = " | "}; /\| failed \|/{ print $1 }' $f | uniq`
