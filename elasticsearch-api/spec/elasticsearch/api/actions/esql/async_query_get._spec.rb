d4ca98ddfc812e78f9faad92296d31772b3498b5
Fernando Briano <fernando@picandocodigo.net>
2024-01-15 09:16:12 +0000

    [API] Adds experimental APIs: esql.async_query, esql.async_query_get

---
+++ elasticsearch-api/spec/elasticsearch/api/actions/esql/async_query_get._spec.rb
@@ -0,0 +1,35 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+require 'spec_helper'
+
+describe 'client.esql#async_query_get' do
+  let(:expected_args) do
+    [
+      'GET',
+      '_query/async/foo',
+      {},
+      nil,
+      {},
+      { defined_params: {id: 'foo' }, endpoint: 'esql.async_query_get' }
+    ]
+  end
+
+  it 'performs the request' do
+    expect(client_double.esql.async_query_get(id: 'foo')).to be_a Elasticsearch::API::Response
+  end
+end
