8301f05c6c8f2692477ca640288524cf2c39dc32
Fernando Briano <fernando@picandocodigo.net>
2024-01-15 09:16:12 +0000

    [API] Adds experimental APIs: esql.async_query, esql.async_query_get

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/esql/async_query_get.rb
@@ -0,0 +1,63 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from build hash f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch/tree/main/rest-api-spec
+#
+module Elasticsearch
+  module API
+    module Esql
+      module Actions
+        # Retrieves the results of a previously submitted async query request given its ID.
+        #
+        # @option arguments [String] :id The async query ID
+        # @option arguments [Time] :wait_for_completion_timeout Specify the time that the request should block waiting for the final response
+        # @option arguments [Time] :keep_alive Specify the time interval in which the results (partial or final) for this search will be available
+        # @option arguments [Hash] :headers Custom HTTP headers
+        #
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/esql-async-query-get-api.html
+        #
+        def async_query_get(arguments = {})
+          request_opts = { endpoint: arguments[:endpoint] || "esql.async_query_get" }
+
+          defined_params = [:id].inject({}) do |set_variables, variable|
+            set_variables[variable] = arguments[variable] if arguments.key?(variable)
+            set_variables
+          end
+          request_opts[:defined_params] = defined_params unless defined_params.empty?
+
+          raise ArgumentError, "Required argument 'id' missing" unless arguments[:id]
+
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body = nil
+
+          _id = arguments.delete(:id)
+
+          method = Elasticsearch::API::HTTP_GET
+          path   = "_query/async/#{Utils.__listify(_id)}"
+          params = Utils.process_params(arguments)
+
+          Elasticsearch::API::Response.new(
+            perform_request(method, path, params, body, headers, request_opts)
+          )
+        end
+      end
+    end
+  end
+end
