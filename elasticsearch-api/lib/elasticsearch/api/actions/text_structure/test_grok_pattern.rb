104cbfab6142fe7f2d0c71879b0a332b451a929a
Fernando Briano <fernando@picandocodigo.net>
2024-01-26 09:44:14 +0000

    [API] Adds text_structure.test_grok_pattern API

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/text_structure/test_grok_pattern.rb
@@ -0,0 +1,54 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from build hash f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch/tree/main/rest-api-spec
+#
+module Elasticsearch
+  module API
+    module TextStructure
+      module Actions
+        # Tests a Grok pattern on some text.
+        #
+        # @option arguments [String] :ecs_compatibility Optional parameter to specify the compatibility mode with ECS Grok patterns - may be either 'v1' or 'disabled'
+        # @option arguments [Hash] :headers Custom HTTP headers
+        # @option arguments [Hash] :body The Grok pattern and text. (*Required*)
+        #
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/test-grok-pattern-api.html
+        #
+        def test_grok_pattern(arguments = {})
+          request_opts = { endpoint: arguments[:endpoint] || "text_structure.test_grok_pattern" }
+
+          raise ArgumentError, "Required argument 'body' missing" unless arguments[:body]
+
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body   = arguments.delete(:body)
+
+          method = Elasticsearch::API::HTTP_POST
+          path   = "_text_structure/test_grok_pattern"
+          params = Utils.process_params(arguments)
+
+          Elasticsearch::API::Response.new(
+            perform_request(method, path, params, body, headers, request_opts)
+          )
+        end
+      end
+    end
+  end
+end
