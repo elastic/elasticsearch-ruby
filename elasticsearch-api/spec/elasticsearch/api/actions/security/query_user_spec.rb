388b9760b891a3c2fb806bcfc3a14a293425ee94
Fernando Briano <fernando@picandocodigo.net>
2024-02-05 14:47:54 +0000

    [API] Adds security.query_user endpoint

---
+++ elasticsearch-api/spec/elasticsearch/api/actions/security/query_user_spec.rb
@@ -0,0 +1,49 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+require 'spec_helper'
+
+describe 'client#security#query_user' do
+  let(:expected_args) do
+    [
+      method,
+      '_security/_query/user',
+      {},
+      body,
+      {},
+      { endpoint: 'security.query_user' }
+    ]
+  end
+
+  let(:body) { nil }
+  let(:method) { 'GET' }
+
+  it 'performs the request' do
+    expect(client_double.security.query_user).to be_a Elasticsearch::API::Response
+  end
+
+  context 'when body is specified' do
+    let(:body) do
+      { size: 1, query: 'test' }
+    end
+    let(:method) { 'POST' }
+
+    it 'performs the request' do
+      expect(client_double.security.query_user(body: body)).to be_a Elasticsearch::API::Response
+    end
+  end
+end
