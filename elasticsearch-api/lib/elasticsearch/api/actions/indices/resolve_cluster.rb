beab4acf29398e3255b5492ea717031a99db1415
Fernando Briano <fernando@picandocodigo.net>
2024-02-09 10:24:48 +0000

    [API] Adds indices.resolve_cluster endpoint

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/indices/resolve_cluster.rb
@@ -0,0 +1,64 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from build hash f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch/tree/main/rest-api-spec
+#
+module Elasticsearch
+  module API
+    module Indices
+      module Actions
+        # Resolves the specified index expressions to return information about each cluster, including the local cluster, if included.
+        #
+        # @option arguments [List] :name A comma-separated list of cluster:index names or wildcard expressions
+        # @option arguments [Boolean] :ignore_unavailable Whether specified concrete indices should be ignored when unavailable (missing or closed)
+        # @option arguments [Boolean] :ignore_throttled Whether specified concrete, expanded or aliased indices should be ignored when throttled
+        # @option arguments [Boolean] :allow_no_indices Whether to ignore if a wildcard indices expression resolves into no concrete indices. (This includes `_all` string or when no indices have been specified)
+        # @option arguments [String] :expand_wildcards Whether wildcard expressions should get expanded to open or closed indices (default: open) (options: open, closed, hidden, none, all)
+        # @option arguments [Hash] :headers Custom HTTP headers
+        #
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/indices-resolve-cluster-api.html
+        #
+        def resolve_cluster(arguments = {})
+          request_opts = { endpoint: arguments[:endpoint] || 'indices.resolve_cluster' }
+
+          defined_params = [:name].each_with_object({}) do |variable, set_variables|
+            set_variables[variable] = arguments[variable] if arguments.key?(variable)
+          end
+          request_opts[:defined_params] = defined_params unless defined_params.empty?
+
+          raise ArgumentError, "Required argument 'name' missing" unless arguments[:name]
+
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body = nil
+
+          _name = arguments.delete(:name)
+
+          method = Elasticsearch::API::HTTP_GET
+          path   = "_resolve/cluster/#{Utils.__listify(_name)}"
+          params = Utils.process_params(arguments)
+
+          Elasticsearch::API::Response.new(
+            perform_request(method, path, params, body, headers, request_opts)
+          )
+        end
+      end
+    end
+  end
+end
---
+++ elasticsearch-api/spec/elasticsearch/api/actions/indices/resolve_cluster_spec.rb
@@ -0,0 +1,51 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+require 'spec_helper'
+
+describe 'client.indices#delete_alias' do
+
+  let(:expected_args) do
+    [
+      'GET',
+      '_resolve/cluster/foo',
+      {},
+      nil,
+      {},
+      { defined_params: { name: 'foo'}, endpoint: 'indices.resolve_cluster' }
+    ]
+  end
+
+  context 'when there is no name specified' do
+    let(:client) do
+      Class.new { include Elasticsearch::API }.new
+    end
+
+    it 'raises an exception' do
+      expect {
+        client.indices.resolve_cluster
+      }.to raise_exception(ArgumentError)
+    end
+  end
+
+
+  context 'when name is specified' do
+    it 'performs the request' do
+      expect(client_double.indices.resolve_cluster(name: 'foo')).to be_a Elasticsearch::API::Response
+    end
+  end
+end
