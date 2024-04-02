99456fefca33e7f53f8c2b30a15c63523aa7ab74
Fernando Briano <fernando@picandocodigo.net>
2024-04-02 15:04:58 +0100

    [API] Adds new namespace DataStreams

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/data_streams/get_global_retention.rb
@@ -0,0 +1,55 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from build hash f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch/tree/main/rest-api-spec
+#
+module Elasticsearch
+  module API
+    module DataStreams
+      module Actions
+        # Returns global retention configuration that applies to all data streams managed by the data stream lifecycle.
+        # This functionality is Experimental and may be changed or removed
+        # completely in a future release. Elastic will take a best effort approach
+        # to fix any issues, but experimental features are not subject to the
+        # support SLA of official GA features.
+        #
+        # @option arguments [Boolean] :local Return the global retention retrieved from the node that received the request.
+        # @option arguments [Hash] :headers Custom HTTP headers
+        #
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/data-streams-get-global-retention.html
+        #
+        def get_global_retention(arguments = {})
+          request_opts = { endpoint: arguments[:endpoint] || 'data_streams.get_global_retention' }
+
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body   = nil
+
+          method = Elasticsearch::API::HTTP_GET
+          path   = '_data_stream/_global_retention'
+          params = Utils.process_params(arguments)
+
+          Elasticsearch::API::Response.new(
+            perform_request(method, path, params, body, headers, request_opts)
+          )
+        end
+      end
+    end
+  end
+end
