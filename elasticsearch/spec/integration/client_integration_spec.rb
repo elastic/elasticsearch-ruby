df212cf6afd56c66b376151964174a37f922bfa7
Fernando Briano <fernando@picandocodigo.net>
2024-05-14 14:36:10 +0100

    [Tests] Reorders checking for valid server

--- elasticsearch/spec/integration/client_integration_spec.rb
+++ elasticsearch/spec/integration/client_integration_spec.rb
@@ -14,11 +14,12 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-ELASTICSEARCH_URL = ENV['TEST_ES_SERVER'] || "http://localhost:#{(ENV['PORT'] || 9200)}"
-raise URI::InvalidURIError unless ELASTICSEARCH_URL =~ /\A#{URI::DEFAULT_PARSER.make_regexp}\z/
 
 require 'spec_helper'
 require 'logger'
+
+ELASTICSEARCH_URL = ENV['TEST_ES_SERVER'] || "http://localhost:#{(ENV['PORT'] || 9200)}"
+raise URI::InvalidURIError unless ELASTICSEARCH_URL =~ /\A#{URI::DEFAULT_PARSER.make_regexp}\z/
 
 context 'Elasticsearch client' do
   let(:logger) { Logger.new($stderr) }
