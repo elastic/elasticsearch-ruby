ee7f964d1e9475570486197679ce43836c9ffee4
Fernando Briano <fernando@picandocodigo.net>
2024-07-04 08:30:52 +0100

    Test Runner: Adds enterprise search users for entsearch platinum tests

--- elasticsearch-api/spec/rest_api/rest_api_tests_helper.rb
+++ elasticsearch-api/spec/rest_api/rest_api_tests_helper.rb
@@ -65,7 +65,10 @@ unless ADMIN_CLIENT.ping
   exit
 end
 
-Elasticsearch::RestAPIYAMLTests::WipeCluster.create_xpack_rest_user(ADMIN_CLIENT) if test_suite == 'platinum'
+if test_suite == 'platinum'
+  Elasticsearch::RestAPIYAMLTests::WipeCluster.create_xpack_rest_user(ADMIN_CLIENT)
+  Elasticsearch::RestAPIYAMLTests::WipeCluster.create_enterprise_search_users(ADMIN_CLIENT)
+end
 
 YAML_FILES_DIRECTORY = "#{PROJECT_PATH}/../../tmp/rest-api-spec/test/#{test_suite}".freeze
 
