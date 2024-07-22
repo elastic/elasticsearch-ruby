1857071fd18f60f80ba463b70dc95081e5c72a97
Fernando Briano <fernando@picandocodigo.net>
2024-07-22 17:11:10 +0100

    [API] Adds geoip_database endpoints - delete, get, put

---
+++ elasticsearch-api/spec/elasticsearch/api/actions/ingest/get_geoip_database_spec.rb
@@ -0,0 +1,39 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+require 'spec_helper'
+
+describe 'client.ingest#get_geoip_database' do
+  let(:expected_args) do
+    [
+      'GET',
+      '_ingest/geoip/database/foo',
+      {},
+      nil,
+      {},
+      { defined_params: { id: 'foo' }, endpoint: 'ingest.get_geoip_database' }
+    ]
+  end
+
+  let(:client) do
+    Class.new { include Elasticsearch::API }.new
+  end
+
+  it 'performs the request' do
+    expect(client_double.ingest.get_geoip_database(id: 'foo', body: {})).to be_a Elasticsearch::API::Response
+  end
+end
