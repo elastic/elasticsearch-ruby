7fa3fc907ead079ffa08e069daceea0757645f2a
Fernando Briano <fernando@picandocodigo.net>
2024-10-03 09:33:01 +0100

    [API] Updates source code docs

--- elasticsearch-api/lib/elasticsearch/api/actions/ingest/put_geoip_database.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/ingest/put_geoip_database.rb
@@ -28,7 +28,7 @@ module Elasticsearch
         # @option arguments [Hash] :headers Custom HTTP headers
         # @option arguments [Hash] :body The database configuration definition (*Required*)
         #
-        # @see https://www.elastic.co/guide/en/elasticsearch/reference/8.15/TODO.html
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/8.15/put-geoip-database-api.html
         #
         def put_geoip_database(arguments = {})
           request_opts = { endpoint: arguments[:endpoint] || 'ingest.put_geoip_database' }
