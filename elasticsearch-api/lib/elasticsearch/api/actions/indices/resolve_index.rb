23723df86f15300f9c0be4a4cd4759dcc68f12c4
Fernando Briano <fernando@picandocodigo.net>
2024-10-03 09:33:01 +0100

    [API] Updates source code docs

--- elasticsearch-api/lib/elasticsearch/api/actions/indices/resolve_index.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/indices/resolve_index.rb
@@ -26,6 +26,8 @@ module Elasticsearch
         #
         # @option arguments [List] :name A comma-separated list of names or wildcard expressions
         # @option arguments [String] :expand_wildcards Whether wildcard expressions should get expanded to open or closed indices (default: open) (options: open, closed, hidden, none, all)
+        # @option arguments [Boolean] :ignore_unavailable Whether specified concrete indices should be ignored when unavailable (missing or closed)
+        # @option arguments [Boolean] :allow_no_indices Whether to ignore if a wildcard indices expression resolves into no concrete indices. (This includes `_all` string or when no indices have been specified)
         # @option arguments [Hash] :headers Custom HTTP headers
         #
         # @see https://www.elastic.co/guide/en/elasticsearch/reference/8.15/indices-resolve-index-api.html
