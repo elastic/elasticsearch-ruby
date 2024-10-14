29df6151656017b6e1ee97c0cd7d9e29956e396c
Fernando Briano <fernando@picandocodigo.net>
2024-10-14 11:01:41 +0100

    [API] Adds ingest ip_location APIs
    - delete_ip_location_database
    - get_ip_location_database
    - put_ip_location_database

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/ingest/get_ip_location_database.rb
@@ -0,0 +1,62 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from build hash f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch/tree/main/rest-api-spec
+#
+module Elasticsearch
+  module API
+    module Ingest
+      module Actions
+        # Returns the specified ip location database configuration
+        #
+        # @option arguments [List] :id A comma-separated list of ip location database configurations to get; use `*` to get all ip location database configurations
+        # @option arguments [Hash] :headers Custom HTTP headers
+        #
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/8.16/get-ip-location-database-api.html
+        #
+        def get_ip_location_database(arguments = {})
+          request_opts = { endpoint: arguments[:endpoint] || 'ingest.get_ip_location_database' }
+
+          defined_params = [:id].each_with_object({}) do |variable, set_variables|
+            set_variables[variable] = arguments[variable] if arguments.key?(variable)
+          end
+          request_opts[:defined_params] = defined_params unless defined_params.empty?
+
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body = nil
+
+          _id = arguments.delete(:id)
+
+          method = Elasticsearch::API::HTTP_GET
+          path   = if _id
+                     "_ingest/ip_location/database/#{Utils.__listify(_id)}"
+                   else
+                     '_ingest/ip_location/database'
+                   end
+          params = {}
+
+          Elasticsearch::API::Response.new(
+            perform_request(method, path, params, body, headers, request_opts)
+          )
+        end
+      end
+    end
+  end
+end
