c6b5583b80f65885d107c2472a55bb4ee0c5c1c7
Fernando Briano <fernando@picandocodigo.net>
2024-12-10 10:20:37 +0000

    [API] Adds master_timeout to cat.aliases, indices.exists_aliases, indices.get_alias

--- elasticsearch-api/lib/elasticsearch/api/actions/indices/get_alias.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/indices/get_alias.rb
@@ -29,6 +29,7 @@ module Elasticsearch
         # @option arguments [Boolean] :ignore_unavailable Whether specified concrete indices should be ignored when unavailable (missing or closed)
         # @option arguments [Boolean] :allow_no_indices Whether to ignore if a wildcard indices expression resolves into no concrete indices. (This includes `_all` string or when no indices have been specified)
         # @option arguments [String] :expand_wildcards Whether to expand wildcard expression to concrete indices that are open, closed or both. (options: open, closed, hidden, none, all)
+        # @option arguments [Time] :master_timeout Timeout for waiting for new cluster state in case it is blocked
         # @option arguments [Hash] :headers Custom HTTP headers
         #
         # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/indices-aliases.html
