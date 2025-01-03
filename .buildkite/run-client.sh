ca41a398485013f73cfd6ec66ce23a97c213ea09
Fernando Briano <fernando@picandocodigo.net>
2025-01-03 10:23:59 +0000

    [CI] Create Serverless project

--- .buildkite/run-client.sh
+++ .buildkite/run-client.sh
@@ -9,6 +9,19 @@ repo=`pwd`
 export RUBY_VERSION=${RUBY_VERSION:-3.1}
 export TRANSPORT_VERSION=${TRANSPORT_VERSION:-8}
 
+if [[ "$TEST_SUITE" == "serverless" ]]; then
+  if [[ -z $EC_PROJECT_PREFIX ]]; then
+    echo -e "\033[31;1mERROR:\033[0m Required environment variable [EC_PROJECT_PREFIX] not set\033[0m"
+    exit 1
+  fi
+
+  # Create a serverless project:
+  source $script_path/create-serverless.sh
+
+  # Make sure we remove projects:
+  trap cleanup EXIT
+fi
+
 echo "--- :ruby: Building Docker image"
 docker build \
        --file $script_path/Dockerfile \
