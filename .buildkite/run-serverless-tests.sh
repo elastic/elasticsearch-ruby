92f6912a170b74e952047e9bada4f1ee6aebe50d
Fernando Briano <fernando@picandocodigo.net>
2025-01-03 10:27:40 +0000

    [CI] Updates pipeline, adds info task

---
+++ .buildkite/run-serverless-tests.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+#
+# Once called Elasticsearch should be up and running
+#
+script_path=$(dirname $(realpath -s $0))
+set -euo pipefail
+repo=`pwd`
+
+export RUBY_VERSION=${RUBY_VERSION:-3.4}
+export TRANSPORT_VERSION=${TRANSPORT_VERSION:-8}
+
+if [[ -z $EC_PROJECT_PREFIX ]]; then
+  echo -e "\033[31;1mERROR:\033[0m Required environment variable [EC_PROJECT_PREFIX] not set\033[0m"
+  exit 1
+fi
+
+# Create a serverless project:
+source $script_path/create-serverless.sh
+# Make sure we remove projects:
+trap cleanup EXIT
+
+echo "--- :ruby: Building Docker image"
+docker build \
+       --file $script_path/Dockerfile \
+       --tag elastic/elasticsearch-ruby \
+       --build-arg RUBY_VERSION=$RUBY_VERSION \
+       --build-arg TRANSPORT_VERSION=$TRANSPORT_VERSION \
+       --build-arg RUBY_SOURCE=$RUBY_SOURCE \
+       .
+
+echo "--- :ruby: Running $TEST_SUITE tests"
+docker run \
+       -e "ELASTIC_USER=elastic" \
+       -e "BUILDKITE=true" \
+       -e "QUIET=${QUIET}" \
+       -e "TRANSPORT_VERSION=${TRANSPORT_VERSION}" \
+       -e "ELASTICSEARCH_URL=${ELASTICSEARCH_URL}" \
+       -e "API_KEY=${ES_API_SECRET_KEY}" \
+       --volume $repo:/usr/src/app \
+       --name elasticsearch-ruby \
+       --rm \
+       elastic/elasticsearch-ruby \
+       bundle exec rake info
