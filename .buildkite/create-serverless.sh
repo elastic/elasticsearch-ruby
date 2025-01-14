09c47467b39b7cc8b1dd5f042f3b8d577a57c343
Fernando Briano <fernando@picandocodigo.net>
2025-01-14 17:46:40 +0000

    [CI] Separating jobs in pipeline for Serverless

--- .buildkite/create-serverless.sh
+++ .buildkite/create-serverless.sh
@@ -2,17 +2,14 @@
 
 set -euo pipefail
 
+if [[ -z $EC_PROJECT_PREFIX ]]; then
+  echo -e "\033[31;1mERROR:\033[0m Required environment variable [EC_PROJECT_PREFIX] not set\033[0m"
+  exit 1
+fi
+
 # Using BUILDKITE_JOB_ID for the name to make it unique:
-export EC_PROJECT_NAME="$EC_PROJECT_PREFIX-$BUILDKITE_JOB_ID"
-
-# ensure serverless instance is deleted even if script errors
-cleanup() {
-  echo -e "--- :elasticsearch: :broom::sparkles: Tear down serverless instance $EC_PROJECT_NAME"
-  qaf elastic-cloud projects delete
-  rm -rf "~/.elastic/cloud.json"
-}
-trap cleanup EXIT
-
+# export EC_PROJECT_NAME="$EC_PROJECT_PREFIX-$BUILDKITE_JOB_ID"
+export EC_PROJECT_NAME="$EC_PROJECT_PREFIX-test"
 echo -e "--- :elasticsearch: Start serverless instance $EC_PROJECT_NAME"
 
 qaf elastic-cloud projects create --project-type elasticsearch
@@ -21,6 +18,8 @@ deployment=$(qaf elastic-cloud projects describe $EC_PROJECT_NAME --as-json --sh
 # Set ELASTICSEARCH_URL and API_KEY variables
 export ES_API_SECRET_KEY=$(echo "$deployment" | jq -r '.credentials.api_key')
 export ELASTICSEARCH_URL=$(echo "$deployment" | jq -r '.elasticsearch.url')
+buildkite-agent meta-data set "ES_API_SECRET_KEY" $ES_API_SECRET_KEY
+buildkite-agent meta-data set "ELASTICSEARCH_URL" $ELASTICSEARCH_URL
 
 echo -e "--- :computer: Environment variables"
 echo -e "ELASTICSEARCH_URL $ELASTICSEARCH_URL"
