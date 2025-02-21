9ab3ed9445e2bee033d3ca749b334855399a7256
Fernando Briano <fernando@picandocodigo.net>
2025-02-21 09:41:28 +0000

    [API] Adds migrate endpoints from elasticsearch-specification

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/indices/migrate_reindex.rb
@@ -0,0 +1,60 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from commit dcb1c1df18a84a0182caa631b4577d89a4602cfe
+# @see https://github.com/elastic/elasticsearch-specification
+#
+module Elasticsearch
+  module API
+    module Indices
+      module Actions
+        # Reindex legacy backing indices.
+        # Reindex all legacy backing indices for a data stream.
+        # This operation occurs in a persistent task.
+        # The persistent task ID is returned immediately and the reindexing work is completed in that task.
+        # This functionality is Experimental and may be changed or removed
+        # completely in a future release. Elastic will take a best effort approach
+        # to fix any issues, but experimental features are not subject to the
+        # support SLA of official GA features.
+        #
+        # @option arguments [Hash] :headers Custom HTTP headers
+        # @option arguments [Hash] :body reindex
+        #
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/migrate-data-stream.html
+        #
+        def migrate_reindex(arguments = {})
+          request_opts = { endpoint: arguments[:endpoint] || 'indices.migrate_reindex' }
+
+          raise ArgumentError, "Required argument 'body' missing" unless arguments[:body]
+
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body = arguments.delete(:body)
+
+          method = Elasticsearch::API::HTTP_POST
+          path   = "_migration/reindex"
+          params = {}
+
+          Elasticsearch::API::Response.new(
+            perform_request(method, path, params, body, headers, request_opts)
+          )
+        end
+      end
+    end
+  end
+end
