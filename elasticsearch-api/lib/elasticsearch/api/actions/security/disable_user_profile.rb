b604e6574adb5dbd9b6d68a5b7457dc5a8d6afd6
Fernando Briano <fernando@picandocodigo.net>
2025-02-26 15:20:16 +0000

    [API] Adds user profile APIs

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/security/disable_user_profile.rb
@@ -0,0 +1,70 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from commit f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch-specification
+#
+module Elasticsearch
+  module API
+    module Security
+      module Actions
+        # Disable a user profile.
+        # Disable user profiles so that they are not visible in user profile searches.
+        # NOTE: The user profile feature is designed only for use by Kibana and Elastic's Observability, Enterprise Search, and Elastic Security solutions.
+        # Individual users and external applications should not call this API directly.
+        # Elastic reserves the right to change or remove this feature in future releases without prior notice.
+        # When you activate a user profile, its automatically enabled and visible in user profile searches. You can use the disable user profile API to disable a user profile so it’s not visible in these searches.
+        # To re-enable a disabled user profile, use the enable user profile API .
+        #
+        # @option arguments [String] :uid Unique identifier for the user profile. (*Required*)
+        # @option arguments [String] :refresh If 'true', Elasticsearch refreshes the affected shards to make this operation visible to search.
+        #  If 'wait_for', it waits for a refresh to make this operation visible to search.
+        #  If 'false', it does nothing with refreshes. Server default: false.
+        # @option arguments [Hash] :headers Custom HTTP headers
+        #
+        # @see https://www.elastic.co/docs/api/doc/elasticsearch/operation/operation-security-disable-user-profile
+        #
+        def disable_user_profile(arguments = {})
+          request_opts = { endpoint: arguments[:endpoint] || 'security.disable_user_profile' }
+
+          defined_params = [:uid].inject({}) do |set_variables, variable|
+            set_variables[variable] = arguments[variable] if arguments.key?(variable)
+            set_variables
+          end
+          request_opts[:defined_params] = defined_params unless defined_params.empty?
+
+          raise ArgumentError, "Required argument 'uid' missing" unless arguments[:uid]
+
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body = nil
+
+          _uid = arguments.delete(:uid)
+
+          method = Elasticsearch::API::HTTP_PUT
+          path   = "_security/profile/#{Utils.__listify(_uid)}/_disable"
+          params = Utils.process_params(arguments)
+
+          Elasticsearch::API::Response.new(
+            perform_request(method, path, params, body, headers, request_opts)
+          )
+        end
+      end
+    end
+  end
+end
---
+++ elasticsearch-api/lib/elasticsearch/api/actions/security/has_privileges_user_profile.rb
@@ -0,0 +1,56 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from commit f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch-specification
+#
+module Elasticsearch
+  module API
+    module Security
+      module Actions
+        # Check user profile privileges.
+        # Determine whether the users associated with the specified user profile IDs have all the requested privileges.
+        # NOTE: The user profile feature is designed only for use by Kibana and Elastic's Observability, Enterprise Search, and Elastic Security solutions. Individual users and external applications should not call this API directly.
+        # Elastic reserves the right to change or remove this feature in future releases without prior notice.
+        #
+        # @option arguments [Hash] :headers Custom HTTP headers
+        # @option arguments [Hash] :body request body
+        #
+        # @see https://www.elastic.co/docs/api/doc/elasticsearch/operation/operation-security-has-privileges-user-profile
+        #
+        def has_privileges_user_profile(arguments = {})
+          request_opts = { endpoint: arguments[:endpoint] || 'security.has_privileges_user_profile' }
+
+          raise ArgumentError, "Required argument 'body' missing" unless arguments[:body]
+
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body = arguments.delete(:body)
+
+          method = Elasticsearch::API::HTTP_POST
+          path   = "_security/profile/_has_privileges"
+          params = {}
+
+          Elasticsearch::API::Response.new(
+            perform_request(method, path, params, body, headers, request_opts)
+          )
+        end
+      end
+    end
+  end
+end
