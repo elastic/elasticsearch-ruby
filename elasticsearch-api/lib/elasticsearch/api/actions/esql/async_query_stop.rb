123cd7c2360022ba9fe3712b6fd6146cec2c60c1
Fernando Briano <fernando@picandocodigo.net>
2025-03-04 14:38:31 +0000

    [API] Updates esql.async_query_stop

--- elasticsearch-api/lib/elasticsearch/api/actions/esql/async_query_stop.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/esql/async_query_stop.rb
@@ -15,16 +15,22 @@
 # specific language governing permissions and limitations
 # under the License.
 #
-# Auto generated from build hash f284cc16f4d4b4289bc679aa1529bb504190fe80
-# @see https://github.com/elastic/elasticsearch/tree/main/rest-api-spec
+# Auto generated from commit f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch-specification
 #
 module Elasticsearch
   module API
     module Esql
       module Actions
-        # Stops a previously submitted async query request given its ID and collects the results.
+        # Stop async ES|QL query.
+        # This API interrupts the query execution and returns the results so far.
+        # If the Elasticsearch security features are enabled, only the user who first submitted the ES|QL query can stop it.
         #
-        # @option arguments [String] :id The async query ID
+        # @option arguments [String] :id The unique identifier of the query.
+        #  A query ID is provided in the ES|QL async query API response for a query that does not complete in the designated time.
+        #  A query ID is also provided when the request was submitted with the +keep_on_completion+ parameter set to +true+. (*Required*)
+        # @option arguments [Boolean] :drop_null_columns Indicates whether columns that are entirely +null+ will be removed from the +columns+ and +values+ portion of the results.
+        #  If +true+, the response will include an extra section under the name +all_columns+ which has the name of all the columns.
         # @option arguments [Hash] :headers Custom HTTP headers
         #
         # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/esql-async-query-stop-api.html
@@ -32,8 +38,9 @@ module Elasticsearch
         def async_query_stop(arguments = {})
           request_opts = { endpoint: arguments[:endpoint] || 'esql.async_query_stop' }
 
-          defined_params = [:id].each_with_object({}) do |variable, set_variables|
+          defined_params = [:id].inject({}) do |set_variables, variable|
             set_variables[variable] = arguments[variable] if arguments.key?(variable)
+            set_variables
           end
           request_opts[:defined_params] = defined_params unless defined_params.empty?
 
@@ -48,7 +55,7 @@ module Elasticsearch
 
           method = Elasticsearch::API::HTTP_POST
           path   = "_query/async/#{Utils.__listify(_id)}/stop"
-          params = {}
+          params = Utils.process_params(arguments)
 
           Elasticsearch::API::Response.new(
             perform_request(method, path, params, body, headers, request_opts)
