0fe74130a974c81fe20c94683e3f2236862128a3
Elastic Machine <elasticmachine@users.noreply.github.com>
2025-03-24 06:03:19 +0000

    Auto-generated API code

--- elasticsearch-api/lib/elasticsearch/api/actions/indices/get_data_lifecycle_stats.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/indices/get_data_lifecycle_stats.rb
@@ -26,7 +26,7 @@ module Elasticsearch
         #
         # @option arguments [Hash] :headers Custom HTTP headers
         #
-        # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/data-streams-get-lifecycle-stats.html
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/9.0/data-streams-get-lifecycle-stats.html
         #
         def get_data_lifecycle_stats(arguments = {})
           request_opts = { endpoint: arguments[:endpoint] || 'indices.get_data_lifecycle_stats' }
