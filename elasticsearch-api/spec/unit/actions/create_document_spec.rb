949f365320c66c5e868adf2b4a810e35c37d76f1
Fernando Briano <fernando@picandocodigo.net>
2025-03-31 14:57:58 +0100

    [API] Updates create endpoint for new behaviour.
    The API now makes a request, instead of calling index like in previous versions (< 9.0.0).
    * `body`, `index` and `id` are now required parameters.
    * `op_type` is now a String parameter: Set to 'create' to only index the document if it does not
    already exist (put if absent). If a document with the specified '_id' already exists, the indexing
    operation will fail. The behavior is the same as using the '<index>/_create' endpoint. If a document
    ID is specified, this paramater defaults to 'index'. Otherwise, it defaults to `create`. If the
    request targets a data stream, an `op_type` of `create` is required.
    
    Additional new parameters:
    * :if_primary_term (Integer) -  Only perform the operation if the document has this primary term.
    * :if_seq_no (Integer) - Only perform the operation if the document has this sequence number.

--- elasticsearch-api/spec/unit/actions/create_document_spec.rb
+++ elasticsearch-api/spec/unit/actions/create_document_spec.rb
@@ -18,42 +18,40 @@
 require 'spec_helper'
 
 describe 'client#create_document' do
-
   let(:expected_args) do
     [
-        'PUT',
-        'foo/_doc/123',
-        { op_type: 'create' },
-        { foo: 'bar' },
-        {},
-        { defined_params: { id: '123', index: 'foo' }, endpoint: 'index' }
+      'PUT',
+      'foo/_create/123',
+      {},
+      { foo: 'bar' },
+      {},
+      { defined_params: { id: '123', index: 'foo' }, endpoint: 'create' }
     ]
   end
 
   it 'performs the request' do
-    expect(client_double.create(index: 'foo', id: '123', body: { foo: 'bar'})).to be_a Elasticsearch::API::Response
+    expect(client_double.create(index: 'foo', id: '123', body: { foo: 'bar' })).to be_a Elasticsearch::API::Response
   end
 
   context 'when the request needs to be URL-escaped' do
-
     let(:expected_args) do
       [
-          'PUT',
-          'foo/_doc/123',
-          { op_type: 'create' },
-          {},
-          {}
+        'PUT',
+        'foo/_doc/123',
+        {},
+        {},
+        {}
       ]
     end
 
     let(:expected_args) do
       [
         'PUT',
-        'foo/_doc/123',
-        { op_type: 'create' },
+        'foo/_create/123',
         {},
         {},
-        { defined_params: { id: '123', index: 'foo' }, endpoint: 'index' }
+        {},
+        { defined_params: { id: '123', index: 'foo' }, endpoint: 'create' }
       ]
     end
 
@@ -63,58 +61,29 @@ describe 'client#create_document' do
   end
 
   context 'when an id is provided as an integer' do
-
-    let(:expected_args) do
-      [
-          'PUT',
-          'foo/_doc/1',
-          { op_type: 'create' },
-          { foo: 'bar' },
-          {}
-      ]
-    end
-
     let(:expected_args) do
       [
         'PUT',
         'foo/_doc/1',
         { op_type: 'create' },
         { foo: 'bar' },
+        {}
+      ]
+    end
+
+    let(:expected_args) do
+      [
+        'PUT',
+        'foo/_create/1',
         {},
-        { defined_params: { id: 1, index: 'foo' }, endpoint: 'index' }
-      ]
-    end
-
-    it 'updates the arguments with the `op_type`' do
-      expect(client_double.create(index: 'foo', id: 1, body: { foo: 'bar' })).to be_a Elasticsearch::API::Response
-    end
-  end
-
-  context 'when an id is not provided' do
-
-    let(:expected_args) do
-      [
-          'POST',
-          'foo/_doc',
-          { },
-          { foo: 'bar' },
-          {}
-      ]
-    end
-
-    let(:expected_args) do
-      [
-        'POST',
-        'foo/_doc',
-        { },
         { foo: 'bar' },
         {},
-        { defined_params: { index: 'foo' }, endpoint: 'index' }
+        { defined_params: { id: 1, index: 'foo' }, endpoint: 'create' }
       ]
     end
 
-    it 'updates the arguments with the `op_type`' do
-      expect(client_double.create(index: 'foo', body: { foo: 'bar' })).to be_a Elasticsearch::API::Response
+    it 'performs the request' do
+      expect(client_double.create(index: 'foo', id: 1, body: { foo: 'bar' })).to be_a Elasticsearch::API::Response
     end
   end
 end
