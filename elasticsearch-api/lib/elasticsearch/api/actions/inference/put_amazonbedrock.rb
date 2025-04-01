897d5991e7938513bf9e1832069a9ea6bfbfad07
Fernando Briano <fernando@picandocodigo.net>
2025-04-01 09:25:24 +0100

    [API] Adds new inference PUT endpoints

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/inference/put_amazonbedrock.rb
@@ -0,0 +1,70 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from commit f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch-specification
+#
+module Elasticsearch
+  module API
+    module Inference
+      module Actions
+        # Create an Amazon Bedrock inference endpoint.
+        # Creates an inference endpoint to perform an inference task with the +amazonbedrock+ service.
+        #
+        # @option arguments [String] :task_type The type of the inference task that the model will perform. (*Required*)
+        # @option arguments [String] :amazonbedrock_inference_id The unique identifier of the inference endpoint. (*Required*)
+        # @option arguments [Hash] :headers Custom HTTP headers
+        # @option arguments [Hash] :body request body
+        #
+        # @see https://www.elastic.co/docs/api/doc/elasticsearch/operation/operation-inference-put-amazonbedrock
+        #
+        def put_amazonbedrock(arguments = {})
+          request_opts = { endpoint: arguments[:endpoint] || 'inference.put_amazonbedrock' }
+
+          defined_params = [:task_type, :amazonbedrock_inference_id].each_with_object({}) do |variable, set_variables|
+            set_variables[variable] = arguments[variable] if arguments.key?(variable)
+          end
+          request_opts[:defined_params] = defined_params unless defined_params.empty?
+
+          raise ArgumentError, "Required argument 'task_type' missing" unless arguments[:task_type]
+
+          unless arguments[:amazonbedrock_inference_id]
+            raise ArgumentError,
+                  "Required argument 'amazonbedrock_inference_id' missing"
+          end
+
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body = arguments.delete(:body)
+
+          _task_type = arguments.delete(:task_type)
+
+          _amazonbedrock_inference_id = arguments.delete(:amazonbedrock_inference_id)
+
+          method = Elasticsearch::API::HTTP_PUT
+          path   = "_inference/#{Utils.listify(_task_type)}/#{Utils.listify(_amazonbedrock_inference_id)}"
+          params = {}
+
+          Elasticsearch::API::Response.new(
+            perform_request(method, path, params, body, headers, request_opts)
+          )
+        end
+      end
+    end
+  end
+end
---
+++ elasticsearch-api/lib/elasticsearch/api/actions/inference/put_jinaai.rb
@@ -0,0 +1,77 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from commit f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch-specification
+#
+module Elasticsearch
+  module API
+    module Inference
+      module Actions
+        # Create an JinaAI inference endpoint.
+        # Create an inference endpoint to perform an inference task with the +jinaai+ service.
+        # To review the available +rerank+ models, refer to <https://jina.ai/reranker>.
+        # To review the available +text_embedding+ models, refer to the <https://jina.ai/embeddings/>.
+        # When you create an inference endpoint, the associated machine learning model is automatically deployed if it is not already running.
+        # After creating the endpoint, wait for the model deployment to complete before using it.
+        # To verify the deployment status, use the get trained model statistics API.
+        # Look for +"state": "fully_allocated"+ in the response and ensure that the +"allocation_count"+ matches the +"target_allocation_count"+.
+        # Avoid creating multiple endpoints for the same model unless required, as each endpoint consumes significant resources.
+        #
+        # @option arguments [String] :task_type The type of the inference task that the model will perform. (*Required*)
+        # @option arguments [String] :jinaai_inference_id The unique identifier of the inference endpoint. (*Required*)
+        # @option arguments [Hash] :headers Custom HTTP headers
+        # @option arguments [Hash] :body request body
+        #
+        # @see https://www.elastic.co/docs/api/doc/elasticsearch/operation/operation-inference-put-jinaai
+        #
+        def put_jinaai(arguments = {})
+          request_opts = { endpoint: arguments[:endpoint] || 'inference.put_jinaai' }
+
+          defined_params = [:task_type, :jinaai_inference_id].each_with_object({}) do |variable, set_variables|
+            set_variables[variable] = arguments[variable] if arguments.key?(variable)
+          end
+          request_opts[:defined_params] = defined_params unless defined_params.empty?
+
+          raise ArgumentError, "Required argument 'task_type' missing" unless arguments[:task_type]
+
+          unless arguments[:jinaai_inference_id]
+            raise ArgumentError,
+                  "Required argument 'jinaai_inference_id' missing"
+          end
+
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body = arguments.delete(:body)
+
+          _task_type = arguments.delete(:task_type)
+
+          _jinaai_inference_id = arguments.delete(:jinaai_inference_id)
+
+          method = Elasticsearch::API::HTTP_PUT
+          path   = "_inference/#{Utils.listify(_task_type)}/#{Utils.listify(_jinaai_inference_id)}"
+          params = {}
+
+          Elasticsearch::API::Response.new(
+            perform_request(method, path, params, body, headers, request_opts)
+          )
+        end
+      end
+    end
+  end
+end
