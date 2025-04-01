8c1ce1b99d0492735cb4a7725689c27f25a24565
Fernando Briano <fernando@picandocodigo.net>
2025-04-01 09:25:24 +0100

    [API] Updates inference endpoints

--- elasticsearch-api/lib/elasticsearch/api/actions/inference/chat_completion_unified.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/inference/chat_completion_unified.rb
@@ -15,47 +15,43 @@
 # specific language governing permissions and limitations
 # under the License.
 #
-# Auto generated from commit c34efd13258a01121b10a538e5b450b4e65c7bf3
+# Auto generated from commit 69cbe7cbe9f49a2886bb419ec847cffb58f8b4fb
 # @see https://github.com/elastic/elasticsearch-specification
 #
 module Elasticsearch
   module API
     module Inference
       module Actions
-        # Get an inference endpoint
+        # Perform chat completion inference
         #
-        # @option arguments [String] :task_type The task type
-        # @option arguments [String] :inference_id The inference Id
+        # @option arguments [String] :inference_id The inference Id (*Required*)
+        # @option arguments [Time] :timeout Specifies the amount of time to wait for the inference request to complete. Server default: 30s.
         # @option arguments [Hash] :headers Custom HTTP headers
+        # @option arguments [Hash] :body chat_completion_request
         #
-        # @see https://www.elastic.co/docs/api/doc/elasticsearch/operation/operation-inference-get
+        # @see https://www.elastic.co/docs/api/doc/elasticsearch/operation/operation-inference-unified-inference
         #
-        def get(arguments = {})
-          request_opts = { endpoint: arguments[:endpoint] || 'inference.get' }
+        def chat_completion_unified(arguments = {})
+          request_opts = { endpoint: arguments[:endpoint] || 'inference.chat_completion_unified' }
 
-          defined_params = [:inference_id, :task_type].each_with_object({}) do |variable, set_variables|
+          defined_params = [:inference_id].each_with_object({}) do |variable, set_variables|
             set_variables[variable] = arguments[variable] if arguments.key?(variable)
           end
           request_opts[:defined_params] = defined_params unless defined_params.empty?
 
+          raise ArgumentError, "Required argument 'body' missing" unless arguments[:body]
+          raise ArgumentError, "Required argument 'inference_id' missing" unless arguments[:inference_id]
+
           arguments = arguments.clone
           headers = arguments.delete(:headers) || {}
 
-          body = nil
-
-          _task_type = arguments.delete(:task_type)
+          body = arguments.delete(:body)
 
           _inference_id = arguments.delete(:inference_id)
 
-          method = Elasticsearch::API::HTTP_GET
-          path   = if _task_type && _inference_id
-                     "_inference/#{Utils.listify(_task_type)}/#{Utils.listify(_inference_id)}"
-                   elsif _inference_id
-                     "_inference/#{Utils.listify(_inference_id)}"
-                   else
-                     '_inference'
-                   end
-          params = {}
+          method = Elasticsearch::API::HTTP_POST
+          path   = "_inference/chat_completion/#{Utils.listify(_inference_id)}/_stream"
+          params = Utils.process_params(arguments)
 
           Elasticsearch::API::Response.new(
             perform_request(method, path, params, body, headers, request_opts)
--- elasticsearch-api/lib/elasticsearch/api/actions/inference/put_watsonx.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/inference/put_watsonx.rb
@@ -15,40 +15,45 @@
 # specific language governing permissions and limitations
 # under the License.
 #
-# Auto generated from commit c34efd13258a01121b10a538e5b450b4e65c7bf3
+# Auto generated from commit 69cbe7cbe9f49a2886bb419ec847cffb58f8b4fb
 # @see https://github.com/elastic/elasticsearch-specification
 #
 module Elasticsearch
   module API
     module Inference
       module Actions
-        # Create an inference endpoint.
+        # Create a Watsonx inference endpoint.
+        # Create an inference endpoint to perform an inference task with the +watsonxai+ service.
+        # You need an IBM Cloud Databases for Elasticsearch deployment to use the +watsonxai+ inference service.
+        # You can provision one through the IBM catalog, the Cloud Databases CLI plug-in, the Cloud Databases API, or Terraform.
         # When you create an inference endpoint, the associated machine learning model is automatically deployed if it is not already running.
         # After creating the endpoint, wait for the model deployment to complete before using it.
         # To verify the deployment status, use the get trained model statistics API.
         # Look for +"state": "fully_allocated"+ in the response and ensure that the +"allocation_count"+ matches the +"target_allocation_count"+.
         # Avoid creating multiple endpoints for the same model unless required, as each endpoint consumes significant resources.
-        # IMPORTANT: The inference APIs enable you to use certain services, such as built-in machine learning models (ELSER, E5), models uploaded through Eland, Cohere, OpenAI, Mistral, Azure OpenAI, Google AI Studio, Google Vertex AI, Anthropic, Watsonx.ai, or Hugging Face.
-        # For built-in models and models uploaded through Eland, the inference APIs offer an alternative way to use and manage trained models.
-        # However, if you do not plan to use the inference APIs to use these models or if you want to use non-NLP models, use the machine learning trained model APIs.
         #
-        # @option arguments [String] :task_type The task type
-        # @option arguments [String] :inference_id The inference Id (*Required*)
+        # @option arguments [String] :task_type The task type.
+        #  The only valid task type for the model to perform is +text_embedding+. (*Required*)
+        # @option arguments [String] :watsonx_inference_id The unique identifier of the inference endpoint. (*Required*)
         # @option arguments [Hash] :headers Custom HTTP headers
-        # @option arguments [Hash] :body inference_config
+        # @option arguments [Hash] :body request body
         #
-        # @see https://www.elastic.co/docs/api/doc/elasticsearch/operation/operation-inference-put
+        # @see https://www.elastic.co/docs/api/doc/elasticsearch/operation/operation-inference-put-watsonx
         #
-        def put(arguments = {})
-          request_opts = { endpoint: arguments[:endpoint] || 'inference.put' }
+        def put_watsonx(arguments = {})
+          request_opts = { endpoint: arguments[:endpoint] || 'inference.put_watsonx' }
 
-          defined_params = [:inference_id, :task_type].each_with_object({}) do |variable, set_variables|
+          defined_params = [:task_type, :watsonx_inference_id].each_with_object({}) do |variable, set_variables|
             set_variables[variable] = arguments[variable] if arguments.key?(variable)
           end
           request_opts[:defined_params] = defined_params unless defined_params.empty?
 
-          raise ArgumentError, "Required argument 'body' missing" unless arguments[:body]
-          raise ArgumentError, "Required argument 'inference_id' missing" unless arguments[:inference_id]
+          raise ArgumentError, "Required argument 'task_type' missing" unless arguments[:task_type]
+
+          unless arguments[:watsonx_inference_id]
+            raise ArgumentError,
+                  "Required argument 'watsonx_inference_id' missing"
+          end
 
           arguments = arguments.clone
           headers = arguments.delete(:headers) || {}
@@ -57,14 +62,10 @@ module Elasticsearch
 
           _task_type = arguments.delete(:task_type)
 
-          _inference_id = arguments.delete(:inference_id)
+          _watsonx_inference_id = arguments.delete(:watsonx_inference_id)
 
           method = Elasticsearch::API::HTTP_PUT
-          path   = if _task_type && _inference_id
-                     "_inference/#{Utils.listify(_task_type)}/#{Utils.listify(_inference_id)}"
-                   else
-                     "_inference/#{Utils.listify(_inference_id)}"
-                   end
+          path   = "_inference/#{Utils.listify(_task_type)}/#{Utils.listify(_watsonx_inference_id)}"
           params = {}
 
           Elasticsearch::API::Response.new(
