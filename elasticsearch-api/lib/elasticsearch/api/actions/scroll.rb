2b3ef7f5172c17cbab5381072be59eba14a33db8
Fernando Briano <fernando@picandocodigo.net>
2025-04-02 09:30:22 +0100

    [API] Updates scroll and clear_scroll endpoints
    Since sending the `scroll_id` as a parameter was deprecated, now it needs to be sent in the body for
    `clear_scroll`, `scroll`.

--- elasticsearch-api/lib/elasticsearch/api/actions/scroll.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/scroll.rb
@@ -15,34 +15,38 @@
 # specific language governing permissions and limitations
 # under the License.
 #
-# Auto generated from build hash f284cc16f4d4b4289bc679aa1529bb504190fe80
-# @see https://github.com/elastic/elasticsearch/tree/main/rest-api-spec
+# Auto generated from commit f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch-specification
 #
 module Elasticsearch
   module API
     module Actions
-      # Allows to retrieve a large numbers of results from a single search request.
+      # Run a scrolling search.
+      # IMPORTANT: The scroll API is no longer recommend for deep pagination. If you need to preserve the index state while paging through more than 10,000 hits, use the +search_after+ parameter with a point in time (PIT).
+      # The scroll API gets large sets of results from a single scrolling search request.
+      # To get the necessary scroll ID, submit a search API request that includes an argument for the +scroll+ query parameter.
+      # The +scroll+ parameter indicates how long Elasticsearch should retain the search context for the request.
+      # The search response returns a scroll ID in the +_scroll_id+ response body parameter.
+      # You can then use the scroll ID with the scroll API to retrieve the next batch of results for the request.
+      # If the Elasticsearch security features are enabled, the access to the results of a specific scroll ID is restricted to the user or API key that submitted the search.
+      # You can also use the scroll API to specify a new scroll parameter that extends or shortens the retention period for the search context.
+      # IMPORTANT: Results from a scrolling search reflect the state of the index at the time of the initial search request. Subsequent indexing or document changes only affect later search and scroll requests.
       #
-      # @option arguments [String] :scroll_id The scroll ID *Deprecated*
-      # @option arguments [Time] :scroll Specify how long a consistent view of the index should be maintained for scrolled search
-      # @option arguments [Boolean] :rest_total_hits_as_int Indicates whether hits.total should be rendered as an integer or an object in the rest search response
+      # @option arguments [String] :scroll_id The scroll ID
+      # @option arguments [Time] :scroll The period to retain the search context for scrolling. Server default: 1d.
+      # @option arguments [Boolean] :rest_total_hits_as_int If true, the API response’s hit.total property is returned as an integer. If false, the API response’s hit.total property is returned as an object.
       # @option arguments [Hash] :headers Custom HTTP headers
-      # @option arguments [Hash] :body The scroll ID if not passed by URL or query parameter.
+      # @option arguments [Hash] :body request body
       #
       # *Deprecation notice*:
       # A scroll id can be quite large and should be specified as part of the body
       # Deprecated since version 7.0.0
       #
       #
-      # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/search-request-body.html#request-body-search-scroll
+      # @see https://www.elastic.co/docs/api/doc/elasticsearch/operation/operation-scroll
       #
       def scroll(arguments = {})
         request_opts = { endpoint: arguments[:endpoint] || 'scroll' }
-
-        defined_params = [:scroll_id].each_with_object({}) do |variable, set_variables|
-          set_variables[variable] = arguments[variable] if arguments.key?(variable)
-        end
-        request_opts[:defined_params] = defined_params unless defined_params.empty?
 
         arguments = arguments.clone
         headers = arguments.delete(:headers) || {}
@@ -57,11 +61,7 @@ module Elasticsearch
                    Elasticsearch::API::HTTP_GET
                  end
 
-        path = if _scroll_id
-                 "_search/scroll/#{Utils.__listify(_scroll_id)}"
-               else
-                 '_search/scroll'
-               end
+        path   = '_search/scroll'
         params = Utils.process_params(arguments)
 
         Elasticsearch::API::Response.new(
