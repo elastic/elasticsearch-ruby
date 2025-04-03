ffcb5d551f4049c380a2f7da47996dc959d4315b
Fernando Briano <fernando@picandocodigo.net>
2025-04-03 14:57:29 +0100

    [API] Adds security.delegate_pky endpoint

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/security/delegate_pki.rb
@@ -0,0 +1,53 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from build hash f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch/tree/main/rest-api-spec
+#
+module Elasticsearch
+  module API
+    module Security
+      module Actions
+        # Delegate PKI authentication.
+        #
+        # @option arguments [Hash] :headers Custom HTTP headers
+        # @option arguments [Hash] :body The X509Certificate chain. (*Required*)
+        #
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/8.17/security-api-delegate-pki-authentication.html
+        #
+        def delegate_pki(arguments = {})
+          request_opts = { endpoint: arguments[:endpoint] || 'security.delegate_pki' }
+
+          raise ArgumentError, "Required argument 'body' missing" unless arguments[:body]
+
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body   = arguments.delete(:body)
+
+          method = Elasticsearch::API::HTTP_POST
+          path   = '_security/delegate_pki'
+          params = {}
+
+          Elasticsearch::API::Response.new(
+            perform_request(method, path, params, body, headers, request_opts)
+          )
+        end
+      end
+    end
+  end
+end
