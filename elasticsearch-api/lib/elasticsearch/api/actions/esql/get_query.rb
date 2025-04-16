e655e71f1be13cae251ee586b1dafa7db714aa8e
Fernando Briano <fernando@picandocodigo.net>
2025-04-16 16:39:34 +0100

    [API] Adds esql get_query and list_queries

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/esql/get_query.rb
@@ -0,0 +1,65 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# This code was automatically generated from the Elasticsearch Specification
+# See https://github.com/elastic/elasticsearch-specification
+# See Elasticsearch::ES_SPECIFICATION_COMMIT for commit hash.
+module Elasticsearch
+  module API
+    module Esql
+      module Actions
+        # Get a specific running ES|QL query information.
+        # Returns an object extended information about a running ES|QL query.
+        # This functionality is Experimental and may be changed or removed
+        # completely in a future release. Elastic will take a best effort approach
+        # to fix any issues, but experimental features are not subject to the
+        # support SLA of official GA features.
+        #
+        # @option arguments [String] :id The query ID (*Required*)
+        # @option arguments [Hash] :headers Custom HTTP headers
+        #
+        # @see [TODO]
+        #
+        def get_query(arguments = {})
+          request_opts = { endpoint: arguments[:endpoint] || 'esql.get_query' }
+
+          defined_params = [:id].each_with_object({}) do |variable, set_variables|
+            set_variables[variable] = arguments[variable] if arguments.key?(variable)
+          end
+          request_opts[:defined_params] = defined_params unless defined_params.empty?
+
+          raise ArgumentError, "Required argument 'id' missing" unless arguments[:id]
+
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body = nil
+
+          _id = arguments.delete(:id)
+
+          method = Elasticsearch::API::HTTP_GET
+          path   = "_query/queries/#{Utils.listify(_id)}"
+          params = {}
+
+          Elasticsearch::API::Response.new(
+            perform_request(method, path, params, body, headers, request_opts)
+          )
+        end
+      end
+    end
+  end
+end
