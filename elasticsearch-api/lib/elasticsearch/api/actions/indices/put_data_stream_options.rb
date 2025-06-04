b93d84e6028d8b41bee2a7e6af91a17867a3615a
Fernando Briano <fernando@picandocodigo.net>
2025-06-04 12:58:13 +0100

    [API] Adds data stream options endpoints

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/indices/put_data_stream_options.rb
@@ -0,0 +1,83 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# This code was automatically generated from the Elasticsearch Specification
+# See https://github.com/elastic/elasticsearch-specification
+# See Elasticsearch::ES_SPECIFICATION_COMMIT for commit hash.
+module Elasticsearch
+  module API
+    module Indices
+      module Actions
+        # Update data stream options.
+        # Update the data stream options of the specified data streams.
+        #
+        # @option arguments [String, Array<String>] :name Comma-separated list of data streams used to limit the request.
+        #  Supports wildcards (`*`).
+        #  To target all data streams use `*` or `_all`. (*Required*)
+        # @option arguments [String, Array<String>] :expand_wildcards Type of data stream that wildcard patterns can match.
+        #  Supports comma-separated values, such as `open,hidden`.
+        #  Valid values are: `all`, `hidden`, `open`, `closed`, `none`. Server default: open.
+        # @option arguments [Time] :master_timeout Period to wait for a connection to the master node. If no response is
+        #  received before the timeout expires, the request fails and returns an
+        #  error. Server default: 30s.
+        # @option arguments [Time] :timeout Period to wait for a response.
+        #  If no response is received before the timeout expires, the request fails and returns an error. Server default: 30s.
+        # @option arguments [Boolean] :error_trace When set to `true` Elasticsearch will include the full stack trace of errors
+        #  when they occur.
+        # @option arguments [String] :filter_path Comma-separated list of filters in dot notation which reduce the response
+        #  returned by Elasticsearch.
+        # @option arguments [Boolean] :human When set to `true` will return statistics in a format suitable for humans.
+        #  For example `"exists_time": "1h"` for humans and
+        #  `"eixsts_time_in_millis": 3600000` for computers. When disabled the human
+        #  readable values will be omitted. This makes sense for responses being consumed
+        #  only by machines.
+        # @option arguments [Boolean] :pretty If set to `true` the returned JSON will be "pretty-formatted". Only use
+        #  this option for debugging only.
+        # @option arguments [Hash] :headers Custom HTTP headers
+        # @option arguments [Hash] :body request body
+        #
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/index.html
+        #
+        def put_data_stream_options(arguments = {})
+          request_opts = { endpoint: arguments[:endpoint] || 'indices.put_data_stream_options' }
+
+          defined_params = [:name].each_with_object({}) do |variable, set_variables|
+            set_variables[variable] = arguments[variable] if arguments.key?(variable)
+          end
+          request_opts[:defined_params] = defined_params unless defined_params.empty?
+
+          raise ArgumentError, "Required argument 'name' missing" unless arguments[:name]
+
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body = arguments.delete(:body)
+
+          _name = arguments.delete(:name)
+
+          method = Elasticsearch::API::HTTP_PUT
+          path   = "_data_stream/#{Utils.listify(_name)}/_options"
+          params = Utils.process_params(arguments)
+
+          Elasticsearch::API::Response.new(
+            perform_request(method, path, params, body, headers, request_opts)
+          )
+        end
+      end
+    end
+  end
+end
