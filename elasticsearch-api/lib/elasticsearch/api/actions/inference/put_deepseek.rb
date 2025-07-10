2fbc2ef35669e4e22a7f2943a6e5e77547b3c8c3
Fernando Briano <fernando@picandocodigo.net>
2025-07-10 10:36:03 +0100

    [API] Adds inference endpoints put_amazonsagemaker and put_deepseek Updates cluster.put_component_template: * removes timeout * Adds [String] :cause User defined reason for create the component template

--- elasticsearch-api/lib/elasticsearch/api/actions/inference/put_deepseek.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/inference/put_deepseek.rb
@@ -20,40 +20,44 @@
 #
 module Elasticsearch
   module API
-    module Cluster
+    module Inference
       module Actions
-        # Creates or updates a component template
+        # Configure a DeepSeek inference endpoint
         #
-        # @option arguments [String] :name The name of the template
-        # @option arguments [Boolean] :create Whether the index template should only be added if new or can also replace an existing one
-        # @option arguments [String] :cause User defined reason for create the component template
-        # @option arguments [Time] :master_timeout Specify timeout for connection to master
+        # @option arguments [String] :task_type The task type
+        # @option arguments [String] :deepseek_inference_id The inference Id
         # @option arguments [Hash] :headers Custom HTTP headers
-        # @option arguments [Hash] :body The template definition (*Required*)
+        # @option arguments [Hash] :body The inference endpoint's task and service settings
         #
-        # @see https://www.elastic.co/guide/en/elasticsearch/reference/8.18/indices-component-template.html
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/8.18/infer-service-deepseek.html
         #
-        def put_component_template(arguments = {})
-          request_opts = { endpoint: arguments[:endpoint] || 'cluster.put_component_template' }
+        def put_deepseek(arguments = {})
+          request_opts = { endpoint: arguments[:endpoint] || 'inference.put_deepseek' }
 
-          defined_params = [:name].each_with_object({}) do |variable, set_variables|
+          defined_params = %i[task_type deepseek_inference_id].each_with_object({}) do |variable, set_variables|
             set_variables[variable] = arguments[variable] if arguments.key?(variable)
           end
           request_opts[:defined_params] = defined_params unless defined_params.empty?
 
-          raise ArgumentError, "Required argument 'body' missing" unless arguments[:body]
-          raise ArgumentError, "Required argument 'name' missing" unless arguments[:name]
+          raise ArgumentError, "Required argument 'task_type' missing" unless arguments[:task_type]
+
+          unless arguments[:deepseek_inference_id]
+            raise ArgumentError,
+                  "Required argument 'deepseek_inference_id' missing"
+          end
 
           arguments = arguments.clone
           headers = arguments.delete(:headers) || {}
 
           body = arguments.delete(:body)
 
-          _name = arguments.delete(:name)
+          _task_type = arguments.delete(:task_type)
+
+          _deepseek_inference_id = arguments.delete(:deepseek_inference_id)
 
           method = Elasticsearch::API::HTTP_PUT
-          path   = "_component_template/#{Utils.__listify(_name)}"
-          params = Utils.process_params(arguments)
+          path   = "_inference/#{Utils.__listify(_task_type)}/#{Utils.__listify(_deepseek_inference_id)}"
+          params = {}
 
           Elasticsearch::API::Response.new(
             perform_request(method, path, params, body, headers, request_opts)
