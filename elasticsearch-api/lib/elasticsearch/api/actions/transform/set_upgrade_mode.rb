d1c85794b14a243bce250c7a54bd6a97f0783158
Fernando Briano <fernando@picandocodigo.net>
2025-08-27 10:37:18 +0100

    [API] Adds transform.set_upgrade_mode

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/transform/set_upgrade_mode.rb
@@ -0,0 +1,52 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from build hash f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch/tree/main/rest-api-spec
+#
+module Elasticsearch
+  module API
+    module Transform
+      module Actions
+        # Sets a cluster wide upgrade_mode setting that prepares transform indices for an upgrade.
+        #
+        # @option arguments [Boolean] :enabled Whether to enable upgrade_mode Transform setting or not. Defaults to false.
+        # @option arguments [Time] :timeout Controls the time to wait before action times out. Defaults to 30 seconds
+        # @option arguments [Hash] :headers Custom HTTP headers
+        #
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/8.19/transform-set-upgrade-mode.html
+        #
+        def set_upgrade_mode(arguments = {})
+          request_opts = { endpoint: arguments[:endpoint] || 'transform.set_upgrade_mode' }
+
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body   = nil
+
+          method = Elasticsearch::API::HTTP_POST
+          path   = '_transform/set_upgrade_mode'
+          params = Utils.process_params(arguments)
+
+          Elasticsearch::API::Response.new(
+            perform_request(method, path, params, body, headers, request_opts)
+          )
+        end
+      end
+    end
+  end
+end
