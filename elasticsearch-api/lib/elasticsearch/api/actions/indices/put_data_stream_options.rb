76d66e4ba078b215e31cecee3c1a4497652cd282
Fernando Briano <fernando@picandocodigo.net>
2025-08-29 11:16:37 +0100

    [API] Updates API specification to eeef9c87c1273e5dd3a5a313768bfa6728cd39cc - Updates source code docs links - Updates transform.set_upgrade_mode

--- elasticsearch-api/lib/elasticsearch/api/actions/indices/put_data_stream_options.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/indices/put_data_stream_options.rb
@@ -49,7 +49,7 @@ module Elasticsearch
         # @option arguments [Hash] :headers Custom HTTP headers
         # @option arguments [Hash] :body request body
         #
-        # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/index.html
+        # @see https://www.elastic.co/docs/api/doc/elasticsearch/operation/operation-indices-put-data-stream-options
         #
         def put_data_stream_options(arguments = {})
           request_opts = { endpoint: arguments[:endpoint] || 'indices.put_data_stream_options' }
