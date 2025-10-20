cc0c33951cff094dda17f8b2246640ec86891857
Elastic Machine <elasticmachine@users.noreply.github.com>
2025-10-20 06:04:02 +0000

    Auto-generated API code

---
+++ elasticsearch-api/lib/elasticsearch/api/actions/indices/get_migrate_reindex_status.rb
@@ -0,0 +1,60 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from build hash f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch/tree/main/rest-api-spec
+#
+module Elasticsearch
+  module API
+    module Indices
+      module Actions
+        # Get the migration reindexing status
+        #
+        # @option arguments [String] :index The index or data stream name
+        # @option arguments [Hash] :headers Custom HTTP headers
+        #
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/8.19/migrate-data-stream.html
+        #
+        def get_migrate_reindex_status(arguments = {})
+          request_opts = { endpoint: arguments[:endpoint] || 'indices.get_migrate_reindex_status' }
+
+          defined_params = [:index].each_with_object({}) do |variable, set_variables|
+            set_variables[variable] = arguments[variable] if arguments.key?(variable)
+          end
+          request_opts[:defined_params] = defined_params unless defined_params.empty?
+
+          raise ArgumentError, "Required argument 'index' missing" unless arguments[:index]
+
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body   = nil
+
+          _index = arguments.delete(:index)
+
+          method = Elasticsearch::API::HTTP_GET
+          path   = "_migration/reindex/#{Utils.__listify(_index)}/_status"
+          params = {}
+
+          Elasticsearch::API::Response.new(
+            perform_request(method, path, params, body, headers, request_opts)
+          )
+        end
+      end
+    end
+  end
+end
