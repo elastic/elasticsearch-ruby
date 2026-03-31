c759da94804378a3f95fb66a55cd0a3a7768eb5d
Fernando Briano <fernando@picandocodigo.net>
2026-03-31 11:27:19 +0100

    Auto-generated API code
    Updates with a change in Rubocop:
    https://github.com/rubocop/rubocop/pull/15023

--- elasticsearch-api/lib/elasticsearch/api/actions/inference/put_azureaistudio.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/inference/put_azureaistudio.rb
@@ -42,7 +42,6 @@ module Elasticsearch
 
           raise ArgumentError, "Required argument 'body' missing" unless arguments[:body]
           raise ArgumentError, "Required argument 'task_type' missing" unless arguments[:task_type]
-
           unless arguments[:azureaistudio_inference_id]
             raise ArgumentError,
                   "Required argument 'azureaistudio_inference_id' missing"
--- elasticsearch-api/lib/elasticsearch/api/actions/inference/put_azureopenai.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/inference/put_azureopenai.rb
@@ -42,7 +42,6 @@ module Elasticsearch
 
           raise ArgumentError, "Required argument 'body' missing" unless arguments[:body]
           raise ArgumentError, "Required argument 'task_type' missing" unless arguments[:task_type]
-
           unless arguments[:azureopenai_inference_id]
             raise ArgumentError,
                   "Required argument 'azureopenai_inference_id' missing"
