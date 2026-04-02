f673593ca24e66dee14302861eadf90cd956c6b6
Fernando Briano <fernando@picandocodigo.net>
2026-04-02 12:20:39 +0100

    Rubocop styling update for Layout/EmptyLineAfterGuardClause Source: rubocop/rubocop#15023

--- elasticsearch-api/lib/elasticsearch/api/actions/inference/put_hugging_face.rb
+++ elasticsearch-api/lib/elasticsearch/api/actions/inference/put_hugging_face.rb
@@ -82,7 +82,6 @@ module Elasticsearch
 
           raise ArgumentError, "Required argument 'body' missing" unless arguments[:body]
           raise ArgumentError, "Required argument 'task_type' missing" unless arguments[:task_type]
-
           unless arguments[:huggingface_inference_id]
             raise ArgumentError,
                   "Required argument 'huggingface_inference_id' missing"
