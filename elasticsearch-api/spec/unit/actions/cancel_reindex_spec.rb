9234a160c40e16c43974bb868581d5efa5a0a99f
Fernando Briano <fernando@picandocodigo.net>
2026-04-16 09:53:19 +0100

    [API] Adds Unit tests for new APIs

--- elasticsearch-api/spec/unit/actions/cancel_reindex_spec.rb
+++ elasticsearch-api/spec/unit/actions/cancel_reindex_spec.rb
@@ -15,9 +15,21 @@
 # specific language governing permissions and limitations
 # under the License.
 
-module Elasticsearch
-  module API
-    VERSION = '9.3.0'.freeze
-    ES_SPECIFICATION_COMMIT = '8b89018432772ecd948d24d0463dd57f663835c5'.freeze
+require 'spec_helper'
+
+describe 'client#get_reindex' do
+  let(:expected_args) do
+    [
+      'GET',
+      '_reindex/foo',
+      {},
+      nil,
+      {},
+      { endpoint: 'get_reindex', defined_params: { task_id: 'foo' } }
+    ]
+  end
+
+  it 'performs the request' do
+    expect(client_double.get_reindex(task_id: 'foo')).to be_a Elasticsearch::API::Response
   end
 end
--- elasticsearch-api/spec/unit/actions/security/clone_api_key_spec.rb
+++ elasticsearch-api/spec/unit/actions/security/clone_api_key_spec.rb
@@ -15,9 +15,21 @@
 # specific language governing permissions and limitations
 # under the License.
 
-module Elasticsearch
-  module API
-    VERSION = '9.3.0'.freeze
-    ES_SPECIFICATION_COMMIT = '8b89018432772ecd948d24d0463dd57f663835c5'.freeze
+require 'spec_helper'
+
+describe 'client#security#clone_api_key' do
+  let(:expected_args) do
+    [
+      'POST',
+      '_security/api_key/clone',
+      {},
+      {},
+      {},
+      { endpoint: 'security.clone_api_key' }
+    ]
+  end
+
+  it 'performs the request' do
+    expect(client_double.security.clone_api_key(body: {})).to be_a Elasticsearch::API::Response
   end
 end
