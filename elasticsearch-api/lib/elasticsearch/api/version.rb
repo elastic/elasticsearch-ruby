3fa9fc0b9aa053129255fa6f06ed56ccdf287dfd
Fernando Briano <fernando@picandocodigo.net>
2026-05-01 14:17:26 +0100

    Bumps version to 9.4.0

--- elasticsearch-api/lib/elasticsearch/api/version.rb
+++ elasticsearch-api/lib/elasticsearch/api/version.rb
@@ -17,7 +17,7 @@
 
 module Elasticsearch
   module API
-    VERSION = '9.3.0'.freeze
+    VERSION = '9.4.0'.freeze
     ES_SPECIFICATION_COMMIT = '7ada49949544b577bc7c9ab5420771204a12cdd2'.freeze
   end
 end
